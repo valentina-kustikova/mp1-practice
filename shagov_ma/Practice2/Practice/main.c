1fdbe96cd2dce293cd926880d3277d3e39daaa2f
MaksimShagov <makcim.shagov@mail.ru>
2018-09-23 08:57:03 +0300

    Practice2 structure

---
+++ shagov_ma/Practice2/Practice/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+void main()
+{
+	float x,y,r; //координаты первой окр
+    float a,b,c; //координаты второй окр
+	float k; //расстояние между центрами
+	setlocale(LC_ALL, "Russian");
+	printf("Введите координаты центра первой окружности и ее радиус\n");
+	scanf("%f %f %f", &x, &y, &r);
+	printf("Введите координаты центра второй окружности и ее радиус\n");
+	scanf("%f %f %f", &a, &b, &c);
+	if ((x==a)&&(y==b)&&(r==c))
+	{
+		printf("Окружности совпадают\n");
+		return;
+	}
+	if ((x==a)&&(y==b)&&(r!=c))
+	{
+		printf("Одна окружность вложена в другую\n");
+		return;
+	}
+	k=sqrt((x-a)*(x-a)+(y-b)*(y-b));
+	if (k>r+c)
+	{
+		printf("Окружности не пересекаются\n");
+		return;
+	}
+	if ((k+r==c)||(k+c==r))
+	{
+		printf("Окружности касаются внутренним образом\n");
+		return;
+	}
+	if (k<r+c)
+	{
+		printf("Окружности пересекаются\n");
+		return;
+	}
+	if (k==r+c)
+	{
+		printf("Окружности касаются\n");
+		return;
+	}
+
+}
