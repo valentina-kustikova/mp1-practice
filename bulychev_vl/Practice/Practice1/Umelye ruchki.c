e2c15481b95269ebbbb78e79602cfe62e2ccd662
Vladislavbul <vlad-hk.19.03@mail.ru>
2018-11-19 16:07:53 +0300

    Practice1_3

--- bulychev_vl/Practice/Practice1/Umelye ruchki.c
+++ bulychev_vl/Practice/Practice1/Umelye ruchki.c
@@ -1,68 +1,70 @@
 #include <stdio.h>
-#include <math.h>
 void main()
 {
-	float h, w, d; //vysota(h), shirina(w), glubina(d)
-	float pDVP, pDSP, pDerevo; //plotnosti
-	float zad, bok, verkh, niz, dveri; //komplektuyushhie shkafa
-	float x;
-	double a, polki; //kolichestvo polok
-	printf("Vvedite po poryadku vysotu (h), shirinu (w), glubinu(d) \nDannye dolzhny sootvetstvovat' sleduyushhemu: \n180 <= h <= 220; \n80 <= w <= 120; \n50 <= d <= 90; \n");
-	scanf("%f %f %f", &h, &w, &d);
-	
-	if ((h < 180) || (220 < h))
-	{
-		printf("Nepravil'nye vysota \n");
-		return;
-	}
+    float h, w, d; //vysota(h), shirina(w), glubina(d)
+    float pDVP, pDSP, pDerevo; //plotnosti
+    double zad, bok, verkh, niz, dveri; //komplektuyushhie shkafa
+    float x;
+    double a, polki; //kolichestvo polok
+    printf("Vvedite po poryadku vysotu (h), shirinu (w), glubinu(d) \nDannye dolzhny sootvetstvovat' sleduyushhemu: \n180 <= h <= 220; \n80 <= w <= 120; \n50 <= d <= 90; \n");
+    scanf("%f %f %f", &h, &w, &d);
+    
+    if ((h < 180) || (220 < h))
+    {
+        printf("Nepravil'nye vysota \n");
+        return;
+    }
 
-	if ((w < 80) || (120 < w))
-	{
-		printf("Nepravil'nye shirina \n");
-		return;
-	}
+    if ((w < 80) || (120 < w))
+    {
+        printf("Nepravil'nye shirina \n");
+        return;
+    }
 
-	if ((d < 50) || (90 < d))
-	{
-		printf("Nepravil'nye glubina \n");
-		return;
-	}
-	
-	printf("Vyberete 1 ili 2 \n1 - ispol'zovanie standart nastroek (DVP = 0.85; DSP = 0.65; Derevo = 0.69) \n2 - vvod znachenij plotnostei samostoyatel'no  \nDannye vvodyatsya v g/cm^3 \n");
-	scanf("%f", &x);
-	
-	if ((x < 1) || (2 < x))
-	{
-		printf("Nepravil'nye dannye \n");
-		return;
-	}
+    if ((d < 50) || (90 < d))
+    {
+        printf("Nepravil'nye glubina \n");
+        return;
+    }
+    
+    printf("Vyberete 1 ili 2 \n1 - ispol'zovanie standart nastroek (DVP = 850; DSP = 650; Derevo = 690) \n2 - vvod znachenij plotnostei samostoyatel'no  \nDannye vvodyatsya v kg/m^3 \n");
+    scanf("%f", &x);
+    
+    if ((x < 1) || (2 < x))
+    {
+        printf("Nepravil'nye dannye \n");
+        return;
+    }
 
-	if (x == 1)
-	{
-		pDVP = 0.85f;
-		pDSP = 0.65f;
-		pDerevo = 0.69f;
-	}
+    if (x == 1)
+    {
+        pDVP = 850;
+        pDSP = 650;
+        pDerevo = 690;
+    }
 
-	if (x == 2)
-	{
-		printf("Vvedite plotnost' DVP (pDVP); plotnost' DSP (pDSP); plotnost' dereva (pDereva) \n");
-		scanf("%f %f %f", &pDVP, &pDSP, &pDerevo);
+    if (x == 2)
+    {
+        printf("Vvedite plotnost' DVP (pDVP); plotnost' DSP (pDSP); plotnost' dereva (pDereva) \n");
+        scanf("%f %f %f", &pDVP, &pDSP, &pDerevo);
 
-		if ((pDVP <= 0) || (pDSP <= 0) || (pDerevo <= 0))
-		{
-		printf("Nepravil'nye dannye \n");
-		return;
-		}
-	}
-	
-	zad = h * w * 0.5f/*tolshhina*/ * pDVP;
-	bok = 2 * h * d * 1.5f/*tolshhina*/ * pDSP;
-	verkh = w * d * 1.5f/*tolshhina*/ * pDSP;
-	niz = w * d * 1.5f/*tolshhina*/ * pDSP;
-	dveri = h * w * 1/*tolshhina*/ * pDerevo;
-	a = floor (h / 40); //kolichestvo polok
-	polki = a * d * w * 1.5f/*tolshhina*/ * pDSP;
+        if ((pDVP <= 0) || (pDSP <= 0) || (pDerevo <= 0))
+        {
+        printf("Nepravil'nye dannye \n");
+        return;
+        }
+    }
+    
+    w /= 100;
+    d /= 100;
+    h /= 100;
+    verkh = w * d * 0.015 * pDSP; //naklad po verx vsego
+    niz = w * d * 0.015 * pDSP; //naklad po verx vsego
+    zad = (h - 0.03) * w * 0.005 * pDVP;
+    bok = 2 * (h - 0.03) * (d - 0.015) * 0.015 * pDSP;
+    dveri = (h - 0.03) * w * 0,01 * pDerevo;
+    a = floor ((h - 0.03) / 0.4); 
+    polki = a * (d - 0.015) * (w - 0.03) * 0.015 * pDSP;
 
-	printf("Otvet dan v kilogrammakh \nMassa shkafa = %f", (zad + bok + verkh + niz + dveri + polki)/1000);
+    printf("Otvet dan v kilogrammakh \nMassa shkafa = %f", zad + bok + verkh + niz + dveri + polki);
 }
