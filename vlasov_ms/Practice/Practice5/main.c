a712a68d5438cef6b085ad90e0cd47129371b321
Власов Максим Сергеевич <vlasov.m@ccam.cmc.unn.net>
2018-11-19 14:24:28 +0300

    исправлено форматирование

--- vlasov_ms/Practice/Practice5/main.c
+++ vlasov_ms/Practice/Practice5/main.c
@@ -13,7 +13,8 @@ void insert_sort(int a[], int n);
 void bubble_sort(int a[], int n);
 void counting_sort(int a[], int n);
 
-void main() {
+void main() 
+{
     int a[N], algo;
     setlocale(LC_ALL, "Russian");
     gen(a, N);
@@ -21,40 +22,45 @@ void main() {
     output(a, N);
     printf("Алгоритм сортировки:       "); // ДОСТУПНЫЕ АЛГОРИТМЫ:
     scanf("%d", &algo);                    // 1 - сортировка выбором
-    switch (algo) {                        // 2 - сортировка простыми вставками
-    case 1:                                // 3 - пузырьковая сортировка
-        choosing_sort(a, N);               // 4 - сортировка подсчетом
-        break;
-    case 2:
-        insert_sort(a, N);
-        break;
-    case 3:
-        bubble_sort(a, N);
-        break;
-    case 4:
-        counting_sort(a, N);
-        break;
-    default:
-        printf("Неверный номер.\n");
-        return;
+    switch (algo) 
+    {                        // 2 - сортировка простыми вставками
+        case 1:                                // 3 - пузырьковая сортировка
+            choosing_sort(a, N);               // 4 - сортировка подсчетом
+            break;
+        case 2:
+            insert_sort(a, N);
+            break;
+        case 3:
+            bubble_sort(a, N);
+            break;
+        case 4:
+            counting_sort(a, N);
+            break;
+        default:
+            printf("Неверный номер.\n");
+            return;
     }
     printf("Отсортированный массив: ");
     output(a, N);
 }
 
 // Генерация массива
-void gen(int a[], int n) {
+void gen(int a[], int n) 
+{
     int i;
     srand((unsigned)time(0));
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) 
+    {
         a[i] = rand() % (K - 0 + 1) + 0;
     }
 }
 
 // Вывод массива
-void output(int a[], int n) {
+void output(int a[], int n) 
+{
     int i;
-    for (i = 0; i < n - 1; i++) {
+    for (i = 0; i < n - 1; i++) 
+    {
         printf("%4d", a[i]);
     }
     printf("%4d\n", a[n - 1]);
@@ -64,11 +70,14 @@ void output(int a[], int n) {
 void choosing_sort(int a[], int n)
 {
     int i, j, min, minidx;
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) 
+    {
         min = a[i];
         minidx = i;
-        for (j = i + 1; j < n; j++) {
-            if (a[j] < min) {
+        for (j = i + 1; j < n; j++) 
+        {
+            if (a[j] < min) 
+            {
                 min = a[j];
                 minidx = j;
             }
@@ -82,10 +91,12 @@ void choosing_sort(int a[], int n)
 void insert_sort(int a[], int n)
 {
     int i, j, temp;
-    for (i = 1; i < n; i++) {
+    for (i = 1; i < n; i++) 
+    {
         temp = a[i];
         j = i - 1;
-        while ((j >= 0) && (a[j]>temp)) {
+        while ((j >= 0) && (a[j]>temp)) 
+        {
             a[j + 1] = a[j];
             a[j--] = temp;
         }
@@ -96,9 +107,12 @@ void insert_sort(int a[], int n)
 void bubble_sort(int a[], int n)
 {
     int i, j, temp;
-    for (i = 0; i < n; i++) {
-        for (j = 1; j < n - i; j++) {
-            if (a[j - 1]>a[j]) {
+    for (i = 0; i < n; i++) 
+    {
+        for (j = 1; j < n - i; j++) 
+        {
+            if (a[j - 1]>a[j]) 
+            {
                 temp = a[j];
                 a[j] = a[j - 1];
                 a[j - 1] = temp;
@@ -112,11 +126,14 @@ void counting_sort(int a[], int n)
 {
     int count[K] = { 0 };
     int idx = 0, i, j;
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) 
+    {
         count[a[i]]++;
     }
-    for (i = 0; i < K; i++) {
-        for (j = 0; j < count[i]; j++) {
+    for (i = 0; i < K; i++) 
+    {
+        for (j = 0; j < count[i]; j++) 
+        {
             a[idx++] = i;
         }
     }
