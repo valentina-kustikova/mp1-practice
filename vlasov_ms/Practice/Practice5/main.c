0af143da0f446c1f5daf1f1d352373ea9ccd8bf2
Власов Максим Сергеевич <vla2000@inbox.ru>
2018-12-11 20:41:22 +0300

    улучшены функции сортировок

--- vlasov_ms/Practice/Practice5/main.c
+++ vlasov_ms/Practice/Practice5/main.c
@@ -4,13 +4,11 @@
 #include <locale.h>
 #include <windows.h>
 #include <string.h>
-#define _CRT_SECURE_NO_WARNINGS 1 // разрешение на исп. scanf
-#define N 50                      // количество элементов массива
-#define BUFFER_SIZE 2048          // размер буфера
+#define _CRT_SECURE_NO_WARNINGS
+#define N 10     // количество элементов массива
 
 void gen(int a[], int n, int min, int max);
 void output(int a[], int n);
-int dir_contents(const wchar_t *sDir);
 
 void choosing_sort(int a[], int n);
 void insert_sort(int a[], int n);
@@ -21,29 +19,54 @@ void quick_split(int a[], int* i, int* j, int p);
 void merge_sort(int a[], int l, int r);
 void merge_sorted(int a[], int l, int m, int r);
 
-void user_input(wchar_t **wa) 
-{
-    char *a;
-    *wa = (wchar_t*)malloc(BUFFER_SIZE * sizeof(wchar_t));
-    a = (char*)malloc(BUFFER_SIZE * sizeof(char));
-    fgets(a, BUFFER_SIZE, stdin);
-    a[strlen(a) - 1] = '\0';
-    swprintf(*wa, BUFFER_SIZE, L"%hs", a);
+int dir_contents(const wchar_t *sDir);
+
+wchar_t* user_input() {
+    wchar_t *string;
+    char symbol;
+    int i = 0;
+    string = (wchar_t*)malloc(0 * sizeof(wchar_t));
+    do
+    {
+        symbol = getchar();
+        printf("%c", symbol);
+        string = (wchar_t*)realloc(string, 1 * sizeof(wchar_t));
+        //string[i] = symbol >> (sizeof(wchar_t) - sizeof(char)) * 8;
+        string[i] = (wchar_t)symbol;
+        //*(string + i) = *(string + i) >> 8;
+        i++;
+    } while (symbol != '\n');
+    return string;
 }
+
+/*unsigned short* user_input() {
+    unsigned short *wstring;
+    char cstring[2048] = { 0 };
+    int i = 0, length;
+    for (i = 0; cstring[i] != "."; i++) {
+        scanf("%c", &(cstring[i]));
+    }
+    length = strlen(cstring);
+    wstring = (unsigned short*)malloc(length * sizeof(unsigned short));
+    for (i = 0; i < length; i++) {
+        wstring[i] = (unsigned short)cstring[i];
+    }
+    return wstring;
+}*/
 
 void main() 
 {
-    int a[N], algo;
-    wchar_t *path;
+    int a[N], algo, min, max, i;
+    unsigned short *path;
     setlocale(LC_ALL, "Russian");
 
     // Debug - START
-    user_input(&path);
-    dir_contents(path);
-    return;
+    //path = user_input();
+    //dir_contents((wchar_t*)path);
+    //return;
     // Debug - END
 
-    gen(a, N, 300, 302);
+    gen(a, N, 502, 692);
     printf("Исходный массив:        ");
     output(a, N);
     printf("Алгоритм сортировки:       ");
@@ -172,12 +195,14 @@ void counting_sort(int a[], int n)
 void quick_sort(int a[], int n1, int n2)
 {
     int m = (n1 + n2) / 2;
-    int i = n1, j = n2 + 1;
+    int i = n1, j = n2;
     if (n1 == n2)
         return;
     quick_split(a, &i, &j, a[m]);
-    quick_sort(a, n1, i);
-    quick_sort(a, j, n2);
+    if (i < n2)
+        quick_sort(a, i, n2);
+    if (j > n1)
+        quick_sort(a, n1, j);
 }
 
 // Быстрая перестановка
@@ -187,12 +212,17 @@ void quick_split(int a[], int* i, int* j, int p)
         while (a[*i] < p) (*i)++;
         while (a[*j] > p) (*j)--;
         if (*i <= *j) {
-            int tmp = a[*i];
-            a[*i] = a[*j];
-            a[*j] = tmp;
+            if (a[*i] > a[*j]) 
+            {
+                int tmp = a[*i];
+                a[*i] = a[*j];
+                a[*j] = tmp;
+            }
+            (*i)++;
+            (*j)--;
         }
         output(a, N);
-    } while (*i < *j);
+    } while (*i <= *j);
 }
 
 // Сортировка слиянием
@@ -234,7 +264,7 @@ int dir_contents(const wchar_t *sDir)
 {
     WIN32_FIND_DATA fdFile;
     HANDLE hFind = NULL;
-    wchar_t sPath[BUFFER_SIZE];
+    wchar_t sPath[2048];
 
     wsprintf(sPath, L"%s\\*.*", sDir);
     if ((hFind = FindFirstFile(sPath, &fdFile)) == INVALID_HANDLE_VALUE)
@@ -242,7 +272,7 @@ int dir_contents(const wchar_t *sDir)
         wprintf(L"Path not found: [%s]\n", sDir);
         return 1;
     }
-	wprintf(L"Path: [%s]\n", sDir);
+    wprintf(L"Path: [%s]\n", sDir);
     do
     {
         if (wcscmp(fdFile.cFileName, L".") != 0 && wcscmp(fdFile.cFileName, L"..") != 0)
