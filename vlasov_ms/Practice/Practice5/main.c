0c21c55f195e38a034ecf54e1d399e452440b0bf
Власов Максим Сергеевич <vla2000@inbox.ru>
2018-12-14 21:16:14 +0300

    исправления конфликтов предыдущего коммита

--- vlasov_ms/Practice/Practice5/main.c
+++ vlasov_ms/Practice/Practice5/main.c
@@ -4,8 +4,9 @@
 #include <locale.h>
 #include <windows.h>
 #include <string.h>
-#define _CRT_SECURE_NO_WARNINGS
-#define N 10     // количество элементов массива
+#define _CRT_SECURE_NO_WARNINGS   // разрешение на исп. scanf
+#define N 50                      // количество элементов массива
+#define BUFFER_SIZE 2048          // размер буфера
 
 void gen(int a[], int n, int min, int max);
 void output(int a[], int n);
@@ -21,48 +22,25 @@ void merge_sorted(int a[], int l, int m, int r);
 
 int dir_contents(const wchar_t *sDir);
 
-wchar_t* user_input() {
-    wchar_t *string;
-    char symbol;
-    int i = 0;
-    string = (wchar_t*)malloc(0 * sizeof(wchar_t));
-    do
-    {
-        symbol = getchar();
-        printf("%c", symbol);
-        string = (wchar_t*)realloc(string, 1 * sizeof(wchar_t));
-        //string[i] = symbol >> (sizeof(wchar_t) - sizeof(char)) * 8;
-        string[i] = (wchar_t)symbol;
-        //*(string + i) = *(string + i) >> 8;
-        i++;
-    } while (symbol != '\n');
-    return string;
+void user_input(wchar_t **wa)
+{
+	char *a;
+	*wa = (wchar_t*)malloc(BUFFER_SIZE * sizeof(wchar_t));
+	a = (char*)malloc(BUFFER_SIZE * sizeof(char));
+	fgets(a, BUFFER_SIZE, stdin);
+	a[strlen(a) - 1] = '\0';
+	swprintf(*wa, BUFFER_SIZE, L"%hs", a);
 }
-
-/*unsigned short* user_input() {
-    unsigned short *wstring;
-    char cstring[2048] = { 0 };
-    int i = 0, length;
-    for (i = 0; cstring[i] != "."; i++) {
-        scanf("%c", &(cstring[i]));
-    }
-    length = strlen(cstring);
-    wstring = (unsigned short*)malloc(length * sizeof(unsigned short));
-    for (i = 0; i < length; i++) {
-        wstring[i] = (unsigned short)cstring[i];
-    }
-    return wstring;
-}*/
 
 void main() 
 {
-    int a[N], algo, min, max, i;
-    unsigned short *path;
+    int a[N], algo;
+    wchar_t *path;
     setlocale(LC_ALL, "Russian");
 
     // Debug - START
-    //path = user_input();
-    //dir_contents((wchar_t*)path);
+    //user_input(&path);
+    //dir_contents(path);
     //return;
     // Debug - END
 
@@ -179,7 +157,6 @@ void counting_sort(int a[], int n)
             max = a[i];
     }
     delta = abs(max - min) + 1;
-    //printf("min=%d, max=%d, delta=%d\n", min, max, delta); // debug info
     count = (int*)malloc(delta * sizeof(int));
     for (i = 0; i < delta; i++)
         count[i] = 0;
@@ -221,7 +198,6 @@ void quick_split(int a[], int* i, int* j, int p)
             (*i)++;
             (*j)--;
         }
-        output(a, N);
     } while (*i <= *j);
 }
 
@@ -245,7 +221,7 @@ void merge_sorted(int a[], int l, int m, int r)
     i = l;
     j = m + 1;
     while ((i <= m) && (j <= r)) {
-        if (a[i] < a[j])
+        if (a[i] <= a[j])
             merged[s++] = a[i++];
         else
             merged[s++] = a[j++];
@@ -264,7 +240,7 @@ int dir_contents(const wchar_t *sDir)
 {
     WIN32_FIND_DATA fdFile;
     HANDLE hFind = NULL;
-    wchar_t sPath[2048];
+    wchar_t sPath[BUFFER_SIZE];
 
     wsprintf(sPath, L"%s\\*.*", sDir);
     if ((hFind = FindFirstFile(sPath, &fdFile)) == INVALID_HANDLE_VALUE)
