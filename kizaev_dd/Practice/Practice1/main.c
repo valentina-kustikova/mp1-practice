8dcc3ee5f8b9a66f72a619d713b6e678aa1f901e
DmitryKizaev <d-kiz@yandex.ru>
2018-12-26 22:55:37 +0300

    исправление предыдущих практик

--- kizaev_dd/Practice/Practice1/main.c
+++ kizaev_dd/Practice/Practice1/main.c
@@ -1,48 +1,34 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
- void main()
- {
-    //density
-    double p_dvp, p_dsp, p_wood;
-    //back part
-    double h, w;
-    //side parts
-    double d;
-    double num;
-    
-    p_dvp = 0; p_dsp = 0; p_wood = 0; h = 0; w = 0; d = 0;
-
-    const double back_t = 0.005;
-    const double side_t = 0.015;
-    const double roof_t = 0.015;
-    const double door_t = 0.01;
-    //no info about thickness of shelves, let it be 1 cm
-    const double shelf_t = 0.01;
-
+void main()
+{
+    double p_dvp = 0.0, p_dsp = 0.0, p_wood = 0.0; //density
     printf ("enter density in kg/(m^3) \n");
     printf ("\n");
+    
     do
     {
         printf ("enter DVP density: \n");
         scanf ("%lf", &p_dvp);
     }
-    while (p_dvp <= 0);
+    while (p_dvp <= 0.0);
 
     do
     {
         printf("enter DSP density: \n");
         scanf ("%lf", &p_dsp);
     }
-    while (p_dsp <= 0);
+    while (p_dsp <= 0.0);
 
     do
     {
         printf("enter wood density: \n");
         scanf ("%lf", &p_wood);
     }
-    while (p_wood <= 0);
+    while (p_wood <= 0.0);
 
+    double h = 0.0, w = 0.0; // back part
     do
     {
         printf("enter height (1.80 to 2.20) \n");
@@ -56,7 +42,8 @@
         scanf("%lf", &w);
     }
     while ((w < 0.80) || (w > 1.20));
-    
+
+    double d = 0.0; // side parts
     do
     {
         printf("enter depth (0.50 to 0.90) \n");
@@ -64,17 +51,22 @@
     }
     while ((d < 0.50) || (d > 0.90));
 
-    num = (int) (h - 2 * roof_t) / 0.40;
+    double roof_t = 0.015;
+    int num = (int) ((h - 2 * roof_t) / 0.40); // number of shelves
     
     /* for tests
     printf ("number of shelves: %lf \n", num);
     */
+
+    double back_t = 0.005, side_t = 0.015, door_t = 0.01;
+    //no info about thickness of shelves, let it be 1 cm
+    double shelf_t = 0.01;
     
     double mass=
-    (p_dvp * (h * w * back_t) + //back part
-    p_dsp * ((2 * (h * d * side_t) + (w * d * roof_t)) + //side & roof parts
+    p_dvp * (h * w * back_t) + //back part
+    p_dsp * (2 * ((h * d * side_t) + (w * d * roof_t)) + //sides, roof & bottom
     num * (w * d * shelf_t)) +  // shelves
-    p_wood * (h * w * door_t)); //doors
+    p_wood * (h * w * door_t); //doors
     
-    printf ("weight in kg: %lf \n", mass);
+    printf ("weight in kg: %.2lf \n", mass);
 }
