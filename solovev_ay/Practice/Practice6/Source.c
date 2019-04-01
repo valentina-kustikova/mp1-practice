54f2c0279f4fdb845311f147cab881ce16e87e90
Alexandr-Solovev <43129356+Alexandr-Solovev@users.noreply.github.com>
2019-04-01 23:24:46 +0300

    Update Source.c

--- solovev_ay/Practice/Practice6/Source.c
+++ solovev_ay/Practice/Practice6/Source.c
@@ -1,14 +1,57 @@
 #include "Header.h"
 #include <stdlib.h>
 #include <math.h>
+#include <stdio.h>
+#include<locale.h>
 #define _CRT_SECURE_NO_WARNINGS
 void main()
 {
-	int kolvo = 2;
+	int kolvo, kolvo2;
 	VECTOR rofl;
 	VECTOR kurdyk;
+	VECTOR sum, sub;
+	setlocale(LC_ALL, "Russian");
+	printf("enter size vector1:");
+	scanf("%d", &kolvo);
 	kurdyk = input(kolvo);
-	rofl = input(2);
-	corner(rofl, kurdyk);
+	printf("enter size vector2:");
+	scanf("%d", &kolvo2);
+	rofl = input(kolvo2);
+	if (kolvo == kolvo2)
+	{
+		printf("length 1 and 2\n");
+		length(rofl);
+		printf("\n");
+		length(kurdyk);
+		printf("\n");
+		corner(rofl, kurdyk);
+		printf("\n");
+		printf("skalyar");
+		skalyar(rofl, kurdyk);
+		sum = Sum(kurdyk, rofl);
+		sub = roflan(kurdyk, rofl);
+		printf("\n");
+		printf("vector summa");
+		Output(sum);
+		printf("\n");
+		printf("vector raznosti");
+		Output(sub);
+		Delete(rofl);
+		Delete(sum);
+		Delete(kurdyk);
+		Delete(sub);
+	}
+	else
+	{
+		Output(rofl);
+		Output(kurdyk);
+		printf("length 1 and 2\n");
+		length(rofl);
+		printf("\n");
+		length(kurdyk);
+		printf("\n");
+		Delete(rofl);
+		Delete(kurdyk);
+	}
 	system("pause");
 }
