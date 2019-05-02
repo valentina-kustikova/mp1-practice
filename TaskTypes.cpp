a00f5735196cfd9b04972a96a427bc9e89929cb0
Svetlana Molotkova <42940205+SMolotkova@users.noreply.github.com>
2019-05-02 19:15:07 +0300

    Update TaskTypes.cpp

--- TaskTypes.cpp
+++ TaskTypes.cpp
@@ -4,30 +4,30 @@
 #include"Date.h"
 #include"Time.h"
 #include <fstream>
+#include <string>
 using namespace std;
 
 Tasks::Tasks()
 {
-	description = NULL;
+	description = "";
 	date = Date(0, 0, 0);
 }
 Tasks::~Tasks()
 {
-	delete[]description;	
+	description.clear();	
 }
 Type1::Type1()
 {
-	description = NULL;
+	description = "";
 }
-Type1::Type1(char* description, Date date)
+Type1::Type1(string description, Date date)
 {
-	this->description = new char[sizeof(description)];
-	memcpy(this->description, description, sizeof(description));
+	this->description = description;
 	this->date = date;
 }
 Type1::~Type1()
 {
-	delete[]description;
+	description.clear();
 }
 void Type1:: Print() const
 {
@@ -36,29 +36,22 @@ void Type1:: Print() const
 const Type1& Type1::operator=(const Type1& t)
 {
 	date = t.date;
-	if (description != NULL) delete[] description;
-	description = new char[50];
-	memcpy(description, t.description, sizeof(t.description));
+	if (description != "") description.clear();
+	description = t.description;
 	return *this;
 }
-//const Tasks& Tasks::operator=(const Tasks& t)
-//{
-//	date = t.date;
-//	description = new char[50];
-//	memcpy(description, t.description, sizeof(t.description));
-//	return *this;
-//}
+
 Type2 ::Type2()
 {
-	description = NULL;
+	description = "";
 	date = Date();
 	duration = 0;
 	time = Time();
 }
-Type2 ::Type2(char* des, Date date, unsigned duration, Time time)
+Type2 ::Type2(string des, Date date, unsigned duration, Time time)
 {
 	this->date = date;
-	description = new char[50];
+	this-> description  = des;
 	for (int i = 0; i < 50; i++)
 		description[i] = des[i];
 	this ->duration = duration;
@@ -67,7 +60,7 @@ Type2 ::Type2(char* des, Date date, unsigned duration, Time time)
 }
 Type2 ::~Type2()
 {
-	delete[]description;
+	description.clear();
 	date.~Date();
 	duration = 0;
 	time.~Time();
@@ -81,7 +74,8 @@ void Type2 :: Print()const
 const Type2& Type2 ::operator= (const Type2& t)
 {
 	date = t.date;
-	description = t.description;  // !!!!!
+	description.clear();
+	description = t.description; 
 	time = t.time;
 	duration = t.duration;
 	return *this;
