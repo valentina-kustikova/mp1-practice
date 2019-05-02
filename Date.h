f2ebd89bd3ca80489e7f0ca590b61cf75191a069
Svetlana Molotkova <42940205+SMolotkova@users.noreply.github.com>
2019-05-02 19:12:50 +0300

    Update Date.h

--- Date.h
+++ Date.h
@@ -10,7 +10,6 @@ public:
 	void PrintDate()const;
 	Date();
 	Date(int day, int month, int year);
-	Date(const Date& t);
 	~Date();
 	bool operator == (const Date &date1)const;
 	const Date& operator = (const Date& t);
