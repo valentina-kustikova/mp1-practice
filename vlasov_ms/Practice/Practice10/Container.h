ef846fb0ff18836f5ba8ce055657d00c5d23b179
vlasov-unn <vla2000@inbox.ru>
2019-05-08 20:03:09 +0300

    StupidContainer

--- vlasov_ms/Practice/Practice10/Container.h
+++ vlasov_ms/Practice/Practice10/Container.h
@@ -1,3 +1,9 @@
+/*
+This header contains class that implements data storing in the array of
+pointers by default. Class that use a simple array of objects is located
+in file "StupidContainer.h". Thank you for your understanding.
+*/
+
 #ifndef _CONTAINER_H_
 #define _CONTAINER_H_
 #include <exception>
@@ -6,200 +12,211 @@ template <typename T>
 class Container
 {
 	T** set;
-	size_t size, buffer;
-	static const size_t delta = 10;
+	int csize, buffer;
+	static const int delta = 10;
 
-	void alloc_for(size_t);
+	void alloc_for(int);
 	void shrink_to_fit();
-	size_t calc_buffer(size_t);
+	int  calc_buffer(int);
 public:
 	Container();
-	Container(size_t);
+	Container(int);
 	Container(const Container<T>&);
 	~Container();
 
-	size_t sz() const;
+	int size() const;
 	bool empty() const;
+	int find(T) const;
 
 	Container<T>& add(T);
+	Container<T>& add(T*);
+	Container<T>& copy(T*);
 	Container<T>& remove(T);
-	Container<T>& remove(size_t);
-	Container<T>& remove_if(bool(*callback)(T));
-	Container<T>& remove_if(bool(*callback)(T*));
+	Container<T>& iremove(int);
 	Container<T>& remove_all();
 
 	const Container<T>& operator=(const Container<T>&);
-	T& operator[](size_t);
-	const T& operator[](size_t) const;
+	T& operator[](int);
+	const T& operator[](int) const;
 };
 
 template<typename T>
-void Container<T>::alloc_for(size_t n)
+void Container<T>::alloc_for(int n)
 {
-	if (size + n <= buffer)
+	if (csize + n <= buffer)
 		return;
-	buffer = calc_buffer(size + n);
-	T** new_set = new T*[buffer];
-	for (size_t i = 0; i < size; i++)
+	buffer = calc_buffer(csize + n);
+	T * *new_set = new T * [buffer];
+	for (int i = 0; i < csize; i++)
 		new_set[i] = set[i];
 	delete[] set;
 	set = new_set;
-}
+};
 
 template<typename T>
 void Container<T>::shrink_to_fit()
 {
-	size_t empty = buffer - size;
+	int empty = buffer - csize;
 	if (empty >= delta)
 	{
-		size_t d = calc_buffer(size);
+		int d = calc_buffer(csize);
 		delete[](set + d);
+		// todo : delete objects by pointers one by one
 		buffer = d;
 	}
-}
+};
 
 template<typename T>
-size_t Container<T>::calc_buffer(size_t new_size)
+int Container<T>::calc_buffer(int new_size)
 {
 	return new_size / delta * delta + delta;
-}
+};
 
 template<typename T>
 Container<T>::Container()
 {
-	size = 0;
+	csize = 0;
 	buffer = 0;
 	set = nullptr;
-}
+};
 
 template<typename T>
-Container<T>::Container(size_t new_size)
+Container<T>::Container(int new_size)
 {
-	size = new_size;
+	csize = new_size;
 	buffer = calc_buffer(new_size);
-	set = new T*[buffer];
-	for (size_t i = 0; i < size; i++)
+	set = new T * [buffer];
+	for (int i = 0; i < csize; i++)
 		set[i] = new T;
-}
+};
 
 template<typename T>
 Container<T>::Container(const Container<T>& c)
 {
-	alloc_for(c.size);
-	for (size_t i = 0; i < c.size; i++)
-		**(set + i) = **(c.set + i);
-	size = c.size;
-}
+	alloc_for(c.csize);
+	for (int i = 0; i < c.csize; i++)
+		* *(set + i) = **(c.set + i);
+	csize = c.csize;
+};
 
 template<typename T>
 Container<T>::~Container()
 {
 	remove_all();
-}
+};
 
 template<typename T>
-size_t Container<T>::sz() const
+int Container<T>::size() const
 {
-	return size;
-}
+	return csize;
+};
 
 template<typename T>
 bool Container<T>::empty() const
 {
-	return size == 0;
+	return csize == 0;
+};
+
+template<typename T>
+int Container<T>::find(T elem) const
+{
+	for (int i = 0; i < csize; i++)
+		if (*(set[i]) == elem)
+			return i;
+	return -1;
+};
+
+template<typename T>
+Container<T>& Container<T>::add(T elem)
+{
+	alloc_for(1);
+	set[csize] = new T(elem);
+	csize += 1;
+	return *this;
+};
+
+template<typename T>
+Container<T>& Container<T>::add(T* elem)
+{
+	alloc_for(1);
+	set[csize] = elem;
+	csize += 1;
+	return *this;
 }
 
 template<typename T>
-Container<T>& Container<T>::add(T object)
+Container<T>& Container<T>::copy(T* elem)
 {
-	alloc_for(1);
-	set[size] = new T(object);
-	size += 1;
-	return *this;
+	return add(*elem);
 }
 
 template<typename T>
 Container<T>& Container<T>::remove(T object)
 {
-	for (size_t i = 0; i < size; i++)
+	for (int i = 0; i < csize; i++)
 		if ((*this)[i] == object)
-			remove(i);
+			iremove(i);
 	return *this;
-}
+};
 
 template<typename T>
-Container<T>& Container<T>::remove(size_t index)
+Container<T>& Container<T>::iremove(int index)
 {
-	if (index >= size)
-		throw std::out_of_range("Container does not contain element with that index.");
-	if (index == size - 1)
+	if (index >= csize)
+		throw std::out_of_range("Container does not contain element with that"
+			"index.");
+	if (index == csize - 1)
 	{
 		delete set[index];
-		size -= 1;
+		csize -= 1;
 		return *this;
 	}
 	delete set[index];
-	size -= 1;
-	set[index] = set[size];
+	csize -= 1;
+	set[index] = set[csize];
 	return *this;
-}
-
-template<typename T>
-Container<T>& Container<T>::remove_if(bool(*callback)(T))
-{
-	for (size_t i = 0; i < size; i++)
-		if ((*callback)((*this)[i]))
-			remove(i);
-	return *this;
-}
-
-template<typename T>
-inline Container<T>& Container<T>::remove_if(bool(*callback)(T*))
-{
-	for (size_t i = 0; i < size; i++)
-		if ((*callback)(set[i]))
-			remove(i);
-	return *this;
-}
+};
 
 template<typename T>
 Container<T>& Container<T>::remove_all()
 {
-	if (!size || !set)
+	if (!csize || !set)
 		return *this;
-	for (size_t i = 0; i < size; i++)
+	for (int i = 0; i < csize; i++)
 		delete set[i];
 	delete[] set;
 	set = nullptr;
-	size = buffer = 0;
+	csize = buffer = 0;
 	return *this;
-}
+};
 
 template<typename T>
 const Container<T>& Container<T>::operator=(const Container<T>& c)
 {
 	remove_all();
-	alloc_for(c.size);
-	for (size_t i = 0; i < c.size; i++)
-		**(set + i) = **(c.set + i);
-	size = c.size;
+	alloc_for(c.csize);
+	for (int i = 0; i < c.csize; i++)
+		* *(set + i) = **(c.set + i);
+	csize = c.csize;
 	return *this;
-}
+};
 
 template<typename T>
-T& Container<T>::operator[](size_t index)
+T& Container<T>::operator[](int index)
 {
-	if(index >= size)
-		throw std::out_of_range("Container does not contain element with that index.");
+	if ((index >= csize) || (index < 0))
+		throw std::out_of_range("Container does not contain element with that"
+																	"index.");
 	return **(set + index);
-}
+};
 
 template<typename T>
-const T& Container<T>::operator[](size_t index) const
+const T& Container<T>::operator[](int index) const
 {
-	if (index >= size)
-		throw std::out_of_range("Container does not contain element with that index.");
+	if ((index >= csize) || (index < 0))
+		throw std::out_of_range("Container does not contain element with that"
+																	"index.");
 	return **(set + index);
-}
+};
 
 #endif
--- vlasov_ms/Practice/Practice10/Practice10.cpp
+++ vlasov_ms/Practice/Practice10/Practice10.cpp
@@ -1,23 +1,184 @@
-﻿#include "Container.h"
+﻿#include "StupidContainer.h"
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <vector>
+
+void generate(int*, int);
+int random();
+void print(Container<int>);
+void print(Container<int*>);
+
 int main()
 {
+	int example[10];
+	generate(example, 10);
+	int needle = random(), needle2 = random();
+
+	std::cout << "TEST #1: SIMPLE CONTAINER\n";
 	Container<int> c;
+	std::cout << "Adding: ";
+	for (int i = 0; i < 10; i++)
+		c.add(example[i]), std::cout << c[i] << ' '; // operator[] test
+	std::cout << "\nSet element [" << needle << "] to " << needle2 << ": ";
+	try
+	{
+		c[needle] = needle2;
+		std::cout << "success  ";
+	}
+	catch (std::out_of_range)
+	{
+		std::cout << "index is too big  ";
+	}
+	print(c);
+	std::cout << "Size: " << c.size() << '\n';
+	std::cout << "Is empty: " << (c.empty() ? "true" : "false") << '\n';
 
-	c.add(21).add(45).add(23).add(24).add(25).add(26).add(27).add(28).add(29);
-	c.add(10);
-	c.add(11);
-	for (size_t i = 0; i < c.sz(); i++)
-		std::cout << c[i] << ' ';
-	std::cout << '\n';
+	std::cout << "Find (value = " << needle << "): ";
+	int index = c.find(needle);
+	if (index != -1)
+		std::cout << "found on index " << index << '\n';
+	else
+		std::cout << "not found\n";
 
-	c[0] = 30;
-	for (size_t i = 0; i < c.sz(); i++)
-		std::cout << c[i] << ' ';
-	std::cout << '\n';
+	std::cout << "Remove (value = " << needle2 << "): ";
+	try
+	{
+		c.remove(needle2);
+		std::cout << "deleted  ";
+	}
+	catch (std::range_error)
+	{
+		std::cout << "not found  ";
+	}
+	print(c);
 
-	c.remove(45);
-	for (size_t i = 0; i < c.sz(); i++)
+	std::cout << "Remove (index = " << needle << "): ";
+	try
+	{
+		c.iremove(needle);
+		std::cout << "deleted  ";
+	}
+	catch (std::out_of_range)
+	{
+		std::cout << "index is too big  ";
+	}
+	print(c);
+
+	Container<int> cc = c;
+	std::cout << "Copying to container: ";
+	print(cc);
+
+	//////////////////////////////////////////////////////////////////////////
+
+	generate(example, 10);
+	needle = random(), needle2 = random();
+
+	std::cout << "\nTEST #2: POINTERS CONTAINER\n";
+	Container<int*> c2;
+	std::cout << "Adding: ";
+	for (int i = 0; i < 5; i++)
+		c2.add(example[i]), std::cout << *(c2[i]) << ' ';
+	for (int i = 5; i < 9; i++)
+	{
+		int* ptr = new int(example[i]);
+		c2.add(ptr), std::cout << *(c2[i]) << ' ';
+	}
+	int* ptr = new int(example[9]);
+	c2.add(ptr), std::cout << *(c2[9]) << '\n';
+	std::cout << "Set element [" << needle << "] to &" << needle2 << ": ";
+	try
+	{
+		c2[needle] = new int(needle2);
+		std::cout << "success  ";
+	}
+	catch (std::out_of_range)
+	{
+		std::cout << "index is too big  ";
+	}
+	print(c2);
+	std::cout << "Size: " << c2.size() << '\n';
+	std::cout << "Is empty: " << (c2.empty() ? "true" : "false") << '\n';
+
+	std::cout << "Find (value = " << needle << "): ";
+	index = c2.find(needle);
+	if (index != -1)
+		std::cout << "found on index " << index << '\n';
+	else
+		std::cout << "not found\n";
+	std::cout << "Find (pointer to element [9] = " << ptr << "): ";
+	index = c2.find(ptr);
+	if (index != -1)
+		std::cout << "found on index " << index << '\n';
+	else
+		std::cout << "not found" << (needle == 9 ? " (it is removed above)\n" : "\n");
+
+	std::cout << "Remove (value = " << needle2 << "): ";
+	try
+	{
+		c2.remove(needle2);
+		std::cout << "deleted  ";
+	}
+	catch (std::range_error)
+	{
+		std::cout << "not found  ";
+	}
+	print(c2);
+
+	std::cout << "Remove (index = " << needle << "): ";
+	try
+	{
+		c2.iremove(needle);
+		std::cout << "deleted  ";
+	}
+	catch (std::out_of_range)
+	{
+		std::cout << "index is too big  ";
+	}
+	print(c2);
+
+	Container<int*> cc2 = c2;
+	std::cout << "Copying to container: ";
+	print(cc2);
+	std::cout << "Let's modify: ";
+	try
+	{
+		*(cc2[0]) = random();
+		std::cout << "success  ";
+	}
+	catch (std::out_of_range)
+	{
+		std::cout << "index is unaccessible  ";
+	}
+	print(cc2);
+	std::cout << "Base container: ";
+	print(c2);
+}
+
+void generate(int* a, int count)
+{
+	srand((unsigned)time(0));
+	for (int i = 0; i < count; i++)
+		a[i] = random();
+}
+
+int random()
+{
+	return rand() % 11;
+}
+
+void print(Container<int> c)
+{
+	std::cout << "{ ";
+	for (int i = 0; i < c.size(); i++)
 		std::cout << c[i] << ' ';
-	std::cout << '\n';
+	std::cout << "}\n";
+}
+
+void print(Container<int*> c)
+{
+	std::cout << "{ ";
+	for (int i = 0; i < c.size(); i++)
+		std::cout << *(c[i]) << ' ';
+	std::cout << "}\n";
 }
