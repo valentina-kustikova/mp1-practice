60d503408d8d243d2b472fdccfcb8ff9466c4c11
Svetlana Molotkova <42940205+SMolotkova@users.noreply.github.com>
2019-05-16 09:19:45 +0300

    Delete Bin.h

--- Bin.h
+++
@@ -1,9 +0,0 @@
-#pragma once 
-#include <iostream> 
-using namespace std; 
-
-void Choose_Sort(int a[], int n); //сортировка выбором
-
-void Gen(int *a, int N, double max); // генерация массива 
-
-void Print(int *a, int N); //вывод массива
