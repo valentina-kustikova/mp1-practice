c842dca2ed9dc5d7ee0127c2ad3397a259ee5095
milenagladkaya <43129552+milenagladkaya@users.noreply.github.com>
2019-05-29 13:38:14 +0300

    practice7

--- Practice/Practice7/Source.cpp
+++ Practice/Practice7/Source.cpp
@@ -6,152 +6,152 @@ using namespace std;
 int main() 
 {
 
-	setlocale(LC_ALL, "Russian");
-	int d, c;
+    setlocale(LC_ALL, "Russian");
+    int d, c;
 
-	cout << "Введите размерность векторов v1 и v2: ";
-	do {
-		cin >> d;
-		if (d < 1)
-			cout << "Размерность должна быть больше 0. Попробуйте ввести число ещё раз." << endl;
-	} while (d < 1);
-	Vector v1(d);
-	Vector v2(d);
-	Vector v(d);
+    cout << "Введите размерность векторов v1 и v2: ";
+    do {
+        cin >> d;
+        if (d < 1)
+            cout << "Размерность должна быть больше 0. Попробуйте ввести число ещё раз." << endl;
+    } while (d < 1);
+    Vector v1(d);
+    Vector v2(d);
+    Vector v(d);
 
-	cout << "Введите ккординаты вектора v1:" << endl;
-	v1.Fill();
-	cout << "|v1| = ";
-	v1.Length();
+    cout << "Введите ккординаты вектора v1:" << endl;
+    v1.Fill();
+    cout << "|v1| = ";
+    v1.Length();
 
-	cout << "Введите ккординаты вектора v2:" << endl;
-	v2.Fill();
-	cout << "|v2| = ";
-	v2.Length();
+    cout << "Введите ккординаты вектора v2:" << endl;
+    v2.Fill();
+    cout << "|v2| = ";
+    v2.Length();
 
-	do{
-	int n;
-	Menu();
-	do {
-		cin >> n;
-		if((n < 1) || (n > 13))
-		cout << "Операции с таким номером нет. Попробуйте ввести ещё раз."<< endl;
-	} while ((n < 1) || (n > 13));
-	double dig;
-	switch (n)
-		{
-		case 1:
-		{
-			v = v1 + v2;
-			cout << "v = ( ";
-			v.Print();
-			cout << ")" << endl;
-			break;
-		}
-		case 2:
-		{
-			v = v1 - v2;
-			cout << "v = ( ";
-			v.Print();
-			cout << ")" << endl;
-			break;
-		}
-		case 3:
-		{
-			double scal = v1 * v2;
-			cout << "scal = " << scal << endl;
-			break;
-		}
-		case 4:
-		{
-			dig = Digital();
-			v = v1 + dig;
-			cout << "v = ( ";
-			v.Print();
-			cout << ")" << endl;
-			break;
-		}
-		case 5:
-		{
-			dig = Digital();
-			v = v1 - dig;
-			cout << "v = ( ";
-			v.Print();
-			cout << ")" << endl;
-			break;
-		}
-		case 6:
-		{
-			dig = Digital();
-			v = v1 * dig;
-			cout << "v = ( ";
-			v.Print();
-			cout << ")" << endl;
-			break;
-		}
-		case 7:
-		{
-			v1 += v2;
-			cout << "v1 = ( ";
-			v1.Print();
-			cout << ")" << endl;
-			break;
-		}
-		case 8:
-		{
-			v1 -= v2;
-			cout << "v1 = ( ";
-			v1.Print();
-			cout << ")" << endl;
-			break;
-		}
-		case 9:
-		{
-			v1 *= v2;
-			cout << "v1 = ( ";
-			v1.Print();
-			cout << ")" << endl;
-			break;
-		}
-		case 10:
-		{
-			dig = Digital();
-			v1 += dig;
-			cout << "v1 = ( ";
-			v1.Print();
-			cout << ")" << endl;
-			break;
-		}
-		case 11:
-		{
-			dig = Digital();
-			v1 -= dig;
-			cout << "v1 = ( ";
-			v1.Print();
-			cout << ")" << endl;
-			break;
-		}
-		case 12:
-		{
-			dig = Digital();
-			v1 *= dig;
-			cout << "v1 = ( ";
-			v1.Print();
-			cout << ")" << endl;
-			break;
-		}
-		case 13:
-		{
-			int i;
-			cout << "Введите индекс: ";
-			cin >> i;
-			cout << "v1["<< i << "] = " << v1[i] << endl;
-		}
-		}
-		cout << "Если вы хотите сделать ещё какие-то действия с векторами, введите 1" << endl;
-		cout << "Если нет, любое другое число." << endl;
-		cin >> c;
-	} while (c == 1);
+    do{
+    int n;
+    Menu();
+    do {
+        cin >> n;
+        if((n < 1) || (n > 13))
+        cout << "Операции с таким номером нет. Попробуйте ввести ещё раз."<< endl;
+    } while ((n < 1) || (n > 13));
+    double dig;
+    switch (n)
+        {
+        case 1:
+        {
+            v = v1 + v2;
+            cout << "v = ( ";
+            v.Print();
+            cout << ")" << endl;
+            break;
+        }
+        case 2:
+        {
+            v = v1 - v2;
+            cout << "v = ( ";
+            v.Print();
+            cout << ")" << endl;
+            break;
+        }
+        case 3:
+        {
+            double scal = v1 * v2;
+            cout << "scal = " << scal << endl;
+            break;
+        }
+        case 4:
+        {
+            dig = Digital();
+            v = v1 + dig;
+            cout << "v = ( ";
+            v.Print();
+            cout << ")" << endl;
+            break;
+        }
+        case 5:
+        {
+            dig = Digital();
+            v = v1 - dig;
+            cout << "v = ( ";
+            v.Print();
+            cout << ")" << endl;
+            break;
+        }
+        case 6:
+        {
+            dig = Digital();
+            v = v1 * dig;
+            cout << "v = ( ";
+            v.Print();
+            cout << ")" << endl;
+            break;
+        }
+        case 7:
+        {
+            v1 += v2;
+            cout << "v1 = ( ";
+            v1.Print();
+            cout << ")" << endl;
+            break;
+        }
+        case 8:
+        {
+            v1 -= v2;
+            cout << "v1 = ( ";
+            v1.Print();
+            cout << ")" << endl;
+            break;
+        }
+        case 9:
+        {
+            v1 *= v2;
+            cout << "v1 = ( ";
+            v1.Print();
+            cout << ")" << endl;
+            break;
+        }
+        case 10:
+        {
+            dig = Digital();
+            v1 += dig;
+            cout << "v1 = ( ";
+            v1.Print();
+            cout << ")" << endl;
+            break;
+        }
+        case 11:
+        {
+            dig = Digital();
+            v1 -= dig;
+            cout << "v1 = ( ";
+            v1.Print();
+            cout << ")" << endl;
+            break;
+        }
+        case 12:
+        {
+            dig = Digital();
+            v1 *= dig;
+            cout << "v1 = ( ";
+            v1.Print();
+            cout << ")" << endl;
+            break;
+        }
+        case 13:
+        {
+            int i;
+            cout << "Введите индекс: ";
+            cin >> i;
+            cout << "v1["<< i << "] = " << v1[i] << endl;
+        }
+        }
+        cout << "Если вы хотите сделать ещё какие-то действия с векторами, введите 1" << endl;
+        cout << "Если нет, любое другое число." << endl;
+        cin >> c;
+    } while (c == 1);
 
-	system("pause");
+    system("pause");
 }
--- Practice/Practice7/Vector.cpp
+++ Practice/Practice7/Vector.cpp
@@ -5,196 +5,196 @@ using namespace std;
 
 Vector::Vector()
 {
-	dim = 0;
-	x = NULL;
+    dim = 0;
+    x = NULL;
 }
 
 Vector::Vector(int _dim)
 {
-	dim = _dim;
-	x = new double[dim];
+    dim = _dim;
+    x = new double[dim];
 }
 
 Vector::Vector(const Vector& v)
 {
-	dim = v.dim;
-	x = new double[v.dim];
-	for (int i = 0; i < dim; i++)
-		x[i] = v.x[i];
+    dim = v.dim;
+    x = new double[v.dim];
+    for (int i = 0; i < dim; i++)
+        x[i] = v.x[i];
 }
 
 Vector::~Vector()
 {
-	dim = 0;
-	delete[] x;
+    dim = 0;
+    delete[] x;
 }
 
 Vector& Vector::operator= (const Vector& v)
 {
-	if (this != &v)
-	{
-		dim = v.dim;
-		delete[] x;
-		x = new double[dim];
-		for (int i = 0; i < dim; i++)
-			x[i] = v.x[i];
-	}
-	return *this;
+    if (this != &v)
+    {
+        dim = v.dim;
+        delete[] x;
+        x = new double[dim];
+        for (int i = 0; i < dim; i++)
+            x[i] = v.x[i];
+    }
+    return *this;
 }
 
 Vector Vector::operator+ (const Vector& v)
 {
-	Vector tmp(*this);
-	for (int i = 0; i < dim; i++)
-		tmp.x[i] += v.x[i];
-	return tmp;
+    Vector tmp(*this);
+    for (int i = 0; i < dim; i++)
+        tmp.x[i] += v.x[i];
+    return tmp;
 }
 
 Vector Vector::operator- (const Vector& v) 
 {
-	Vector tmp(*this);
-	for (int i = 0; i < dim; i++)
-		tmp.x[i] -= v.x[i];
-	return tmp;
+    Vector tmp(*this);
+    for (int i = 0; i < dim; i++)
+        tmp.x[i] -= v.x[i];
+    return tmp;
 }
 
 double Vector::operator* (const Vector& v)
 {
-	double scal = 0;
-	for (int i = 0; i < dim; i++)
-		scal += x[i] * v.x[i];
-	return scal;
+    double scal = 0;
+    for (int i = 0; i < dim; i++)
+        scal += x[i] * v.x[i];
+    return scal;
 }
 
 Vector Vector::operator+ (double dig)
 {
-	Vector v(*this);
-	for (int i = 0; i < dim; i++)
-		v.x[i] = x[i] + dig;
-	return v;
+    Vector v(*this);
+    for (int i = 0; i < dim; i++)
+        v.x[i] = x[i] + dig;
+    return v;
 }
 
 Vector Vector::operator- (double dig)
 {
-	Vector v(*this);
-	for (int i = 0; i < dim; i++)
-		v.x[i] = x[i] - dig;
-	return v;
+    Vector v(*this);
+    for (int i = 0; i < dim; i++)
+        v.x[i] = x[i] - dig;
+    return v;
 }
 
 Vector Vector::operator* (double dig)
 {
-	Vector v(*this);
-	for (int i = 0; i < dim; i++)
-		v.x[i] = x[i] * dig;
-	return v;
+    Vector v(*this);
+    for (int i = 0; i < dim; i++)
+        v.x[i] = x[i] * dig;
+    return v;
 }
 
 Vector& Vector::operator+= (const Vector & v)
 {
-	for (int i = 0; i < dim; i++)
-		x[i] += v.x[i];
-	return *this;
+    for (int i = 0; i < dim; i++)
+        x[i] += v.x[i];
+    return *this;
 }
 
 Vector& Vector::operator-= (const Vector & v)
 {
-	for (int i = 0; i < dim; i++)
-		x[i] -= v.x[i];
-	return *this;
+    for (int i = 0; i < dim; i++)
+        x[i] -= v.x[i];
+    return *this;
 }
 
 Vector& Vector::operator*= (const Vector & v)
 {
-	for (int i = 0; i < dim; i++)
-		x[i] *= v.x[i];
-	return *this;
+    for (int i = 0; i < dim; i++)
+        x[i] *= v.x[i];
+    return *this;
 }
 
 Vector& Vector::operator+= (double dig)
 {
-	for (int i = 0; i < dim; i++)
-		x[i] += dig;
-	return *this;
+    for (int i = 0; i < dim; i++)
+        x[i] += dig;
+    return *this;
 }
 
 Vector& Vector::operator-= (double dig)
 {
-	for (int i = 0; i < dim; i++)
-		x[i] -= dig;
-	return *this;
+    for (int i = 0; i < dim; i++)
+        x[i] -= dig;
+    return *this;
 }
 
 Vector& Vector::operator*= (double dig)
 {
-	for (int i = 0; i < dim; i++)
-		x[i] *= dig;
-	return *this;
+    for (int i = 0; i < dim; i++)
+        x[i] *= dig;
+    return *this;
 }
 
 double& Vector::operator[] (int idx)
 {
-	if ((idx < 0) || (idx >= dim))
-		cout << "Нет такого индекса. Попробуйте ввести индекс ещё раз.";
-	return x[idx];
+    if ((idx < 0) || (idx >= dim))
+        cout << "Нет такого индекса. Попробуйте ввести индекс ещё раз.";
+    return x[idx];
 }
 
 const double& Vector::operator[] (int idx)const
 {
-	do {
-		if ((idx < 0) || (idx >= dim))
-			cout << "Нет такого индекса. Попробуйте ввести индекс ещё раз.";
-	} while ((idx < 0) || (idx >= dim));
-	return x[idx];
+    do {
+        if ((idx < 0) || (idx >= dim))
+            cout << "Нет такого индекса. Попробуйте ввести индекс ещё раз.";
+    } while ((idx < 0) || (idx >= dim));
+    return x[idx];
 }
 
 void Vector::Length()
 {
-	double len = 0;
-	for (int i = 0; i < dim; i++)
-		len += x[i] * x[i];
-	len = sqrt(len);
-	cout << fixed;
-	cout.precision(3);
-	cout << len << endl << endl;
+    double len = 0;
+    for (int i = 0; i < dim; i++)
+        len += x[i] * x[i];
+    len = sqrt(len);
+    cout << fixed;
+    cout.precision(3);
+    cout << len << endl << endl;
 }
 
 void Vector::Fill()
 {
-	for (int i = 0; i < dim; i++) {
-		cin >> x[i];
-	}
+    for (int i = 0; i < dim; i++) {
+        cin >> x[i];
+    }
 }
 
 void  Vector::Print()
 {
-	for (int i = 0; i < dim; i++)
-		cout << x[i] << " ";
+    for (int i = 0; i < dim; i++)
+        cout << x[i] << " ";
 }
 
 void Menu()
 {
-	cout << "Для того чтобы выбрать операцию, которую вы хотите провести над векторами" << endl;
-	cout << "введите соответвсвующий номер операции из указанных ниже:" << endl;
-	cout << "1 - v = v1 + v2" << endl;
-	cout << "2 - v = v1 - v2" << endl;
-	cout << "3 - scal = (v1, v2)" << endl;
-	cout << "4 - v = v1 + dig" << endl;
-	cout << "5 - v = v1 - dig" << endl;
-	cout << "6 - v = v1 * dig" << endl;
-	cout << "7 - v1 += v2" << endl;
-	cout << "8 - v1 -= v2" << endl;
-	cout << "9 - v1 *= v2" << endl;
-	cout << "10 - v1 += dig" << endl;
-	cout << "11 - v1 -= dig" << endl;
-	cout << "12 - v1 *= dig" << endl;
-	cout << "13 - v1[i] = dig" << endl;
+    cout << "Для того чтобы выбрать операцию, которую вы хотите провести над векторами" << endl;
+    cout << "введите соответвсвующий номер операции из указанных ниже:" << endl;
+    cout << "1 - v = v1 + v2" << endl;
+    cout << "2 - v = v1 - v2" << endl;
+    cout << "3 - scal = (v1, v2)" << endl;
+    cout << "4 - v = v1 + dig" << endl;
+    cout << "5 - v = v1 - dig" << endl;
+    cout << "6 - v = v1 * dig" << endl;
+    cout << "7 - v1 += v2" << endl;
+    cout << "8 - v1 -= v2" << endl;
+    cout << "9 - v1 *= v2" << endl;
+    cout << "10 - v1 += dig" << endl;
+    cout << "11 - v1 -= dig" << endl;
+    cout << "12 - v1 *= dig" << endl;
+    cout << "13 - v1[i] = dig" << endl;
 }
 
 double Digital()
 {
-	double d;
-	cout << "Введите число" << endl;
-	cin >> d;
-	return d;
+    double d;
+    cout << "Введите число" << endl;
+    cin >> d;
+    return d;
 }
