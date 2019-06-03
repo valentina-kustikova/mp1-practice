589a878f608b11d1c912ec85a16a2ca7a10368e4
DmitryKizaev <d-kiz@yandex.ru>
2019-06-03 12:44:04 +0300

    +practice7

---
+++ kizaev_dd/Practice/Practice7/vector.h
@@ -0,0 +1,41 @@
+#pragma once
+
+class vect
+{
+	private:
+		int dim;
+		double* coord;
+	public:
+		vect ();
+		vect(int dim0);
+		vect(const vect& v0);
+		~vect();
+
+	vect operator+ (const vect& v) const;
+    vect operator- (const vect& v) const;
+	double operator* (const vect& v) const;
+
+    vect operator+ (double d) const;
+    vect operator- (double d) const;
+    vect operator* (double d) const;
+
+    const vect& operator= (const vect & v);
+
+    double& operator[] (int i);
+
+    double vect_len() const;
+
+    vect& operator+= (const vect & v);
+    vect& operator-= (const vect & v);
+    vect& operator+= (double d);
+    vect& operator-= (double d);
+
+    vect& operator*= (double d);
+    void *operator new[](size_t size);
+    void operator delete[] (void *p);
+
+    void vect_print () const;
+    void vect_read ();
+
+    int operator==(const vect & v) const;
+};	
