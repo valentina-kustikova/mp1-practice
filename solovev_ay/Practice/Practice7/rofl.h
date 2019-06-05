d7d4b2f60a2ac00d1ea403c2155b2ed6c68b02be
Alexandr-Solovev <43129356+Alexandr-Solovev@users.noreply.github.com>
2019-06-05 18:39:22 +0300

    upd

--- solovev_ay/Practice/Practice7/rofl.h
+++ solovev_ay/Practice/Practice7/rofl.h
@@ -38,6 +38,9 @@ public:
 	void operator*=(int k);
 	void operator*=(Vector a);
 	void Delete();
+	friend istream& operator>> (istream&, Vector&); // Ввод
+	friend ostream& operator<< (ostream&, const Vector&); // Вывод
+	const double& operator[](int) const;
 };
 
 struct exeption
