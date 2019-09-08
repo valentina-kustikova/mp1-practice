451525b1cebb488f9e26f6f2d90e2e84aca9c1e6
Alexandr-Solovev <43129356+Alexandr-Solovev@users.noreply.github.com>
2019-09-08 12:49:17 +0300

    upd

--- solovev_ay/Practice/Practice0/main.c
+++ solovev_ay/Practice/Practice0/main.c
@@ -1,62 +1,82 @@
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
+#include <time.h>
 #include <locale.h>
-    void main()
-    {
-	float x, x1, y, y1, r, r1, s = 0.0f;
+#define N 10
+void main (){
+	int randommas[N];
+	int b[10] = { 0 }; // массив для проверки неповторяемости 
+	int osnova[N]={0, 1, 2, 3, 4, 5, 6, 7, 8, 9};//ввел массив как базу для создания рандомных чисел
+	int otvet[N];
+	int i=0, kolvoznakov=0, kolvokorov=0, kolvobykov=0, x=100, y, z, score=0, p1, try1, try2;
 	setlocale(LC_ALL, "Russian");
- 	printf("Введите координаты вида (х:у) первой окружности.\n");
-	printf("Введите х: ");
-	scanf("%f",&x);
-	printf("Введите y: ");
-	scanf("%f",&y);
-	printf("Введите радиус первой окружности: ");
-	scanf("%f", &r);
-	if (r <= 0)
+	srand((unsigned int)time(0));
+	printf("Введите количество знаков от 1 до 10\n");
+	while((kolvoznakov < 1) || (kolvoznakov > 10))
+		scanf("%d", &kolvoznakov);//проверка на ввод количества знаков
+	printf("Компьютер загадал %d значное число!", kolvoznakov);
+	//генерирую случайный массив randommas
+	i=kolvoznakov;
+	while (i>0)
 	{
-	printf("Вы ввели неверные данные!\nТакого радиуса не может быть! \n");
-	return;
+		x = 10.0/RAND_MAX * rand();
+		if (i == kolvoznakov) 
+			x=9.0/RAND_MAX * rand() +1;
+		//со второго знака генерирую от 0 до 9
+		if (osnova[x] != -1) //выполняется при начале работы с еще не задействованным эл-том osnova
+		{
+			randommas[i] = osnova[x];
+			osnova[x]=-1;//принимаю рандомные значения в массив//исключаю повторы
+			i--;
+		} 
 	}
-	printf("Координаты первой окружности (%.4f:%.4f) радиус=%.4f\n", x, y, r);
-   	printf("Введите координаты вида (х:у) второй  окружности.\n");
-	printf("Введите х: ");
-	scanf("%f",&x1);
-	printf("Введите y: ");
-	scanf("%f",&y1);
-	printf("Введите радиус второй окружности: ");
-	scanf("%f", &r1);
-	if (r1 <= 0)
+	do {		
+		x = 0;			
+		printf("Введи число без повторяющихся цифр = ");
+		kolvobykov=0;
+		kolvokorov=0;//обнулил счетчики количества животных 
+		do //проверка вводимого числа
+		{
+			scanf("%d", &y);
+			p1 = y;					
+			for (i = 0; i < 10; i++)
+				b[i] = 0;
+				try1 = 0;
+			while (p1 != 0) //проверка на длину
+				{
+					b[p1 % 10]++;
+					p1 /= 10;
+					try1++;
+				}
+				try2 = 0;
+				for (i = 0; i < 10; i++) //проверка на неповторяемость
+					if (b[i] > 1)
+					try2++;
+		} while ((try1 != kolvoznakov) || (try2 != 0));
+		score++;//записываю число попыток
+		for (i = kolvoznakov; i >0; i--)
+		{
+			otvet[i] = y % 10;
+			y = y / 10;	
+		}//конвертирую вводимое пользователем число в массив вида 
+		for (i = kolvoznakov; i > 0; i--)
+		{
+			for (y = (i - 1); y > 0; y--)
+			{
+				if (otvet[i] == otvet[y]) x++;
+			}	
+		} while (x != 0);
+	for (i = kolvoznakov; i >0; i--) 
+	{ 
+	for (y = kolvoznakov; y >0 ; y--) 
 	{
-	printf("Вы ввели неверные данные!\nТакого радиуса не может быть!\n");
-	return;
+		if ((randommas[i] == otvet[y]) && (i == y)) 
+			kolvobykov++; //высчитываю быков
+		if ((randommas[i] == otvet[y]) && (i != y)) 
+			kolvokorov++; //высчитываю коров
+	} 
 	}
-	printf("Координаты второй окружности (%.4f:%.4f) радиус=%.4f\n", x1, y1, r1);
-	s=sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1));
-	if(s > (r + r1)) 
-	{
-	printf("Окружности не пересекаются!\n");
-	return;
+	printf("Коров = %d и Быков = %d \n", kolvokorov, kolvobykov);
+	}while (kolvobykov != kolvoznakov);
+	printf("Ты выиграл! Число попыток - %d!", score);
 	}
-	if((x == x1) && (y == y1) && (r == r1))
-	{
-	printf("Окружности совпадают!\n");
-	return;
-	}
-	if (s == (r + r1))
-	{
-	printf("Окружности касаются внешним образом один раз!\n");
-	return;
-	}
-	if ((s + r == r1) || (s + r1 == r))
-	{
-	printf("Окружности касаются внутренним образом!\n");
-	return;
-	}
-	if (((s + r) < r1)||((s + r1) < r))
-	{
-	printf("Одна окружность находится внутри другой!\n");
-	return;
-	}
- 	printf("Окружности пересекаются два раза!\n");
-	return;
-        } 
