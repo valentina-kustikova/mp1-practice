ca3f2c4eff61d28daedb6ef7970a458456285169
Alexandr-Solovev <43129356+Alexandr-Solovev@users.noreply.github.com>
2019-09-08 13:06:49 +0300

    upd

--- solovev_ay/Practice/Practice6/Header.h
+++ solovev_ay/Practice/Practice6/Header.h
@@ -3,13 +3,13 @@
 typedef struct
 {
 	int n;
-	int* elements;
+	float* elements;
 } VECTOR;
 void Output(VECTOR a);
 VECTOR  input(int _n);
 VECTOR Sum(VECTOR a, VECTOR b);
 VECTOR roflan(VECTOR a, VECTOR b);//-
-void skalyar(VECTOR a, VECTOR b);
-void length(VECTOR a);
-void corner(VECTOR a, VECTOR b);
+int skalyar(VECTOR a, VECTOR b);
+float length(VECTOR a);
+float corner(VECTOR a, VECTOR b);
 int Delete(VECTOR a);
