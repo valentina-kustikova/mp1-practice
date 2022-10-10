1d3f261b3b522fea53158f9a0b5a622bf83e8643
ElizavetaFrolova <liza1827frol@gmail.com>
2022-10-10 23:36:57 +0300

    practice1

--- FrolovaEA/Practice1/Practice1/main.c
+++ FrolovaEA/Practice1/Practice1/main.c
@@ -1,15 +1,56 @@
-#include <stdio.h>
-#include <locale.h>
-#include <math.h>
+#include<stdio.h>
+#include<math.h>
 
 int main()
-{	
-	setlocale(LC_ALL, "rus");
-	float h, w, ;
-	printf("Введите высоту шкафа h: ");
-	scanf("%f", &h);
-	if ((h > 180) && (h < 220))
+{
+	float h, w, d, q_dsp = 725, q_dvp = 850, q_wood = 700, k, t_dsp = 0.0015, t_dvp = 0.005, t_wood = 0.01, v1, v2, v3, v4, v5, v_dsp, m_dvp, m_dsp, m_wood, m;
+	
+
+	printf("Enter the height of the cabinet in meters: ");
+	scanf_s("%f", &h);
+
+	printf("\nEnter the width of the cabinet in meters: ");
+	scanf_s("%f",&w);
+
+	printf("\nEnter the depth of the cabinet in meters: ");
+	scanf_s("%f",&d);
+
+	
+	if ((h < 1.8) || (h > 2.2))
 	{
-		printf("Корректные данные")
+		printf("Incorrect height, 1.8<=height>=2.2");
+		return 1;
 	}
+
+	if ((w < 0.8) || (w > 1.2))
+	{
+		printf("Incorrect width, 0.8<=width>=1.2");
+		return 1;
+	}
+
+	if ((d < 0.5) || (d > 0.9))
+	{
+		printf("Incorrect depth, 0.5<=depth>=0.9");
+		return 1;
+	}
+
+	k = (int)(h / 0.4);
+	v1 = h * w * t_dvp;
+	v2 = 2 * h * d * t_dsp;
+	v3 = 2 * w * d * t_dsp;
+	v4 = 2 * w * h * t_wood;
+	v5 = k * ( w - 2 * t_dsp ) * d * t_dsp;
+	v_dsp = v2 + v3 + v5;
+	m_dvp = v1 * q_dvp;
+	m_dsp =(v2 + v3 + v5 ) * q_dsp;
+	m_wood = v4 * q_wood;
+	m = m_dvp + m_dsp + m_wood;
+
+	printf("%f", m);
+
+		return 0;
+
+
+	
+	
 }
