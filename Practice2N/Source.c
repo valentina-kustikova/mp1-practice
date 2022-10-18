2e10da8737d7b36f9aad589cd55c5bd2be28fbaa
Kamil-Fatekhov <frister22@yandex.ru>
2022-10-18 23:25:49 +0300

    Initial structure

---
+++ Practice2N/Source.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+int main()
+{
+	int a = 1, n, d = 0, up = 1000, c, s, i,count=0;
+	char symbol;
+	printf("\tHi, dear friend. Let`s play the game\n");
+	do
+	{
+		printf("\tChoose the game mode:\n\tPress 1 if you want to make a number\n\tPress 2 if you want to search the hidden number\t");
+		scanf_s("%d", &c);
+	} while (c != 1 && c != 2);
+	switch (c)
+	{
+	case 1:
+
+		do
+		{
+			printf("Input number from 0 to 1000:\t");
+			scanf_s("%d", &n);
+		} while (n < 0 || n>1000);
+		srand(time(NULL));
+		a = rand() % 1000;
+		printf("Is it\t%d?\n", a);
+		printf("\tInput <,>,=\t");
+		do
+		{
+			scanf_s("%c", &symbol);
+			switch (symbol)
+			{
+			case '=':
+				printf("The program has found the number.\t It`s:%d", a); break;
+
+			case '>':
+
+				srand(time(NULL));
+				d = a;
+				a = (d + 1) + rand() % (up - d + 1);
+				printf("Is it:%d?\t", a);
+				printf("%d\t%d\t", d, up); break;
+
+				up = 1000;
+			case'<':
+				i = up;
+
+				srand(time(NULL));
+				up = a;
+				a = d + rand() % (a - d + 1);
+				i = a;
+				printf("Is it:%d\t", a);
+				printf("%d\t%d\t", d, up); break;
+
+				
+			}
+			count++;
+		} while (symbol != '=');
+		printf("Attemps = %d", count);
+	
+	case 2:
+		printf("Input the number\t");
+		do
+		{
+			scanf_s(" % d", &n);
+			if (n > a)
+			{
+				printf("The number is less");
+				return;
+			}
+			if (n < a)
+			{
+				printf("The number is much");
+				return;
+			}
+			if (n = a);
+			{
+				printf("Congratulations! You`re win!");
+				printf("Attemps =\t%d", count);
+				return;
+			}
+			count++;
+		} while (n != a);
+
+	}
+
+		return 0;
+	
+}
