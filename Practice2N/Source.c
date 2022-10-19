a6d9dc826fbd679ef4183ace181483779ddcb3fb
Kamil Fatekhov <113035726+Kamil-Fatekhov@users.noreply.github.com>
2022-10-19 16:55:02 +0300

    Update Source.c

--- Practice2N/Source.c
+++ Practice2N/Source.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 int main()
 {
-	int a = 1, n, d = 0, up = 1000, c, s, i,count=0;
+	int a, n, d = 0, up = 1000, c, s, i, count = 0;
 	char symbol;
+	srand(time(NULL));
 	printf("\tHi, dear friend. Let`s play the game\n");
 	do
 	{
@@ -17,6 +20,7 @@ int main()
 		{
 			printf("Input number from 0 to 1000:\t");
 			scanf_s("%d", &n);
+			getchar();
 		} while (n < 0 || n>1000);
 		srand(time(NULL));
 		a = rand() % 1000;
@@ -32,7 +36,7 @@ int main()
 
 			case '>':
 
-				srand(time(NULL));
+				
 				d = a;
 				a = (d + 1) + rand() % (up - d + 1);
 				printf("Is it:%d?\t", a);
@@ -42,45 +46,47 @@ int main()
 			case'<':
 				i = up;
 
-				srand(time(NULL));
+				
 				up = a;
 				a = d + rand() % (a - d + 1);
 				i = a;
 				printf("Is it:%d\t", a);
 				printf("%d\t%d\t", d, up); break;
 
-				
+
 			}
 			count++;
 		} while (symbol != '=');
 		printf("Attemps = %d", count);
-	
+
 	case 2:
+		
+		a = rand() % 1000;
 		printf("Input the number\t");
 		do
 		{
-			scanf_s(" % d", &n);
+			scanf_s("%d", &n);
 			if (n > a)
 			{
-				printf("The number is less");
-				return;
+				printf("The number is less\t");
+				
 			}
-			if (n < a)
+			else if (n < a)
 			{
-				printf("The number is much");
-				return;
+				printf("The number is much\t");
+				
 			}
-			if (n = a);
+			 else 
 			{
 				printf("Congratulations! You`re win!");
 				printf("Attemps =\t%d", count);
-				return;
+				
 			}
 			count++;
 		} while (n != a);
 
 	}
 
-		return 0;
-	
+	return 0;
+
 }
