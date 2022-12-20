e40cf9ff9db5be715b5ab8c749697b743d8ec967
lyapin-nikita <paladins392@gmail.com>
2022-12-20 23:51:14 +0300

    Practice1_20.12.2022

--- P1_Shkaf/P1_Shkaf/P1_Shkaf.c
+++ P1_Shkaf/P1_Shkaf/P1_Shkaf.c
@@ -1,27 +1,31 @@
 #include <stdio.h>
 
-#define dcp 0.0008
-#define dvp 0.0007
-#define wood 0.00065
+#define dcp 800
+#define dvp 700
+#define wood 650
 
 void main() {
-	float h, b, d, m, m_backwall, m_sidewall, m_topwall, m_doors, m_shelfs;
+	float h, w, d, t_dsp = 0.0015, t_dvp = 0.005, t_wood = 0.01, v1, v2, v3, v4, v5, v_dsp, m_dvp, m_dsp, m_wood, m;
 	int k;
-
 	do { 
-		printf("\n\tEnter the height(180-220cm), width(80-120cm), deepth(50-90cm)\n"); 
-		scanf_s("%f%f%f", &h, &b, &d);
-	} while ((h < 180) || (h > 220) || (b < 80) || (b > 120) || (d < 50) || (d > 90));
+		printf("\n\tEnter the height(1.80-2.20m), width(0.80-1.20m), deepth(0.50-0.90m)\n"); 
+		scanf_s("%f%f%f", &h, &w, &d);
+	} while ((h < 1.8) || (h > 2.2) || (w < 0.8) || (w > 1.2) || (d < 0.5) || (d > 0.9));
 
-	if (h <= 200) k = 4;
+	if (h <= 2) k = 4;
 	else k = 5;
 
-	m_backwall = dvp * h * b * 0.5;
-	m_sidewall = dcp * h * d * 1.5;
-	m_topwall = dcp * b * d * 1.5;
-	m_doors = wood * b * h;
-	m_shelfs = k * dcp * b * d;
-	m = m_backwall + 2 * m_sidewall + 2 * m_topwall + m_doors + m_shelfs;
+	v1 = h * w * t_dvp;
+	v2 = 2 * h * d * t_dsp;
+	v3 = 2 * ( w - 2 * t_dsp ) * d * t_dsp;
+	v4 = 2 * w * h * t_wood;
+	v5 = k * ( w - 2 * t_dsp ) * d * t_dsp;
+	v_dsp = v2 + v3 + v5;
+	m_dvp = v1 * dvp;
+	m_dsp =(v2 + v3 + v5 ) * dcp;
+	m_wood = v4 * wood;
+	m = m_dvp + m_dsp + m_wood;
+
 
 	printf("Weight is %.0f ", m);
 	printf("kg\n");
