571db38665116f5696ddd9219f2d3a7e7c56356c
pipidaster2004 <chebureknabite@gmail.com>
2023-02-15 19:51:37 +0300

    who?

--- KhokhlovAD/Practice2_0/Practice2_0/matrix.h
+++ KhokhlovAD/Practice2_0/Practice2_0/matrix.h
@@ -11,11 +11,11 @@ typedef struct
 void allocate_matrix(TMatrix** matrix, int n);
 void free_matrix(TMatrix** matrix);
 
-void fill_matrix(TMatrix* matrix);
+void fill_matrix(TMatrix* matrix, int n);
 void print_matrix(TMatrix* matrix, int n);
 
-TMatrix* add_matrix(TMatrix* matrix1, TMatrix* matrix2);
+TMatrix* add_matrix(TMatrix* matrix1, TMatrix* matrix2, int n);
 TMatrix* add_const(TMatrix* matrix, int n);
 TMatrix* multi_const(TMatrix* matrix, int n);
-TMatrix* multi_matrix(TMatrix* matrix1, TMatrix* matrix2);
+TMatrix* multi_matrix(TMatrix* matrix1, TMatrix* matrix2, int n);
 #endif 
