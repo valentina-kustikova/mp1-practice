5780711a432624e5d8424c658aff17b949e09aee
Lyapin Nikita <paladins392@gmail.com>
2023-04-27 19:54:11 +0300

    27.04

--- LyapinNA/Practice2_2cpp/Practice2_2cpp/func.h
+++ LyapinNA/Practice2_2cpp/Practice2_2cpp/func.h
@@ -10,7 +10,6 @@ using namespace std;
 
 
 
-
 struct vacancy {
 	string employee;
 	string nameCompany;
@@ -23,11 +22,23 @@ struct vacancy {
 };
 
 
+struct vacancyLib {
+	int countVacancy;
+	vacancy* Vacancy;
+
+
+	vacancyLib();
+	vacancyLib(vacancy* vac, int count);
+	vacancyLib(const string& path);
+	vacancyLib(const vacancyLib& copy_vac);
+	~vacancyLib();
+};
 
 
 
-ifstream read_list(string path); 
+string input_path();
+ifstream read_list(const string path); 
 int count_vacancy(ifstream& read);
 vacancy* fill_struct(ifstream& read, int countVacancy);
-vacancy* search_vacancy(vacancy* Vacancy, int countVacancy);
-void print_info(vacancy* Vacancy);
+vacancyLib search_vacancy(vacancyLib Vacancy);
+void print_info(vacancyLib VacancyLib);
--- LyapinNA/Practice2_2cpp/Practice2_2cpp/maincode.cpp
+++ LyapinNA/Practice2_2cpp/Practice2_2cpp/maincode.cpp
@@ -2,27 +2,14 @@
 
 int main() {
 	system("chcp 1251");
-	  
-
-
-	int countVacancy = 0;
-	string path = "list.txt";
-	ifstream read;
-
-
-
-
-	//Функции открытия потока, подсчета вакансий и заполнения структур 
-	read = read_list(path);
-	countVacancy = count_vacancy(read);
-	vacancy* Vacancy = new vacancy[countVacancy];
-	Vacancy = fill_struct(read, countVacancy);	
-
+	  	
+	string path;
+	path = input_path();
+	vacancyLib Vacancy(path);
 	
-
 	//Поиск по названию
-	vacancy* srchVacancy;
-	if (!(srchVacancy = search_vacancy(Vacancy, countVacancy))) {
+	vacancyLib srchVacancy = search_vacancy(Vacancy);
+	if (srchVacancy.countVacancy == 0) {
 		cerr << "Вакансий не было найдено!";
 		return 1;
 	}
@@ -31,9 +18,6 @@ int main() {
 	print_info(srchVacancy);
 
 
-	read.close();
-	delete[] srchVacancy;
-	delete [] Vacancy;
 	system("pause");
 	return 0;
 }
