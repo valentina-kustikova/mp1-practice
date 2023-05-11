83b6d19958e3e8a2f3de972a5bb70467b270ecec
itmm-y23b <itmm-y23b@E.local>
2023-05-11 09:27:19 +0300

    11.05

--- LyapinNA/Practice2_3/Practice2_3/class.h
+++ LyapinNA/Practice2_3/Practice2_3/class.h
@@ -14,7 +14,6 @@ public:
 	vacancy();
 
 	string getInfoVacancy(int select);
-
 	friend ostream& operator << (ostream& os, const vacancy& vac);
 	friend istream& operator >> (istream& is, vacancy& vac);
 };
@@ -27,23 +26,21 @@ class vacancyLib {
 private: 
 	vacancy* Vacancy;
 	int count;
+	vacancy* fill_from_txt(ifstream& stream, int countVacancy);
+	ifstream read_list(const string& path);
+	int count_vacancy(ifstream& stream);
+
 public:
 	vacancyLib();
 	vacancyLib(const string& path);
 	vacancyLib(vacancy* OutVacancy, int OutCount);
 	vacancyLib(const vacancyLib& vac_copy);
 	~vacancyLib();
-	vacancyLib& operator= (const vacancyLib& cp_vac);
-
-	vacancy* fill_from_txt(ifstream& stream, int countVacancy);
-	ifstream read_list(const string& path);
-	int count_vacancy(ifstream& stream);
 	vacancyLib search_vacancy();
 
+	vacancyLib& operator= (const vacancyLib& cp_vac);
 	friend ostream& operator << (ostream& os, const vacancyLib& vacLib);
 };
-
-
 
 
 
