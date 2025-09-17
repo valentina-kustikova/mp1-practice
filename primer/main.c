62381d82f4fb7d8fe0dfaf3f3101b71ba105348e
Alexander-Ivashchenko <sasaivasenko154@gmail.com>
2025-09-17 15:50:50 +0300

    hello sample

--- primer/main.c
+++ primer/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main() {
-  int a = 1, b = 3;
-    printf("Sum = %d" , a + b);
+  float V1, V2, S, T;
+  scanf_s("%f, %f, %f, %f", &V1, &V2, &S, &T);
+  printf("S=%f", S + (V1 + V2) * T);
   return 0;
 }
