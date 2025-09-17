dd7345abf80dd8ee37e016bbf223c0707195e50a
Sergey-Eroshkin <seregaeroschkin@mail.ru>
2025-09-17 15:48:45 +0300

    Solve of triangle problem

--- Project1/main.c
+++ Project1/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
 int main() {
-	int a, b;
-	scanf_s("%d %d", &a, &b);
-	printf("%d", a + b);
+	float a, b, c;
+	scanf_s("%d %d %d", &a, &b, &c);
+	if (a < 0 || b < 0 || c < 0) {
+		printf("Incorrect input values!");
+		return 1;
+	}
+	if (a >= b + c || b >= a + c || c >= a + b) {
+		printf("Triangle doesn't exist");
+		return 1;
+	}
+	if (a == b && b == c) {
+		printf("Equilateral triangle");
+		return 0;
+	}
+	if (a == b || b == c || c == a) {
+		printf("Isosceles triangle");
+		return 0;
+	}
+	printf("Regular triangle");
 	return 0;
 }
