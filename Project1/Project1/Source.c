15552bee690d9d828807aabbafa65d17f4022508
Yaroslav-Vasiliev <yarvasiliev@gmail.com>
2025-09-17 15:50:02 +0300

    rrdva

--- Project1/Project1/Source.c
+++ Project1/Project1/Source.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <math.h>
 int main()
 {
-  int a, b;
-  scanf_s("%d %d", &a, &b);
-  printf("%d", a + b);
+  float v1, v2, s, t;
+  scanf_s("%f %f %f %f", &v1, &v2, &s, &t);
+  printf("%f", fabs(s-t*(v1+v2)));
   return 0;
 }
