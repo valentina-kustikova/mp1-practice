f232e60ca70008b4408a7634fae35460dcd64114
VIL0N <vlad784876@gmail.com>
2025-09-17 15:48:26 +0300

    СТАТУС

--- Project X/Project X/Source.c
+++ Project X/Project X/Source.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 int main() {
 	float a, b, c;
-	scanf_s("%f %f", &a, &b);
-	c = a + b;
-	printf("%f", c);
+	scanf_s("%f %f %f", &a, &b, &c);
+	printf("s = %.0f", a * b * c);
 	return 0;
 }
