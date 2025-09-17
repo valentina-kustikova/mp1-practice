f7b316530306f250678908ec6ddc8d8769b1832c
annazavarzina <zavarzinaana218@gmail.com>
2025-09-17 15:49:57 +0300

    hello

--- Pr/Pr/Source.c
+++ Pr/Pr/Source.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
-#include <math.h>
+
 int main() {
-	float a = 2, b = 3, c;
-	printf("c=%f", a + b);
+	float v1, v2, s, t;
+	scanf_s("%f%f%f%f", &v1, &v2, &s, &t);
+	printf("s=%f", s+(v1+v2)*t);
 	return 0;
 }
