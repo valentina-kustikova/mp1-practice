6d32d3b829db3783cd8124a835269b852a58bb48
MichaelIvanckov <m.i.ivanckov@gmail.com>
2025-09-17 15:49:33 +0300

    My second commit

--- Sample1/Sample1/Mane.c
+++ Sample1/Sample1/Mane.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 int main() {
-	int a, b;
-	//scanf("%d %d", &a, &b);
-	a = 1;
-	b = 1;
-	printf("Summ = %d", a + b);
+	int a, b, S, T;
+	scanf_s("%d %d %d %d", &a, &b, &S, &T);
+	printf("S = %d", S + (a + b)*T);
 	return 0;
 }
