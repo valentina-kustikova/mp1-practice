266bd0e892bce640c968f112a6a55315784efaec
VladislavAvdeev07 <Vlad.Avdeev07@mail.ru>
2025-09-17 15:49:31 +0300

    Init ssample

--- ssample/ssample/Main.c
+++ ssample/ssample/Main.c
@@ -1,7 +1,7 @@
 # include <stdio.h>
 int main() {
-  int a, b;
-  scanf_s("%d%d",&a,&b);
-  printf("sum=%d", a + b);
+  float V1, V2, S, T;
+  scanf_s("%f%f%f%f", &V1, &V2, &S, &T);
+  printf("s=%f", S+(V1+V2)*T);
   return 0;
 }
