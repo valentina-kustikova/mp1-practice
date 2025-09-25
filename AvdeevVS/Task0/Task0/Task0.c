b9c61210f03ab8853b636e67a5b18d203fba6ae3
VladislavAvdeev07 <Vlad.Avdeev07@mail.ru>
2025-09-25 22:10:26 +0300

    Init Lab0

--- AvdeevVS/Task0/Task0/Task0.c
+++ AvdeevVS/Task0/Task0/Task0.c
@@ -2,34 +2,37 @@
 #include <math.h>
 int main() {
   float x1, x2, y1, y2, r1, r2;
-  scanf_s("%f%f%f%f%f%f", &x1, &y1, &r1, &x2, &y2, &r2);
+  scanf("%f%f%f%f%f%f", &x1, &y1, &r1, &x2, &y2, &r2);
   float s = sqrtf((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
   if ((r1 <= 0) || (r2 <= 0)) {
-    printf("necorr radius");
+    printf("nekorrektnyy radius");
     return 1;
   }
   if ((r1 == r2) && (x1 == x2) && (y1 == y2)) {
-    printf("sovpadayut");
+    printf("sovpadayut ---- Obshchikh tochek: beskontchnost");
     return 0;
   }
   if ((r1 + r2) < s) {
-    printf("ne peresekautsa");
+    printf("ne peresekayutsya ---- Obshchikh tochek: 0");
     return 0;
   }
   if ((r1 + r2) == s) {
-    printf("casanie vnesh");
+    printf("kasayutsya vneshne ---- Obshchikh tochek: 1");
     return 0;
   }
   if (fabs(r1 - r2) == s) {
-    printf("casanie vnutr");
+    printf("kasayutsya vnutrenne ---- Obshchikh tochek: 1");
     return 0;
   }
   if ((s < (r1 + r2)) && (s > fabs(r1 - r2))) {
-    printf("peresechenye");
+    printf("peresekayutsya ---- Obshchikh tochek: 2");
     return 0;
   }
   if (s < fabs(r1 - r2)) {
-    printf("odna v drugoi bez casanii");
+    printf("odna v drugoy bez kasaniy ---- Obshchikh tochek: 0");
+    if ((x1 == x2) && (y1 == y2)) {
+      printf("Kontsentricheskie okruzhnosti");
+    }
     return 0;
   }
   return 0;
