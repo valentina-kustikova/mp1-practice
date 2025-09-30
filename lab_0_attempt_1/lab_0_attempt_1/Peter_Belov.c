84987cad9c96a46ab4919e1979e366fba62b213c
PeterBelov27 <ptr_chernov@inbox.ru>
2025-09-30 16:51:07 +0300

    Сделал lab0 Белов Пётр

---
+++ lab_0_attempt_1/lab_0_attempt_1/Peter_Belov.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+	double x1, y1, r1;
+	double x2, y2, r2;
+	double distance;
+
+	printf("Введите координаты x1 и y1 и радиус r1: ");
+	scanf_s("%lf %lf %lf", &x1, &y1, &r1);
+
+	printf("Введите координаты x2 и y2 и радиус r2: ");
+	scanf_s("%lf %lf %lf", &x2, &y2, &r2);
+
+	if (r1 < 0 || r2 < 0) {
+		printf("Радиус должен быть положительным");
+		return 1;
+	}
+
+	distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+
+	if (distance > r1 + r2) {
+		printf("Окружности не пересекаются\n");
+	}
+	else if (distance < fabs(r1 - r2)) {
+		printf("Одна окружность внутри другой\n");
+	}
+	else if (distance == 0 && r1 == r2) {
+		printf("Окружности совпадают\n");
+	}
+
+	else if (distance == r1 + r2 || distance == fabs(r1 - r2)) {
+		printf("Окружности имеют одну точку касания\n");
+	}
+
+	else {
+		printf("Окружности пересекаются\n");
+	}
+	return 0;
+}
