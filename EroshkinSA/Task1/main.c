a2e517b94f06d0458031c296b5a30d5c89db8a85
Sergey-Eroshkin <seregaeroschkin@mail.ru>
2025-10-05 20:23:01 +0300

    Lab1 First version

---
+++ EroshkinSA/Task1/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+	float h, w, d, dsp = 1.5f, dvp = 0.5f, tree = 1.0f,
+		v_back, v_roofs, v_sides, v_shelfs = 0, v_doors,
+		m_dsp, m_dvp, m_tree, m, current_height = 0;
+	const float density_dsp = 750 / 1e6, density_dvp = 850 / 1e6, density_tree = 720 / 1e6;
+	do {
+		scanf_s("%f %f %f", &h, &w, &d);
+	} while (h < 180 || h > 210 || w < 80 || w > 120 || d < 50 || d > 90);
+	v_back = h * w * dvp;
+	v_roofs = 2 * h * d * dsp;
+	v_sides = 2 * w * d * dsp;
+	while (current_height + 41.5f <= h) {
+		v_shelfs += (w - 3) * d * dsp;
+		current_height += 41.5f;
+	}
+	v_doors = w * h * tree;
+	printf("back = %f \nroofs = %f \nsides = %f \nshelfs = %f \ndoors = %f\n", v_back, v_roofs, v_sides, v_shelfs, v_doors);
+	m_dvp = v_back * density_dvp;
+	m_dsp = (v_roofs + v_sides + v_shelfs) * density_dsp;
+	m_tree = v_doors * density_tree;
+	printf("dvp = %f \ndsp = %f \ntree = %f\n", m_dvp, m_dsp, m_tree);
+	m = m_dvp + m_dsp + m_tree;
+	printf("Mass of the closet = %f kg", m);
+	return 0;
+}
