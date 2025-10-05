90a4a72205c4e3b244371765b3b864e4aa328f46
Tensety <smpaveel@gmail.com>
2025-10-06 01:16:16 +0300

    Lab task number 1

---
+++ Lab2/Wardrobes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main() {
+	int dvp = 850, dsp = 700, oakDensity = 700;
+	float h, w, d, hm, wm, dm, shelves;
+
+	do {
+		printf("Enter correct height h: ");
+		scanf_s("%f", &h);
+	} while (h < 180 || h > 250);
+
+
+	do {
+		printf("Enter correct width w: ");
+		scanf_s("%f", &w);
+	} while (w < 80 || w > 120);
+
+
+	do {
+		printf("Enter correct depth d: ");
+		scanf_s("%f", &d);
+	} while (d < 50 || d > 90);
+
+	// Перевод в СИ
+	
+	hm = h / 100;
+	wm = w / 100;
+	dm = d / 100;
+	
+	shelves = (int)(h) / 40;
+
+	printf("Масса шкафа в килограммах: %f", (dvp * hm * wm * 0.005) + (2 * dsp * hm * dm * 0.015) + (2 * dsp * wm * dm * 0.015) + (oakDensity * hm * wm * 0.01) + (wm * dm * 0.015 * shelves * dsp));
+
+
+
+
+}
