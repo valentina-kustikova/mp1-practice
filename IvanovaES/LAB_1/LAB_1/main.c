616cf2cee015aab2b109a3906872f4b790133e5c
elenaa-gh <hhobana@yandex.ru>
2025-10-06 13:35:27 +0300

    fix dir

--- IvanovaES/LAB_1/LAB_1/main.c
+++ IvanovaES/LAB_1/LAB_1/main.c
@@ -4,35 +4,36 @@
 
 int main() {
 	float h, w, d;
+	float p_dsp = 735.f;
+	float p_dvp = 800.f;
+	float p_der = 700.f;
+	float tol_5 = 0.005f;
+	float tol_15 = 0.015f;
+	float tol_10 = 0.01f;
+
 	setlocale(LC_ALL, "RUS");
+	
 	do {
 		scanf_s("%f %f %f", &h, &w, &d);
-	} while ((h > 220 | h < 180) | (w > 120 | w < 80) | (d > 90 | d < 50));
-	
-	const float p_dsp = 735;
-	const float p_dvp = 800;
-	const float p_der = 700;
-	const float tol_5 = 0.005;
-	const float tol_15 = 0.015;
-	const float tol_10 = 0.01;
+	} while ((h > 220.f || h < 180.f) || (w > 120.f || w < 80.f) || (d > 90.f || d < 50.f));
 
 	int kol_pol;
 	if (fmod(h, 40) == 0) {
-		kol_pol = (h / 40) - 1;
+		kol_pol = (int)((h / 40) - 1);
 	}
 	else {
-		kol_pol = floor(h / 40);
+		kol_pol = (int)floor(h / 40.f);
 	}
 
-	h = h / 100.0;
-	w = w / 100.0;
-	d = d / 100.0;
+	h = h / 100.0f;
+	w = w / 100.0f;
+	d = d / 100.0f;
 	
 	float V_zs = h * w * tol_5;
-	float V_bok = 2 * (h * d * tol_15);
-	float V_kr = 2 * (w * d * tol_15);
+	float V_bok = 2.0f * (h * d * tol_15);
+	float V_kr = 2.0f * (w * d * tol_15);
 	float V_dv = h * w * tol_10;
-	float V_pol = kol_pol * (w * d * tol_15);
+	float V_pol = kol_pol * ((w - 2.f * tol_15) * d * tol_15);
     
 	float massa = (V_zs * p_dvp) + (V_bok * p_dsp) + (V_kr * p_dsp) + (V_dv * p_der) + (V_pol * p_dsp);
 
