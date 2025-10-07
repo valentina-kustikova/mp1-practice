9c82c58f38d970a2851f10f9f1d68afd4184a020
Yaroslav-Vasiliev <yarvasiliev@gmail.com>
2025-10-07 19:57:31 +0300

    compl

--- VasilievYS/Task1/Task1/Source1.c
+++ VasilievYS/Task1/Task1/Source1.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
+#include <locale.h>
 int main()
 {
-	float m, wPol, w, h, tDvp=0.5, tDsp=1.5, tDer=1, d; //в см
+	float m, wPol, w, h, tDvp = 0.5, tDsp = 1.5, tDer = 1.0, d; //в см
 	float pDer = 0.72, pDsp = 1.3, pDvp = 0.85; //в г/см3
 	int cPol;
+	setlocale(LC_ALL, "RUS");
 	do
 	{
-		printf("Введите высоту(180-220см), ширину(80-120см), глубину(50-90см).");
-		scanf("%f %f %f", h, w, d);
+		printf("Введите высоту(180-220см), ширину(80-120см), глубину(50-90см): ");
+		scanf_s("%f %f %f", &h, &w, &d);
 	} while ((h >= 180 && h <= 220) && (w >= 80 && w <= 120) && (d >= 50 && h <= 90));
-	if ((h % 40) == 0)
+	if (((int)h % 40) == 0)
 	{
-		cPol = (h / 40)-1;
+		cPol = (h / 40) - 1;
 	}
 	else
 	{
@@ -20,6 +22,6 @@ int main()
 	}
 	wPol = w - 2 * tDsp;
 	m = (pDvp * h * w * tDvp) + (pDsp * h * d * tDsp * 2) + (pDsp * w * d * tDsp * 2) + (pDer * h * w * tDer) + (pDsp * d * wPol * tDsp * cPol);
-	printf("Масса шкафа = %f", m);
+	printf("Масса шкафа = %f кг", m/1000);
 	return 0;
 }
