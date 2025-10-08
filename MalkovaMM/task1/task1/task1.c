ce98f084e5618c8724ef5c81622b4d95eeb2e6bc
mashamalkova <masha.malkova.2007@yandex.ru>
2025-10-08 15:19:07 +0300

    task1

--- MalkovaMM/task1/task1/task1.c
+++ MalkovaMM/task1/task1/task1.c
@@ -3,25 +3,25 @@
 
 int main()
 {
-	setlocale(LC_ALL, "");
+	setlocale(LC_ALL, "Rus");
 	float h, w, d, x;
 	float m, dvp, dsp, der;
 	do
 	{
 		printf("Введите высоту шкафа 180-220 см: ");
 		scanf_s("%f", &h);
-	} while ((h < 180) || (h > 220));
+	} while ((h < 180.f) || (h > 220.f));
 	do
 	{
 		printf("Введите ширину шкафа 80-120 см: ");
 		scanf_s("%f", &w);
-	} while ((w < 80) || (w > 120));
+	} while ((w < 80.f) || (w > 120.f));
 	do
 	{
 		printf("Введите глубину шкафа 50-90 см: ");
 		scanf_s("%f", &d);
-	} while ((d < 50) || (d > 90));
-	if (h <= 200)
+	} while ((d < 50.f) || (d > 90.f));
+	if (h <= 200.f)
 	{
 		x = 4;
 	}
@@ -29,9 +29,9 @@ int main()
 	{
 		x = 5;
 	}
-	dvp = (h * w * 0.5 * 850) / 1000000;
-	der = (h * w * 1 * 720) / 1000000;
-	dsp = ((2*d*h + w*d*2 + d*w*x) * 1.5 * 620) / 1000000;
+	dvp = ((h + 1.5f *2.f) * w * 0.5f * 735.f) / 1000000.f;
+	der = ((h + 1.5f * 2.f) * w * 1.f * 700.f) / 1000000.f;
+	dsp = ((2.0f*d*h + w*d*2.0f + d*(w - 1.5f*2.f)*x) * 1.5f * 800.f) / 1000000.f;
 	m = dsp + der + dvp;
 	printf("Масса шкафа %f кг", m);
 	return 0;
