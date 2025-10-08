ffb12d3c0b8881e8e25a5442e8ce40860669e643
VIL0N <vlad784876@gmail.com>
2025-10-08 15:46:20 +0300

    Task1end

--- DokuchaevVA/task1dokuchaev011025/task1dokuchaev011025/task1dokuchaev011025/Source.c
+++ DokuchaevVA/task1dokuchaev011025/task1dokuchaev011025/task1dokuchaev011025/Source.c
@@ -1,40 +1,39 @@
 #include <stdio.h>
 #include <locale.h>
 int main() {
-	setlocale(LC_ALL, "Rus");
-
 	float h, w, d, massa1, massa2, massa3, massa4, massa5, massa, n;
-	int der = 700, dsp = 800, dvp = 735, tder = 10, tdsp = 15, tdvp = 5;
+	float der = 700.f, dsp = 800.f, dvp = 735.f, tder = 10.f, tdsp = 15.f, tdvp = 5.f;
+	setlocale(LC_ALL, "Rus");
 
 	do
 	{
 		printf_s("Введите значение высота шкафа H (в см, 180-220)\n");
 		scanf_s("%f", &h);
-	} while ((h < 180) || (h > 220));
-	h = h * 10;
+	} while ((h < 180.f) || (h > 220.f));
+	h = h * 10.f;
 
 	do
 	{
 		printf_s("Введите значение ширины шкафа W (в см, 80-120)\n");
 		scanf_s("%f", &w);
-	} while ((w < 80) || (w > 120));
-	w = w * 10;	
+	} while ((w < 80.f) || (w > 120.f));
+	w = w * 10.f;	
 
 	do
 	{
 		printf_s("Введите значение глубины шкафа D (в см, 50-90)\n");
 		scanf_s("%f", &d);
-	} while ((w < 80) || (d > 90));
-	d = d * 10;
+	} while ((w < 80.f) || (d > 90.f));
+	d = d * 10.f;
 
-	n = ((h / 10)/40)-1;
+	n = ((h / 10.f)/40)-1;
 	n=(int)n;
 
-	massa1 = (h+30) * tdvp * w * dvp / 1000000000.0; //масса задней стенки
-	massa2 = h * tdsp * d * dsp / 1000000000.0 *2;//боковые стенки
-	massa3 = w * d * dsp * tdsp / 1000000000.0 * 2;// дно и потолок
-	massa4 = w * (h+30) * tder * der / 1000000000.0;// двери
-	massa5 = (w - 2*tdsp) * d * dsp * tdsp * n / 1000000000.0;
+	massa1 = (h+30) * tdvp * w * dvp / 1000000000.0f; //масса задней стенки
+	massa2 = h * tdsp * d * dsp / 1000000000.0f * 2.f;//боковые стенки
+	massa3 = w * d * dsp * tdsp / 1000000000.0f * 2.f;// дно и потолок
+	massa4 = w * (h+30) * tder * der / 1000000000.0f;// двери
+	massa5 = (w - 2.f*tdsp) * d * dsp * tdsp * n / 1000000000.0f;
 	massa = massa1 + massa2 + massa3 + massa4 + massa5;
 	printf("Общая масса = %.1f кг\n\n\n", massa);
 	printf("Масска задней стенки = %.1f кг\n", massa1);
