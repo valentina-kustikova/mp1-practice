e69b777d5fccedd8232ba7c67a4c497bb458cf4b
Yaroslav-Vasiliev <Yaroslav-Vasiliev>
2025-10-08 15:31:33 +0300

    corr2

--- VasilievYS/Task1/Task1/Source1.c
+++ VasilievYS/Task1/Task1/Source1.c
@@ -2,15 +2,15 @@
 #include <locale.h>
 int main()
 {
-	float m, wPol, w, h, tDvp = 0.5, tDsp = 1.5, tDer = 1.0, d; //в см
-	float pDer = 0.7, pDsp = 0.8, pDvp = 0.735; //в г/см3
+	float m, wPol, w, h, tDvp = 0.5f, tDsp = 1.5f, tDer = 1.0f, d; //в см
+	float pDer = 0.7f, pDsp = 0.8f, pDvp = 0.735f; //в г/см3
 	int cPol;
 	setlocale(LC_ALL, "RUS");
 	do
 	{
 		printf("Введите высоту(180-220см), ширину(80-120см), глубину(50-90см): ");
 		scanf_s("%f %f %f", &h, &w, &d);
-	} while ((h < 180 || h > 220) || (w < 80 || w > 120) || (d < 50 || d > 90));
+	} while ((h < 180.f || h > 220.f) || (w < 80.f || w > 120.f) || (d < 50.f || d > 90.f));
 	if (((int)h % 40) == 0)
 	{
 		cPol = (h / 40) - 1;
@@ -20,9 +20,9 @@ int main()
 		cPol = h / 40;
 		cPol = (int)cPol;
 	}
-	wPol = w - 2 * tDsp;
-	m = (pDvp * (h * w * tDvp)) + (pDsp * h * d * tDsp * 2) + (pDsp * (w-2*tDsp) * d * tDsp * 2) + (pDer * h * w * tDer) + (pDsp * d * wPol * tDsp * cPol);
+	wPol = w - 2.f * tDsp;
+	m = (pDvp * (h * w * tDvp)) + (pDsp * h * d * tDsp * 2.f) + (pDsp * (w-2.f*tDsp) * d * tDsp * 2.f) + (pDer * h * w * tDer) + (pDsp * d * wPol * tDsp * cPol);
 	//   задняя стенка              боковины                       крышки                                двери                        полки
-	printf("Масса шкафа = %f кг", m/1000);
+	printf("Масса шкафа = %f кг", m/1000.f);
 	return 0;
 }
