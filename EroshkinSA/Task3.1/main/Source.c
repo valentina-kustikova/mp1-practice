e14e345db12d40805bb00ede594026ae85fb3f2a
Sergey-Eroshkin <seregaeroschkin@mail.ru>
2025-10-21 18:11:21 +0300

    Some changes

--- EroshkinSA/Task3.1/main/Source.c
+++ EroshkinSA/Task3.1/main/Source.c
@@ -3,21 +3,20 @@
 #include <time.h>
 
 int main() {
+	system("chcp 1251");
 	srand((unsigned int)time(0));
 	int n, b = 0, k = 0, mistery[5], i, cnt[10] = {0}, number[5], j;
 	char c;
+	printf("Введите длину отгадываемого числа: ");
 	do {
 		scanf_s("%d", &n);
 	} while (n < 2 || n > 5);
-	printf("chosed length equal %d\n", n);
 	for (i = 0; i < n; i++) {
 		do {
 			mistery[i] = rand() % 10;
 		} while (cnt[mistery[i]] || (i == 0 && mistery[i] == 0));
 		cnt[mistery[i]]++;
-		printf("%d", mistery[i]);
 	}
-	printf("\n");
 	scanf_s("%*c");
 	do {
 		b = 0;
@@ -33,7 +32,6 @@ int main() {
 				scanf_s("%c", &c);
 				number[i] = c - '0';
 			}
-
 			for (j = 0; j < n; j++) {
 				if (mistery[j] == number[i]) {
 					if (i == j) b++;
@@ -41,8 +39,8 @@ int main() {
 				}
 			}
 		}
-		if (b != n) printf("b = %d  k = %d\n", b, k);
-		else printf("You win!!!");
+		if (b != n) printf("Быки - %d  Коровы - %d\n", b, k);
+		else printf("Вы выиграли!!!");
 		scanf_s("%*c");
 	} while (b != n);
 	return 0;
