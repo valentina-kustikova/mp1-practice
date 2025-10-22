96232d17279418d93fe72e0d721c6c4a87a95306
KazachkovaDaria <kazackovadara5@gmail.com>
2025-10-22 16:09:08 +0300

    solution from class 2

--- KazachkovaDA/Task3/Task3/task3.c
+++ KazachkovaDA/Task3/Task3/task3.c
@@ -2,29 +2,68 @@
 #include <time.h>
 #include <stdlib.h>
 #include <locale.h>
-#include <math.h>
 #define N 10
 int main()
 {
 	setlocale(LC_ALL, "Rus");
-	int arr[N];
-	int n;
-	int numb;
-	int i;
-	i = 0;
-	scanf_s("¬ведите длину слова: %d", &n);
+	int arr[N], player[N], n, numb, i, l, guess, j, bull, cow;
+	l = 1;
+	do 
+	{
+		printf_s("¬ведите длину слова:\n");
+		scanf_s("%d", &n);
+	} while (n < 2 || n >5);
+	
+	for (i = 0; i < n; i++) {
+		l *= 10;
+	}
+
 	srand(time(NULL));
-	numb = (rand()%100000)+10;
-	printf_s("%d\n", numb);
-	while (numb > 0) {
+	numb = rand()%(l - l/10)+l/10;
+	for (i = 0; i < n; i++) {
 		arr[i] = numb % 10;
 		numb /= 10;
-		i += 1;
-		printf_s("%d", numb);
 	}
 	for (i = 0; i < n; i++) {
 		printf_s("%d", arr[i]);
 	}
-	return 0;
+	
+	do
+	{
+		cow = 0;
+		bull = 0;
+		do
+		{
+			printf_s("¬ведите вашу догадку:");
+			scanf_s("%d", &guess);
+		} while ((guess >= l) || (guess < (l / 10)));
 
+		for (i = 0; i < n; i++) 
+		{
+			player[i] = guess % 10;
+			guess /= 10;
+		}
+			for (i = 0; i < n; i++)
+			{
+				for (j = 0; j < n; j++) 
+				{
+					if (arr[i] == player[i])
+					{
+						bull += 1;
+					}
+					else
+					{
+						if (arr[i] == player[j++])
+						{
+							cow += 1;
+						}
+
+					}
+				}
+				
+			}
+		printf_s("Ѕыков: %d, коров: %d\n", bull, cow);
+	} while (bull != n);
+	
+	return 0;
 }
