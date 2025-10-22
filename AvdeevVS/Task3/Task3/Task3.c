9bb8eebd011c99e5d753c6ce0fc98224f87b43c0
VladislavAvdeev07 <Vlad.Avdeev07@mail.ru>
2025-10-22 15:09:49 +0300

    Init lab3

--- AvdeevVS/Task3/Task3/Task3.c
+++ AvdeevVS/Task3/Task3/Task3.c
@@ -2,18 +2,23 @@
 #include <time.h>
 #include <stdlib.h>
 #define L 10
-int main() {
-	printf("The game 'Bulls and Cows'\n");
+int main() {	
 	int n, cn, hn;
 	int cow = 0, bull = 0;
-	srand(time(0));
+  int mcn[L];
+  int i, j;
+  int l = 1, r = 9;
+  int fl1 = 0;
+  int fl2 = 0, cf = 0;
+  int mhn[L];
+
+  printf("The game 'Bulls and Cows'\n");
+	srand((unsigned int) time(0));
 	printf("Choose the length of mystery numbers(2-5): ");
 	do {
 		scanf_s("%d", &n);
 	} while (n < 2 || n>5);
-	int mcn[L];
-	int i, j;
-	int l = 1, r = 9;
+	
 	for (i = 1; i < n; i++) {
 		l *= 10;
 	}
@@ -21,13 +26,13 @@ int main() {
 		r *= 10;
 		r += 9;
 	}
-	int fl1 = 0;
+	
 	do {
-		fl1 = 1;
-		cn = rand() % (r - l + 1) + l;
 		int p[L] = { 0 };
 		int s = 0;
 		int g1;
+    fl1 = 1;
+    cn = rand() % (r - l + 1) + l;
 		g1 = cn;
 		while (g1) {
 			mcn[s] = g1 % 10;
@@ -44,17 +49,15 @@ int main() {
 		}
 	} while (fl1 == 0);
 	printf("Come up with and enter a number of the selected lenght: ");
-	int fl2 = 0, cf = 0;
-	int mhn[L];
 	while (cf != 1) {
 		do {
+      int pp[L] = { 0 };
+      int u = 0;
+      int g2;
 			fl2 = 1;
 			do {
 				scanf_s("%d", &hn);
-			} while (hn<l || hn>r);
-			int pp[L] = { 0 };
-			int u = 0;
-			int g2;
+			} while (hn<l || hn>r);			
 			g2 = hn;
 			while (g2) {
 				mhn[u] = g2 % 10;
@@ -73,14 +76,19 @@ int main() {
 		cow = 0;
 		bull = 0;
 		for (i = 0; i < n; i++) {
-			for (j = 0; j < n; j++) {
-				if (mcn[i] == mhn[j] && i != j) {
+			for (j = 0; j < i; j++) {
+				if (mcn[i] == mhn[j]) {
 					cow++;
-				}
-				if (mcn[i] == mhn[j] && i == j) {
-					bull++;
-				}
+				}			
 			}
+      for (j = i+1; j < n; j++) {
+        if (mcn[i] == mhn[j]) {
+          cow++;
+        }
+      }
+      if (mcn[i] == mhn[i]) {
+        bull++;
+      }
 		}
 		if (bull == n) {
 			cf = 1;
