3e53f979bfcf05e90bbd9b78f5ce7293f46cc9bd
work3r1337 <c.michasov2004@yandex.ru>
2025-10-27 14:23:58 +0300

    Add number generation

---
+++ MichasovKS/lab-3/lab-3.c
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int gen(int len);
+int valid(int n, int len);
+
+int main()
+{
+    srand((unsigned int) time(NULL));
+    int len;
+    printf("Enter number length: ");
+    scanf_s("%d", &len);
+    int n = gen(len);
+    while (!valid(n, len))
+        n = gen(len);
+    printf("%d", n);
+}
+
+int gen(int len) {
+    int i;
+    int res = 0;
+    for (i = 0; i < len; i++) {
+        res *= 10;
+        res += rand() % 10;
+    }
+    return res;
+}
+
+int valid(int n, int len) {
+    int i;
+    int arr[10] = { 0 };
+    for (i = 0; i < len; i++) {
+        arr[n % 10] += 1;
+        if (n == 0 || arr[n % 10] > 1) return 0;
+        n /= 10;
+    }
+    return 1;
+}
