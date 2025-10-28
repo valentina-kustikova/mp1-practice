94e93ee095fb8dd52d972c2e52302d1a887fe861
Yaroslav-Vasiliev <yarvasiliev@gmail.com>
2025-10-28 15:48:27 +0300

    s

--- VasilievYS/Task3/Task3/Source.c
+++ VasilievYS/Task3/Task3/Source.c
@@ -5,21 +5,48 @@
 #include <math.h>
 int main()
 {
-  int n, temp, i, a[5], j, b[5] = { 0 }, num=0, ans=0, bull=0, cow=0;
+  int n, check = 0, temp, i, a[5], j, b[5] = { 0 }, num=0, ans=0, bull=0, cow=0;
   setlocale(LC_ALL, "Russian");
   srand((unsigned int)time(0));
-  printf("Длина числа: ");
+  printf("Длина числа(от 2 до 5): ");
   scanf_s("%d", &n);
   for (i = 0; i < n; i++)
   {
     a[i] = rand() % 9 + 1;
-    num += a[i] * pow(10, n -1 - i);
+    
   }
-  printf("%d", num);
+  while (check != 1)
+  {
+      check = 1;
+      for (i = 0; i < n; i++)
+      {
+          for (j = 0; j < n; j++)
+          {
+              if (a[i] == a[j] && i!=j)
+              {
+                  a[i] = rand() % 9 + 1; 
+              }
+          }
+      }
+      for (i = 0; i < n; i++)
+      {
+          for (j = 0; j < n; j++)
+          {
+              if (a[i] == a[j] && i!=j)
+              {
+                  check = 0;
+              }
+          }
+      }
+  }
+  for (i = 0; i < n; i++)
+  {
+      num += a[i] * pow(10, n - 1 - i);
+  }
   while (num != ans)
   {
     printf("Ваше число длинной из %d cимволов: ", n);
-    scanf("%d", ans);
+    scanf_s("%d", &ans);
     if (ans == num)
       printf("Верно");
     else
@@ -28,6 +55,7 @@ int main()
       {
         temp = ans % 10;
         b[n - 1 - i] = temp;
+        ans /= 10;
       }
       for (i = 0; i < n; i++)
       {
@@ -38,15 +66,21 @@ int main()
         }
         else
         {
-          for (j = 0, j < n, j++)
+          for (j = 0; j < n; j++)
           {
-
+              if (b[i] == a[j] && i != j)
+              {
+                  cow++;
+                  continue;
+              }
           }
         }
       }
+      printf("Быков: %d\n", bull);
+      printf("Коров: %d\n", cow);
+      cow = 0;
+      bull = 0;
     }
   }
   return 0;
-
-
 }
