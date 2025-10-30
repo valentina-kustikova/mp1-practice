0d28f97409ef2fd689b98feec00a21a1ea980782
PeterBelov27 <ptr_chernov@inbox.ru>
2025-10-30 10:16:53 +0300

    Закинул работу Task1 на ветку Lab1 и поправил расчеты

--- BelovPE/Lab1/Lab1/Task1.c
+++ BelovPE/Lab1/Lab1/Task1.c
@@ -11,24 +11,27 @@ int main(void) {
 	printf("Введите высоту, ширину и глубину шкафа в см в отдельных строках:\n");
 	do {
 		scanf_s("%lf", &h);
+		if (h < 180 || h > 220) printf("Неправильное значение высоты шкафа: 180-220 см\n");
 	} while (h < 180 || h > 220);
 
 	do {
 		scanf_s("%lf", &w);
+		if (w < 80 || w > 120) printf("Неправильное значение ширины шкафа: 80-120 см\n");
 	} while (w < 80 || w > 120);
 
 	do {
 		scanf_s("%lf", &d);
+		if (d < 50 || d > 90) printf("Неправильное значение глубины шкафа: 50-90 см\n");
 	} while (d < 50 || d > 90);
 
 	double Mback_wall, Mrl_wall, Mhl_wall, Mdoors, Mshelves;
 	Mback_wall = (((h * w) / 10000) * 0.005) * p_dvp;
-	Mrl_wall = (((h * d) / 10000) * 0.015) * p_dcp;
-	Mhl_wall = ((((w * d) / 10000) * 0.015)) * p_dcp;
+	Mrl_wall = (((h * d) / 10000) * 0.015) * p_dcp * 2;
+	Mhl_wall = ((((w * d) / 10000) * 0.015)) * p_dcp * 2;
 	Mdoors = (((h * w) / 10000) * 0.01) * p_tree;
-	Mshelves = ((((w * d) / 10000) * 0.015)) * p_dcp * ((h / 40) - 1);
+	Mshelves = (((((w - 3) * (d - 1.5)) / 10000) * 0.015)) * p_dcp * (((h - 3) / 40) - 1);
 
-	printf("Масса шкафа: %.3lf", Mback_wall + Mrl_wall + Mhl_wall + Mdoors + Mshelves);
+	printf("Масса шкафа: %.3lf кг", Mback_wall + Mrl_wall + Mhl_wall + Mdoors + Mshelves);
 
 	return 0;
 }
