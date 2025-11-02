f8a8f1bfe9b446ae3f61005466839b1a5383f4b4
Sergey-Eroshkin <seregaeroschkin@mail.ru>
2025-11-02 12:38:19 +0300

    Some changes

--- EroshkinSA/Task4.1/Source.c
+++ EroshkinSA/Task4.1/Source.c
@@ -2,8 +2,7 @@
 #define F 4
 #define N 256
 
-char names[10000][256] = { {""} };
-int last_cost, last_sale;
+char names[10000][N] = { {""} };
 
 
 void form(int costs[], int sales[], int count[]) {
@@ -26,7 +25,7 @@ void calculate(int count[], int costs[], int sales[]) {
 	printf("\nОбщая сумма: %d\n", summ);
 }
 
-int scan(int count[], int costs[], int sales[]) {
+int scan(int count[], int costs[], int sales[], int* last_cost, int* last_sale) {
 // Считываю очередной лог
 	char code[F];
 	int i = 0, id = 0, true_cost;
@@ -41,15 +40,15 @@ int scan(int count[], int costs[], int sales[]) {
 		return -1;
 	}
 // Сохраняю последний лог
-	last_cost = costs[id]; last_sale = sales[id];
+	*last_cost = costs[id]; *last_sale = sales[id];
 	return id;
 }
 
-void last(int id, int len) {
+void last(int id, int len, int* last_cost, int* last_sale) {
 	int i = 0;
 	printf("ПОСЛЕДНИЙ ОТСКАНИРОВАННЫЙ ТОВАР  ");
 	for (i = 0; i < len; i++) printf("%c", names[id][i]);
-	printf(": %d -> %d\n", last_cost, last_cost * (100 - last_sale) / 100);
+	printf(": %d -> %d\n", *last_cost, *last_cost * (100 - *last_sale) / 100);
 }
 
 void base(int length[], int costs[], int sales[]) {
@@ -87,7 +86,7 @@ int main() {
 	int length[10000] = { 0 };
 	int true_costs[10000] = { 0 }, costs[10000] = { 0 }, count[10000] = { 0 }, sales[10000] = { 0 };
 	printf("Доступные действия:\n0 - Завершение работы\n1 <****> - Считать штрих-код\n2 - Вывести последний отсканированный товар\n3 - Добавить товар в чек\n4 - Сформировать и вывести чек\n5 - Узнать общую стоимость товаров в чеке\n");
-	int last_id, last_cost, last_true_cost;
+	int last_cost, last_sale;
 	printf("Коды доступные для сканирования: ");
 	base(length, costs, sales);
 	printf("\n");
@@ -97,7 +96,7 @@ int main() {
 		case 0:
 			return 0;
 		case 1:
-			id = scan(count, costs, sales);
+			id = scan(count, costs, sales, &last_cost, &last_sale);
 			scanf_s("%*c");
 			break;
 		case 2:
@@ -105,7 +104,7 @@ int main() {
 				printf("Пусто\n");
 				break;
 			}
-			last(id, length[id]);
+			last(id, length[id], &last_cost, &last_sale);
 			break;
 		case 3:
 			if (id == -1) printf("Пусто\n");
