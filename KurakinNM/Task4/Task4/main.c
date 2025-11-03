826b12a1b50631ece680e2e507232a0b2e7a9d7b
NickKurakin <nmkurakin@gmail.com>
2025-11-03 19:35:13 +0300

    fix masivs

--- KurakinNM/Task4/Task4/main.c
+++ KurakinNM/Task4/Task4/main.c
@@ -4,22 +4,21 @@
 #define L 4
 
 void scan(char qr[]);
-void info(int qrSelect, char name[], int cost[], int discount[]);
-void addToCheck(int qrSelect, int check[]);
-void createCheck(int check[], char name[], int cost[], float discount[]);
+void info(char qrSelect[], char* qrs[], char* name[], int cost[], int discount[]);
+void addToCheck(char qrSelect[], int check[], char* qrs[]);
+void createCheck(int check[], char* name[], int cost[], float discount[]);
 void totalCost(int check[], int cost[], float discount[]);
 
 int main()
 {
-	int i,j, select;
+	int i, select;
 	int check[L] = {0,0,0,0};
-	char qr[L][4] = { "0000","0001","0200","9000" };
-	char name[L][4] = { "milk","cake","juse","bean" };
+	char* qr[L] = { "0000","0001","0200","9000" };
+	char* name[L] = { "milk","cake","juse","bean" };
 	int cost[L] = {50,250,15,30};
 	float discount[L] = { 0.10f,0.40f,0.0f,0.15f };
-	char qrSelect[4] = "9999";
-	printf("QRs: ");
-	for (i = 0; i < L; i++) { for (j = 0; j < 4; j++) { printf("%c",qr[i][j]); }; printf(" "); };
+	char qrSelect[5] = "9999";
+	for (i = 0; i < L; i++) { printf("%s ",qr[i]); };
 	printf("\n");
 	do
 	{
@@ -27,11 +26,11 @@ int main()
 		scanf_s("%d", &select);
 		switch (select)
 		{
-		case 1: scan(qrSelect); for (j = 0; j < 4; j++) { printf("%c", qrSelect[j]); }; break;
-		/*case 2: info(qrSelect, name, cost, discount); break;
-		case 3: addToCheck(qrSelect, check); break;
+		case 1: scan(qrSelect); break;
+		case 2: info(qrSelect, qr, name, cost, discount); break;
+		case 3: addToCheck(qrSelect, check, qr); break;
 		case 4: createCheck(check, name, cost, discount); break;
-		case 5: totalCost(check, cost, discount); select = 0; break;*/
+		case 5: totalCost(check, cost, discount); select = 0; break;
 		default:break;
 		}
 
@@ -45,27 +44,34 @@ void scan(char qr[])
 	printf("Enter QR:");
 	gets(qr);
 }
-void info(int qrSelect, char name[],int cost[], float discount[])
+void info(char qrSelect[], char* qrs[], char* name[], int cost[], float discount[])
+{
+	int i,j;
+	for (i = 0; i < L; i++) {
+		if (strcmp(qrSelect, qrs[i]) == 0)
+		{
+			printf("product name: %s\n", name[i]);
+			printf("product cost: %d\n", cost[i]);
+			printf("product discount: %2.1f\n", discount[i] * 100);
+			break;
+		}
+	}
+	if (i == L) { printf("uncorect QR\n"); }
+}
+void addToCheck(char qrSelect[], int check[], char* qrs[])
 {
 	int i;
-	if (qrSelect == -1) { printf("uncorect QR\n"); }
-	else
-	{
-		printf("product name: ");
-		for (i = 0; i < 4; i++)
+	for (i = 0; i < L; i++) {
+		if (strcmp(qrSelect, qrs[i]) == 0)
 		{
-			printf("%c", name[4* qrSelect + i]);
+			check[i]++;
+			printf("product added\n");
+			break;
 		}
-		printf("\nproduct cost: %d\n",cost[qrSelect]);
-		printf("product discount: %2.1f\n", discount[qrSelect]*100);
 	}
+	if (i == L) { printf("uncorect QR\n"); }
 }
-void addToCheck(int qrSelect, int check[])
-{
-	if (qrSelect == -1) { printf("uncorect QR\n"); }
-	else { check[qrSelect]++; printf("product added\n");}
-}
-void createCheck(int check[], char name[], int cost[], float discount[])
+void createCheck(int check[], char* name[], int cost[], float discount[])
 {
 	int i,j;
 	printf("\tname\t|\tcost\t|\tquantity\t|\ttotal cost\t\n");
@@ -74,12 +80,7 @@ void createCheck(int check[], char name[], int cost[], float discount[])
 	{
 		if (check[i] > 0)
 		{
-			printf("\t");
-			for (j = 0; j < 4; j++)
-			{
-				printf("%c", name[4 * i + j]);
-			}
-			printf("\t|\t%.2f\t|\t%8d\t|\t%.2f\t\n", cost[i] * (1 - discount[i]), check[i], cost[i] * (1 - discount[i]) * check[i]);
+			printf("\t%s\t|\t%.2f\t|\t%8d\t|\t%.2f\t\n", name[i], cost[i] * (1 - discount[i]), check[i], cost[i] * (1 - discount[i]) * check[i]);
 		}
 	}
 }
