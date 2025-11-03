def4e1c67207fbecfddf44e093394b3c41a5a523
anastasiaefremova794 <anastasiaefremova3435@gmail.com>
2025-11-03 14:05:07 +0300

    fix copy numbers

--- KurakinNM/BullsAndCows/BullsAndCows/main.c
+++ KurakinNM/BullsAndCows/BullsAndCows/main.c
@@ -7,7 +7,8 @@ int main()
 	int i,j,bulls = 0,cows = 0,userNumber,L;
 	printf("enter number (1-9):");
 	scanf_s("%d", &L);
-	int num[10];
+	int num[9];
+	int userNum[9];
 	for (i = L - 1; i >= 0; i--)
 	{
 		do {
@@ -27,15 +28,24 @@ int main()
 #endif
 		printf("enter an %d-digit number: ", L);
 		scanf_s("%d", &userNumber);
-		i = 0; bulls = 0; cows = 0;
-		while (userNumber > 0 && i < L)
+		for (i = 0; i < L; i++) { userNum[i] = userNumber % 10; userNumber /= 10;}
+		for (i = 0; i < L; i++)
 		{
-			if (userNumber % 10 == num[i]) { bulls++; };
-			for (j = 0; j < L; j++) { if (userNumber % 10 == num[j]) { cows++; } };
-			i++;
-			userNumber /= 10;
+			for (j = i+1; j < L; j++) { if (userNum[j] == userNum[i]) { break; } };
+			if (j != L) { break; }
 		}
-		printf("Bulls = %d, Cows = %d\n\n", bulls, cows- bulls);
+		if (i == L) {
+			i = 0; bulls = 0; cows = 0;
+			while (userNumber > 0 && i < L)
+			{
+				if (userNumber % 10 == num[i]) { bulls++; };
+				for (j = 0; j < L; j++) { if (userNumber % 10 == num[j]) { cows++; } };
+				i++;
+				userNumber /= 10;
+			}
+			printf("Bulls = %d, Cows = %d\n\n", bulls, cows - bulls);
+		}
+		else { printf("Uncorect enter!\n"); }
 	}
 	printf("You win!");
 	return 0;
