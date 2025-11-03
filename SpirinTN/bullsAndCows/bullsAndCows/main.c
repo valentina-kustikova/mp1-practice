72da41653bec16d82fccbbd00c8d6c0d51c629f4
terentys <terenty818@gmail.com>
2025-11-03 14:23:43 +0300

    update

--- SpirinTN/bullsAndCows/bullsAndCows/main.c
+++ SpirinTN/bullsAndCows/bullsAndCows/main.c
@@ -40,15 +40,13 @@ int setLengthNumber() {
 }
 
 int generationNumber(int lengthNumber) {
-	int i, resultNumber;
-	int digits[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
-	int randPos = rand() % 9;
-	resultNumber = digits[randPos];
-	digits[randPos] = 0;
-	for (i = 1; i < lengthNumber; i++) {
-		int randPos = rand() % (10 - i);
+	int i, resultNumber = 0;
+	int digits[9] = { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+	for (i = 0; i < lengthNumber; i++) {
+		int randPos = rand() % (9 - i);
 		resultNumber = resultNumber * 10 + digits[randPos];
-		digits[randPos] = digits[9 - i];
+		digits[randPos] = digits[8 - i];
 	}
 	return resultNumber;
 }
@@ -58,7 +56,7 @@ int counterBulls(int userNumber, int hiddenNumber, int lengthNumber);
 int counterCows(int userNumber, int hiddenNumber, int lengthNumber);
 
 void tryingGuessNumber(int hiddenNumber, int lengthNumber) {
-	int userNumber = 0, rangeMultiplier = (int) pow(10, lengthNumber - 1), i;
+	int userNumber = 0, rangeMultiplier = (int) pow(10, lengthNumber - 1);
 
 	while (userNumber != hiddenNumber) {
 		printf("Input number (all the digits are different, length number - %d digits): ", lengthNumber);
@@ -89,9 +87,11 @@ int isIdenticalDigits(int userNumber) {
 int counterBulls(int userNumber, int hiddenNumber, int lengthNumber) {
 	int count = 0, i;
 	for (i = 0; i < lengthNumber; i++) {
-		if (hiddenNumber / (int)pow(10, i) % 10 == userNumber / (int)pow(10, i) % 10) {
+		if (userNumber % 10 == hiddenNumber % 10) {
 			count++;
 		}
+		userNumber /= 10;
+		hiddenNumber /= 10;
 	}
 	return count;
 }
@@ -99,14 +99,23 @@ int counterBulls(int userNumber, int hiddenNumber, int lengthNumber) {
 int counterCows(int userNumber, int hiddenNumber, int lengthNumber) {
 	int count = 0, i, j;
 	for (i = 0; i < lengthNumber; i++) {
-		for (j = 0; j < lengthNumber; j++) {
-			if (j == i) {
-				continue;
-			}
-			if (hiddenNumber / (int)pow(10, i) % 10 == userNumber / (int)pow(10, j) % 10) {
+		int tmpHiddenNumber = hiddenNumber;
+		for (j = 0; j < i; j++) {
+			if (userNumber % 10 == tmpHiddenNumber % 10) {
 				count++;
+				break;
 			}
+			tmpHiddenNumber /= 10;
 		}
+		tmpHiddenNumber /= 10;
+		for (j = i + 1; j < lengthNumber; j++) {
+			if (userNumber % 10 == tmpHiddenNumber % 10) {
+				count++;
+				break;
+			}
+			tmpHiddenNumber /= 10;
+		}
+		userNumber /= 10;
 	}
 	return count;
 }
