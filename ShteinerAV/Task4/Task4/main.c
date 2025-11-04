59a95abb6011d9897fe9a14ef1182ac08e33c019
DoDiK13 <aleksandrstejner905@gmail.com>
2025-11-04 12:48:49 +0300

    Correct version

--- ShteinerAV/Task4/Task4/main.c
+++ ShteinerAV/Task4/Task4/main.c
@@ -2,172 +2,129 @@
 #include <locale.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
-#define KOL_ITEM 3
-#define N 5
+#define N 4
 
 
-void scan_item(int new_item, int array, int size);
+void user_input(int *user_choise);
 
 int main() {
     srand((unsigned int)time(0));
     setlocale(LC_ALL, "rus");
 
-    int userChoice, is_true = 1, i, j, rand_skid_1 = 1 + rand() % 50, rand_skid_2 = 1 + rand() % 50, rand_skid_3 = 1 + rand() % 50, itog_summa=0;
+    int is_true = 1, user_choise, i, flag = 0, itog_summ = 0;
+    char new_barcode[5] = "";
 
-    int array[KOL_ITEM][N] = {
-        {1234, 159, rand_skid_1, 0, 159 - (int)(159 * rand_skid_1 / (float)100)},  // товар 1
-        {1111, 500, rand_skid_2, 0, 500 - (int)(500 * rand_skid_2 / (float)100)},  // товар 2
-        {1212, 400, rand_skid_3, 0, 400 - (int)(400 * rand_skid_3 / (float)100)}   // товар 3
-    };
+    char name_list[N][20] = { "Мандаринки", "Бананчики", "Яблочки", "Апельсинки" };
+    char barcode_list[N][4] = { "1234", "1111", "1212", "0980"};
+    int price_list[N] = { 350, 139, 100, 239 };
+    int cnt_list[N] = { 0, 0, 0, 0 };
+    int discounts[N] = { 1 + rand() % 50 , 1 + rand() % 50 , 1 + rand() % 50 , 1 + rand() % 50 };
+    int price_with_discounts[4] = { 
+        price_list[0] - (int)(price_list[0] * (1 + rand() % 50) / (float)100),
+        price_list[1] - (int)(price_list[1] * (1 + rand() % 50) / (float)100),
+        price_list[2] - (int)(price_list[2] * (1 + rand() % 50) / (float)100),
+        price_list[3] - (int)(price_list[3] * (1 + rand() % 50) / (float)100)
+        };
 
-    do {
-        system("cls");
-        printf("Электронная касса \n\n");
-        printf("1) Сканировать товар\n");
-        printf("2) Сформировать чек\n");
+   do {
+       system("cls");
+       printf("  ЭЛЕКТРОННАЯ КАССА\n");
+       printf("----------------------\n");
+       printf("1) Сканировать товар\n");
+       printf("2) Сформировать чек\n");
 
-        printf("\nКоманда: "); 
-        scanf_s("%d", &userChoice);
-        getchar();
+       user_input(&user_choise);
 
-        while (userChoice < 1 || userChoice > 2) {
-            printf("Ты ввёл команду неверно!"); 
-            printf("\nКоманда: ");
-            scanf_s("%d", &userChoice);
-            getchar();
-        }
+       if (user_choise == 1) {
+           system("cls");
+           printf("Введите штрих-код товара: ");
+           scanf_s("%4s", new_barcode, sizeof(new_barcode));
+           
+           for (i = 0; i < N; i++) {
+               if (strncmp(new_barcode, barcode_list[i], 4) == 0) {
+                   flag = 1;
+                   printf("\n    Товар   |  Цена(руб)  |  Скидка | Цена со скидкой\n");
+                   printf("-------------------------------------------------------\n");
+                   printf(" %-16s %-12d %-12d %d", name_list[i], price_list[i], discounts[i], price_with_discounts[i]);
 
-        if (userChoice == 1) {
-            system("cls");
-            int barcode, id=-1;
+                   printf("\n\nДобавть этот товар в чек?\n");
+                   printf("1) Да          2) Нет\n");
 
-            printf("Введите штрих-код товара на упаковке (4 цифры): ");
-            scanf_s("%d", &barcode);
+                   user_input(&user_choise);
 
-            // Сканирование элементов, проверка на наличие товара
-            for (i = 0; i < KOL_ITEM; i++) {
-                if (array[i][0] == barcode) {
-                    id = i;
-                    break;
-                }
-            }
-            //----------------------------------
+                   if (user_choise == 1) {
+                       cnt_list[i]++;
+                   }
+                   else {
+                       continue;
+                   }
+               }
+           }
+           if (flag == 0) {
+               printf("Такого товара нет в базе данных! \n");
+               system("pause");
+           }
+       }
 
-            // Вывод сканированного товара
-            if (id != -1) {
-                printf("\n    Товар   |  Цена(руб)  |  Скидка | Цена со скидкой\n");
-                printf("-------------------------------------------------------\n");
+       else if (user_choise == 2) {
+           system("cls");
+           printf("                ЧЕК\n");
+           printf("--------------------------------------\n");
+           flag = 0;
+           for (i = 0; i < N; i++) {
+               if (cnt_list[i] != 0) {
+                   flag = 1;
+                   printf("\n    Товар   | Кол-во | Общая сумма \n");
+                   printf("--------------------------------------\n");
+                   printf(" %-15s %-8d %d\n", name_list[i], cnt_list[i], price_with_discounts[i] * cnt_list[i]);
+               }
+           }
+           printf("\n--------------------------------------");
 
-                if (id == 0) {
-                    printf(" Апельсинка       ");
-                }
-                else if (id == 1) {
-                    printf("   Яболчки       ");
-                }
-                else {
-                    printf("  Бананчики      ");
-                }
+           if (flag == 1) {
+               printf("\n\nЖелаете оплатить товар? \n");
+               printf("1) Да          2) Нет\n");
 
-                for (i = 1; i < N; i++) {
-                    if (i != 3) {
-                        printf("%d          ", array[id][i]);
-                    }
-                }
-                printf("\n-------------------------------------------------------\n");
+               user_input(&user_choise);
 
-                printf("\n");
-            }
-            else {
-                printf("Такого товара нет в осартементе!\n ");
-                system("pause");
-                continue;
-            }
-            //----------------------------------
-
-            // Добавление в чек
-            printf("Добавить этот товар в чек?\n");
-            printf("1) Да\n");
-            printf("2) Нет\n");
-
-            printf("\nКоманда: ");
-            scanf_s("%d", &userChoice);
-            getchar();
-
-            while (userChoice < 1 || userChoice > 2) {
-                printf("Ты ввёл команду неверно!");
-                printf("\nКоманда: ");
-                scanf_s("%d", &userChoice);
-                getchar();
-            }
-
-            if (userChoice == 1) {
-                array[id][3]++;
-            }
-            else {
-                continue;
-            }
-            //----------------------------------
-            system("pause");
-        }
-
-        if (userChoice == 2) {
-            system("cls");
-            printf("-----------------ЧЕК------------------\n\n");
-            // Вывод товаров, попавших в чек
-            for (i = 0; i < KOL_ITEM; i++) {
-                if (array[i][3] > 0) {
-                    printf("\n    Товар   | Кол-во | Общая сумма \n");
-                    printf("--------------------------------------\n");
-
-                    if (i == 0) {
-                        printf(" Апельсинка     %d          %d\n", array[i][3], array[i][3]* array[i][4]);
-                    }
-                    else if (i == 1) {
-                        printf("   Яболчки       %d          %d\n", array[i][3], array[i][3] * array[i][4]);
-                    }
-                    else {
-                        printf("  Бананчики      %d          %d\n", array[i][3], array[i][3] * array[i][4]);
-                    }   
-                }
-            }
-            printf("\n\n--------------------------------------\n\n");
-            //----------------------------------
-
-            printf("Оплатить чек?\n");
-            printf("1) Да\n");
-            printf("2) Нет\n");
-
-            printf("\nКоманда: ");
-            scanf_s("%d", &userChoice);
-            getchar();
-
-            while (userChoice < 1 || userChoice > 2) {
-                printf("Ты ввёл команду неверно!");
-                printf("\nКоманда: ");
-                scanf_s("%d", &userChoice);
-                getchar();
-            }
-
-            // Вывод суммы чека
-            if (userChoice == 1) {
-                system("cls");
-                for (i = 0; i < KOL_ITEM; i++) {
-                    if (array[i][2] > 0) {
-                        itog_summa += array[i][3] * array[i][4];
-                    }
-                }
-                printf("\nСумма к оплате: %d\n", itog_summa);
-                is_true = 0;
-            }
-            else {
-                continue;
-            }
-            //----------------------------------
-
-        }
-
+               if (user_choise == 1) {
+                   system("cls");
+                   for (i = 0; i < N; i++) {
+                       if (cnt_list[i] != 0) {
+                           itog_summ += price_with_discounts[i] * cnt_list[i];
+                       }
+                   }
+                   printf("Сумма к оплате: %d\n", itog_summ); 
+                   system("pause");
+                   return 1;
+               }
+               else {
+                   continue;
+                   system("pause");
+               }
+           }
+           else {
+               system("cls");
+               printf("Вы не добавили товар в чек!\n");
+               system("pause");
+               continue;
+           }
+           
+       }
     } while (is_true);
     return 0;
+}
+
+void user_input(int *user_choise) {
+    printf("\nКоманда: ");
+    scanf_s("%d", user_choise);
+
+    while (*user_choise < 1 || *user_choise > 2) {
+        printf("Ты ввёл команду неверно");
+        printf("\nКоманда: ");
+        scanf_s("%d", user_choise);
+    }
 }
 
