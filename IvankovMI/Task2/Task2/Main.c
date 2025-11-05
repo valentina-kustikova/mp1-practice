b596f5eaaf5866bb1256ed1787b540c38596a730
MichaelIvanckov <m.i.ivanckov@gmail.com>
2025-11-05 11:14:00 +0300

    Иванков М. И. Lab1 - финальная работающая версия - убраны лишние printf'ы для отладки

--- IvankovMI/Task2/Task2/Main.c
+++ IvankovMI/Task2/Task2/Main.c
@@ -18,15 +18,15 @@ int main() {
 	h = hi;
 	w = wi;
 	d = di;
-	h /= 100;                                                         printf("h = %f m\n", h);       //m
-	w /= 100;                                                         printf("w = %f m\n", w);
-	d /= 100;                                                         printf("d = %f m\n", d);
-	m1 = h * w * pdvp * 0.005;                                        printf("m1 = %f kg\n", m1);    //Задняя стенка     ( m*m*m * (kg/m3) = kg)
-	m2 = 2 * (h - 2 * t) * (d - 0.01) * t * pdsp;                     printf("m2 = %f kg\n", m2);    //Боковины
-	m3 = 2 * w * d * t * pdsp;                                        printf("m3 = %f kg\n", m3);    //Крышки
-	m4 = h * w * pd * 0.01;                                           printf("m4 = %f kg\n", m4);    //Дверцы
+	h /= 100;                                                             //m
+	w /= 100;                                                         
+	d /= 100;                                                         
+	m1 = h * w * pdvp * 0.005;                                            //Задняя стенка     ( m*m*m * (kg/m3) = kg)
+	m2 = 2 * (h - 2 * t) * (d - 0.01) * t * pdsp;                         //Боковины
+	m3 = 2 * w * d * t * pdsp;                                            //Крышки
+	m4 = h * w * pd * 0.01;                                               //Дверцы
 	k = (hi <= 202) ? 4 : 5;
-	m5 = k * (w - t * 2) * (d - 0.01) * t * pdsp;                     printf("m5 = %f kg\n", m5);    //Полки
+	m5 = k * (w - t * 2) * (d - 0.01) * t * pdsp;                         //Полки
 	printf("\nМасса вашего шкафа = %f kg\n", m1 + m2 + m3 + m4 + m5);
 	printf("Полок в нем %d\n", k);
 	return 0;
