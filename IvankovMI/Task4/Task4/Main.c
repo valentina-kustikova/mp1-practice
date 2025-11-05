877db7bb329429d2fd3f45db64e8ef51341a8c8e
MichaelIvanckov <m.i.ivanckov@gmail.com>
2025-11-05 12:26:10 +0300

    Иванков М. И. Lab3 - Игра Быки и коровы

--- IvankovMI/Task4/Task4/Main.c
+++ IvankovMI/Task4/Task4/Main.c
@@ -3,10 +3,12 @@
 #include <locale.h>
 #include <time.h>
 #include <math.h>
+#include <string.h>
 
 int unique(int a[5], int n, int i);
+void get_int_arr(int a[5],int n);
 int main() {
-	int n, sl[5] = -1, i, j;
+	int n, sl[5] = {-1}, mysl[5] = {0}, i, j;  //на вс€кий случай инициализирую массивы, пока не знаю, зачем
 
 	setlocale(LC_ALL, "Rus");
 	srand(time(NULL));
@@ -15,14 +17,39 @@ int main() {
 	scanf_s("%d", &n);
 	sl[0] = rand() % 10;
 	for (i = 1; i < n; i++) {
-		while (unique(sl, n, i))               //(sl[i] == sl[i - 1]) || (sl[i] == sl[abs(i - 2)])     (for (j = i; j > 0; j--) sl[i] == sl[j])
+		while (!unique(sl, n, i))               //(sl[i] == sl[i - 1]) || (sl[i] == sl[abs(i - 2)])     (for (j = i; j > 0; j--) sl[i] == sl[j])
 			sl[i] = rand() % 10;
 	}
-
+	printf("\nOk'эй, € загадал число из %d цифр. ¬водите свою отгадку, \nа € отвечу, сколько в ней коров (цифра есть, но не на своем месте) \nи быков (цифра и еЄ положение угаданы верно).\n", n);
+	get_int_arr(mysl, n);
 }
 
 int unique(int a[5], int n, int i) {
-	int ok = 1, j = 0;
+	//ф-€ провер€ет уникальность i-того элемента в срезе массива от 0 до i
+	int j = 0;
 	for (; j < i; j++)
-		if a[j]
+		if (a[j] == a[i])
+			return 0;
+	return 1;
+}
+
+void get_int_arr(int a[5], int n); {
+	char inp[100];
+	fscanf_s("%s", inp);
+	if ((strlen(inp) != n) || (strlen(inp) > n))
+		printf("¬ведите число от ")
+}
+
+int arr_is_digit(char a[100]) {
+
+}
+
+
+
+
+void get_int_arr1(int a[5], int n); {
+	char inp[100];
+	fscanf_s("%s", inp);
+	if ((strlen(inp) < 2) || (strlen(inp) > n))
+		printf("¬ведите число от ")
 }
