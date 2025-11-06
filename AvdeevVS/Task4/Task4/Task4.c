e3b42a7f40cf4ff8d895f57ba58ddd07dd3978d0
VladislavAvdeev07 <Vlad.Avdeev07@mail.ru>
2025-11-06 23:44:39 +0300

    Init lab4

--- AvdeevVS/Task4/Task4/Task4.c
+++ AvdeevVS/Task4/Task4/Task4.c
@@ -1,78 +1,56 @@
 #include <stdio.h>
+#include <string.h>
 #define D 10
 #define S 100
 #define P 5
 int order() {
   int t;
-  do {
-    scanf_s("%d", &t);
-  } while (t < 1 || t>4);
+  scanf_s("%d", &t);
   scanf_s("%*c");
   return t;
 }
-void Case1(char scan[][P], int N[], int h, int b) {
-  int n = 0, i;
-  for (i = 0; i < 4; i++) {
-    scanf_s("%c", &(scan[h][i]));
-    n *= 10;
-    n += scan[h][i] - '0';
-  }
-  N[b] = n;
+void Case1(char scan[][P], int h) {
+  scanf_s("%4s", &(scan[h]));
 }
-void Case2(char code[][P], char name[][D], int cost[], int dis[], char scan[][P], int h) {
-  int y, i, j;
+void Case2(char code[][P], char name[][D], float cost[], float dis[], char scan[][P], int h) {
+  int j;
   for (j = 0; j < D; j++) {
-    y = 1;
-    for (i = 0; i < P - 1; i++) {
-      if (code[j][i] != scan[h - 1][i]) {
-        y = 0;
-        break;
-      }
+    if (strcmp(code[j], scan[h - 1]) != 0) {
+      continue;
     }
-    if (y != 0) {
-      for (i = 0; i < P - 1; i++) {
-        printf("%c", code[j][i]);
-      }
+    else {
+      printf("%s", code[j]);
       printf(" ");
-      for (i = 0; i < D; i++) {
-        printf("%c", name[j][i]);
-      }
-      printf(" %d ", cost[j]);
-      printf("%d%%\n", dis[j]);
+      printf("%s", name[j]);
+      printf(" %.0f ", cost[j]);
+      printf("%.0f%%\n", dis[j]);
       break;
     }
   }
 }
-void Case3(char scan[][P], char code[][P], char name[][D], int cost[],
-  int dis[], int N[], float sum1, float sum2, int num[], int h) {
-  int i, j, k, x, f, pro;
+void Case3(char scan[][P], char code[][P], char name[][D], float cost[],
+  float dis[], float sum1, float sum2, int num[], int h) {
+  int j, k, p, fl;
   for (k = 0; k < h; k++) {
-    x = 1;
     for (j = 0; j < D; j++) {
-      x = 1;
-      for (i = 0; i < P - 1; i++) {
-        if (code[j][i] != scan[k][i]) {
-          x = 0;
-          break;
-        }
+      if (strcmp(code[j], scan[k]) != 0) {
+        continue;
       }
-      if (x != 0) {
-        pro = 1;
-        for (f = 0; f < k; f++) {
-          if (N[f] == N[k]) {
-            pro = 0;
+      else {
+        fl = 1;
+        for (p = 0; p < k; p++) {
+          if (strcmp(scan[p], scan[k]) == 0) {
+            fl = 0;
             break;
           }
         }
-        if (pro == 1) {
-          for (i = 0; i < D; i++) {
-            printf("%c", name[j][i]);
-          }
+        if (fl == 1) {
+          printf("%s", name[j]);
           sum1 += cost[j];
           sum2 += 0.01f * dis[j] * cost[j];
-          printf(" %d", cost[j]);
+          printf(" %.0f", cost[j]);
           printf(" %d", num[k]);
-          printf(" %d\n", cost[j] * num[k]);
+          printf(" %.0f\n", cost[j] * num[k]);
         }
         else {
           sum1 += cost[j];
@@ -89,30 +67,29 @@ void Case3(char scan[][P], char code[][P], char name[][D], int cost[],
 void Case4() {
   printf("Thank you for your purchase!");
 }
-void numbers(int N[], int num[], int h) {
-  int i, j, u, x;
+void def() {
+  printf("Fatal -- Unavailable command\n");
+}
+void numbers(char scan[][P], int num[], int h) {
+  int i, j, u;
   for (i = 0; i < h; i++) {
     u = 0;
     for (j = 0; j < h; j++) {
-      if (N[i] == N[j]) {
+      if (strcmp(scan[i], scan[j]) == 0) {
         u++;
       }
     }
     num[i] = u;
   }
 }
-void pri1(char code[][P], char name[][D], int cost[], int dis[]) {
+void pri(char code[][P], char name[][D], float cost[], float dis[]) {
   int i, j, k, f;
   for (i = 0; i < D; i++) {
-    for (j = 0; j < P - 1; j++) {
-      printf("%c", code[i][j]);
-    }
+    printf("%s", code[i]);
     printf(" ");
-    for (k = 0; k < D; k++) {
-      printf("%c", name[i][k]);
-    }
-    printf(" %d", cost[i]);
-    printf(" %d%%\n", dis[i]);
+    printf("%s", name[i]);
+    printf(" %.0f", cost[i]);
+    printf(" %.0f%%\n", dis[i]);
   }
   printf("\n");
   printf("Choose an order:\n");
@@ -124,27 +101,29 @@ void pri1(char code[][P], char name[][D], int cost[], int dis[]) {
 int main() {
   char name[D][D] = { "apples","peachs","oranges","melons","grape","bread","butter","pears","kiwi","lemons" };
   char code[D][P] = { "1607","0505","1498","2332","6752","1351","0064","2353","9437","1027" }, scan[S][P];
-  int cost[D] = { 150,220,200,310,230,60,370,180,290,130 }, dis[D] = { 6,3,3,4,5,7,10,9,9,8 };
-  int ord, fl = 1, h = 0, b = 0, num[S], N[S];
+  float cost[D] = { 150.0f,220.0f,200.0f,310.0f,230.0f,60.0f,370.0f,180.0f,290.0f,130.0f };
+  float dis[D] = { 6.0f,3.0f,3.0f,4.0f,5.0f,7.0f,10.0f,9.0f,9.0f,8.0f };
+  int ord, fl = 1, h = 0, num[S];
   float sum2 = 0.0f, sum1 = 0.0f;
-  pri1(code, name, cost, dis);
+  pri(code, name, cost, dis);
   while (fl != 0) {
     ord = order();
     switch (ord) {
     case 1:
-      Case1(scan, N, h, b);
-      h++, b++;
+      Case1(scan, h++);
       break;
     case 2:
       Case2(code, name, cost, dis, scan, h);
       break;
     case 3:
-      numbers(N, num, h);
-      Case3(scan, code, name, cost, dis, N, sum1, sum2, num, h);
+      numbers(scan, num, h);
+      Case3(scan, code, name, cost, dis, sum1, sum2, num, h);
       break;
     case 4:
       Case4();
       return 0;
+    default:
+      def();
     }
   }
   return 0;
