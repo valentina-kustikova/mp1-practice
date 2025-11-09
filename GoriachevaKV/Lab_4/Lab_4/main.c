74b854180defc9c222895952e04f43b9c8b3ec35
KrissUhu <trototrop@gmail.com>
2025-11-10 02:22:29 +0300

    start working on lab 4

---
+++ GoriachevaKV/Lab_4/Lab_4/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int codes[5][4] = {
+{1, 1, 1, 1},
+{0, 1, 2, 3},
+{1, 2, 3, 4},
+{5, 6, 7, 8},
+{9, 0, 1, 2}
+};
+
+char names[5][20] = {
+"Pizza",
+"Cucumber",
+"Potato",
+"Chocolate",
+"Tea"
+};
+
+int cost[5] = { 100, 20, 50, 150, 300 };
+
+int discount = 0.85; // 15%
+
+
+int main() {
+
+
+	return 0;
+}
