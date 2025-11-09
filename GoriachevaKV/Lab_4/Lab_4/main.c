ec2b14c6965879cf91c041b11673bd76fe6e5d42
KrissUhu <trototrop@gmail.com>
2025-11-10 02:59:25 +0300

    added code scanner

--- GoriachevaKV/Lab_4/Lab_4/main.c
+++ GoriachevaKV/Lab_4/Lab_4/main.c
@@ -20,9 +20,25 @@ int cost[5] = { 100, 20, 50, 150, 300 };
 
 int discount = 0.85; // 15%
 
+int check_the_code(int* code, int code_array[]) {
+	int tmp = code;
+	for (int i = 3; i >= 0; i--) {
+		code_array[i] = tmp % 10;
+		tmp /= 10;
+	}
+}
+
 
 int main() {
-
+	int code;
+	int code_array[4];
+	while (1) {
+		printf("Input a four-number code\n");
+		scanf_s("%d", &code);
+		check_the_code(code, code_array);
+		printf("ARRAY CODE: %d %d %d %d\n\n", code_array[0], code_array[1], code_array[2], code_array[3]);
+		break;
+	}
 
 	return 0;
 }
