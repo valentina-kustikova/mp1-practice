df5dcd42767187b823b32f657e9246a9bf05433f
Yaroslav-Vasiliev <yarvasiliev@gmail.com>
2025-11-10 23:28:25 +0300

    korbi

--- VasilevYS/Korovi/Korovi/Korovi.c
+++ VasilevYS/Korovi/Korovi/Korovi.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <locale.h>
-#include <math.h>
-int checkv(int num, int n)
+int checkSize(int num, int n)
 {
     int cnt = 0;
     while (num > 0)
@@ -19,88 +18,65 @@ int checkv(int num, int n)
 }
 int main()
 {
-    int n, check = 0, temp, i, a[5], j, b[5] = { 0 }, num = 0, ans = 0, bull = 0, cow = 0;
+    int n, used[9] = { 0 }, temp, i, a[5], j = 0;
+    int b[5] = { 0 }, num = 0, ans = 0, bull = 0, cow = 0;
     setlocale(LC_ALL, "Russian");
     srand((unsigned int)time(0));
     do {
         printf("Длина числа(от 2 до 5): ");
         scanf_s("%d", &n);
     } while (n < 2 || n>5);
-    for (i = 0; i < n; i++)
-    {
-        a[i] = rand() % 9 + 1;
-
-    }
-    while (check != 1)
-    {
-        check = 1;
-        for (i = 0; i < n; i++)
-        {
-            for (j = 0; j < n; j++)
-            {
-                if (a[i] == a[j] && i != j)
-                {
-                    a[i] = rand() % 9 + 1;
-                }
-            }
-        }
-        for (i = 0; i < n; i++)
-        {
-            for (j = 0; j < n; j++)
-            {
-                if (a[i] == a[j] && i != j)
-                {
-                    check = 0;
-                }
-            }
-        }
-    }
-    for (i = 0; i < n; i++)
-    {
-        num += a[i] * pow(10, n - 1 - i);
-    }
-    while (num != ans)
-    {
-
+    a[0] = rand() % 9 + 1;
+    num = a[0];
+    used[a[0]]++;
+    for (i = 1; i < n; i++)
+    {  
         do
         {
-            printf("Ваше число длинной из %d cимволов: ", n);
-            scanf_s("%d", &ans);
-        } while (checkv(ans, n) != 1);
-        if (ans == num)
-            printf("Верно");
-        else
+            a[i] = rand() % 9 + 1;
+        } while (used[a[i]] != 0);
+        num = num * 10 + a[i];
+        used[a[i]]++;
+    } 
+    do {
+        cow = 0;
+        bull = 0;
+        do
         {
-            for (i = 0; i < n; i++)
+            printf("Ваше число длиной из %d cимволов: ", n);
+            scanf_s("%d", &ans);
+        } while (checkSize(ans, n) != 1);
+        for (i = 0; i < n; i++)
+        {
+            temp = ans % 10;
+            b[n - 1 - i] = temp;
+            ans /= 10;
+        }
+        for (i = 0; i < n; i++)
+        {
+            if (b[i] == a[i])
             {
-                temp = ans % 10;
-                b[n - 1 - i] = temp;
-                ans /= 10;
+                bull++;
+                continue;
             }
-            for (i = 0; i < n; i++)
+            else
             {
-                if (b[i] == a[i])
+                for (j = 0; j < n; j++)
                 {
-                    bull++;
-                    continue;
-                }
-                else
-                {
-                    for (j = 0; j < n; j++)
+                    if (b[i] == a[j] && i != j)
                     {
-                        if (b[i] == a[j] && i != j)
-                        {
-                            cow++;
-                            continue;
-                        }
+                        cow++;
+                        continue;
                     }
                 }
             }
+        }
+        if (bull != n)
+        {
             printf("Быков: %d\n", bull);
             printf("Коров: %d\n", cow);
-            cow = 0;
-            bull = 0;
         }
-    }
+    } while (bull != n);
+    printf("Верно");
     return 0;
 }
