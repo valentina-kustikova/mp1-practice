e5e6e62c5fd42c6c289700d6687644c5eee69901
work3r1337 <c.michasov2004@yandex.ru>
2025-11-10 14:19:18 +0300

    Fix barcode

--- MichasovKS/lab-4/lab-4.c
+++ MichasovKS/lab-4/lab-4.c
@@ -1,19 +1,20 @@
 ﻿#include <stdio.h>
+#include <string.h>
 
 #define MAX_NAME_SIZE 10
 #define RECORDS_SIZE 10
-#define MAX_BARCODE 10000
+#define MAX_BARCODE 4
 
 typedef struct Record {
-	int barcode;
+	char barcode[MAX_BARCODE+1];
 	char name[MAX_NAME_SIZE];
 	int price;
 	int discount;
 } Record;
 
-int scan(void);
+void scan(char barcode[]);
 void print_record(Record record);
-int find_record(int barcode, Record records[]);
+int find_record(char barcode[], Record records[]);
 void print_check(int check[], Record records[]);
 void print_amount(int check[], Record records[]);
 
@@ -22,18 +23,18 @@ int check[MAX_BARCODE];
 int main()
 {
 	Record records[RECORDS_SIZE];
-	records[0] = (Record){ 1234, "Milk", 100, 10 };
-	records[1] = (Record){ 1278, "Beer", 110, 15 };
-	records[2] = (Record){ 1534, "Pickles", 130, 5 };
-	records[3] = (Record){ 7234, "Yogurt", 75, 7 };
-	records[4] = (Record){ 1894, "Pate", 122, 3 };
-	records[5] = (Record){ 6734, "Spam", 90, 5 };
-	records[6] = (Record){ 8934, "Onion", 30, 3 };
-	records[7] = (Record){ 1234, "Potato", 40, 2 };
-	records[8] = (Record){ 9274, "Coffee", 300, 20 };
-	records[9] = (Record){ 7254, "Milk", 100, 10 };
+	records[0] = (Record){ "1234", "Milk", 100, 10 };
+	records[1] = (Record){ "1278", "Beer", 110, 15 };
+	records[2] = (Record){ "1534", "Pickles", 130, 5 };
+	records[3] = (Record){ "7234", "Yogurt", 75, 7 };
+	records[4] = (Record){ "1894", "Pate", 122, 3 };
+	records[5] = (Record){ "6734", "Spam", 90, 5 };
+	records[6] = (Record){ "8934", "Onion", 30, 3 };
+	records[7] = (Record){ "1234", "Potato", 40, 2 };
+	records[8] = (Record){ "9274", "Coffee", 300, 20 };
+	records[9] = (Record){ "7254", "Milk", 100, 10 };
 
-	int curr_barcode = -1;
+	char curr_barcode[MAX_BARCODE+1] = "----";
 	int running = 1;
 	while (running) {
 		int command;
@@ -41,13 +42,13 @@ int main()
 		scanf_s("%d", &command);
 		switch (command) {
 		case 1:
-			curr_barcode = scan();
+			scan(curr_barcode);
 			break;
 		case 2:
 			if (find_record(curr_barcode, records) != -1) {
 				print_record(records[find_record(curr_barcode, records)]);
 			}
-			break;
+			break;/*
 		case 3:
 			if (find_record(curr_barcode, records) != -1) {
 				check[curr_barcode]++;
@@ -59,6 +60,7 @@ int main()
 		case 5:
 			print_amount(check, records);
 			break;
+			*/
 		case 0:
 			running = 0;
 			break;
@@ -66,21 +68,20 @@ int main()
 	}
 }
 
-int scan(void) {
-	int barcode;
+void scan(char barcode[]) {
 	printf("Enter barcode: ");
-	scanf_s("%d", &barcode);
+	scanf_s("%s", barcode, MAX_BARCODE+1);
 	return barcode;
 }
 
 void print_record(Record record) {
-	printf("%-7d %-11d %-5d %-s\n", record.barcode, record.discount, record.price, record.name);
+	printf("%-7s %-11d %-5d %-s\n", record.barcode, record.discount, record.price, record.name);
 }
 
-int find_record(int barcode, Record records[]) {
+int find_record(char barcode[], Record records[]) {
 	int i;
 	for (i = 0; i < RECORDS_SIZE; i++) {
-		if (records[i].barcode == barcode) return i;
+		if (strcmp(records[i].barcode, barcode) == 0) return i;
 	}
 	return -1;
 }
