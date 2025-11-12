03e05b18f7b6ec8db30148fb4fe7f77ba425f923
mashamalkova <masha.malkova.2007@yandex.ru>
2025-11-12 15:56:46 +0300

    task4

--- MalkovaMM/task4/task4/task4.c
+++ MalkovaMM/task4/task4/task4.c
@@ -4,12 +4,15 @@
 #include <locale.h>
 #include <string.h>
 
+int korzina[10] = { 0 };
+
 int skidka()
 {
 	int s;
 	s = rand() % (50) + 1;
 	return s;
 }
+
 void wehave(char* name[], char* qr[], int count[], int skidki[])
 {
 	char* name1[] = { "яблоко","банан","виноград","куриное филе","картофель (1кг)", "молоко", "творог", "колбаса", "йогурт", "вода (0.5 литра)" };
@@ -26,44 +29,42 @@ void wehave(char* name[], char* qr[], int count[], int skidki[])
 	}
 }
 
-int main()
+void check(int korzina[], char* name[], int skidki[], int count[])
 {
-	setlocale(LC_ALL, "Rus");
-	srand((unsigned int)time(0));
-	char* name[10];
-	char* qr[10];
-	int count[10];
-	int skidki[10];
-	int i = 0;
-	int ch = -1;
-	char qrnow[5] = "9999";
-	int korzina[10] = { 0 };
 	int sumk = 0;
 	int sumsk = 0;
-	wehave(name, qr, count, skidki);
-	printf("В наличии: \n");
-	for (i=0; i < 10; i++)
-	{
-		printf("Qr: %s \n", qr[i]);
-		printf("Наименование: %s \n", name[i]);
-		printf("Цена: %d руб \n \n", count[i]);
-	}
-	do
-	{
-		i = 0;
-		printf("Отсканируйте первый товар: \n");
-		scanf_s("%s", qrnow, (unsigned)sizeof(qrnow));
-		for (; i < 10; i++)
-		{
-			if (strcmp(qrnow, qr[i]) == 0)
-				break;
+	int i;
+	for (i = 0; i < 10; i++) {
+		if (korzina[i] > 0) {
+			printf("Наименование: %s \n", name[i]);
+			printf("Цена за единицу: %d руб \n", count[i]);
+			printf("Количество: %d \n", korzina[i]);
+			printf("Скидка: %d%% \n", skidki[i]);
+			printf("Общая цена: %d руб \n \n", (count[i] * korzina[i] - (count[i] * korzina[i]) * (skidki[i]) / 100));
+			sumk += count[i] * korzina[i];
+			sumsk += (count[i] * korzina[i]) * (skidki[i]) / 100;
 		}
-	} while (i == 10);
-	korzina[i]++;
+	}
+	if (sumk == 0) {
+		printf("чек пуст");
+	}
+	else {
+		printf("общая стоимость: %d руб \n", sumk);
+		printf("суммарная скидка: %d руб \n", sumsk);
+		printf("итоговая сумма к оплате: %d руб", sumk - sumsk);
+	}
+}
+
+void scan(char* qr[])
+{
+	int ch = -1;
+	int sc = -1;
+	int i = 0;
+	char qrnow[5] = "9999";
 	do {
 		do
 		{
-			printf("Если хотите отсканировать следующий товар - 0, если хотите получить чек - 1: ");
+			printf("Если хотите отсканировать товар - 0, если хотите получить чек - 1: ");
 			scanf_s("%d", &ch);
 		} while (ch != 0 && ch != 1);
 		if (ch == 0) {
@@ -78,25 +79,37 @@ int main()
 						break;
 				}
 			} while (i == 10);
-			korzina[i]++;
+			printf("Добавить товар? Да - 0: ");
+			scanf_s("%d", &sc);
+			if (sc==0)
+				korzina[i]++;
 		}
 	} while (ch != 1);
-	/*for (i = 0; i < 10; i++) {
-	 printf("%d", korzina[i]);
-	}*/
-	for (i = 0; i < 10; i++) {
-		if (korzina[i] > 0) {
-			printf("Наименование: %s \n", name[i]);
-			printf("Цена за единицу: %d руб \n", count[i]);
-			printf("Количество: %d \n", korzina[i]);
-			printf("Скидка: %d%% \n", skidki[i]);
-			printf("Общая цена: %d руб \n \n", (count[i] * korzina[i] - (count[i] * korzina[i]) * (skidki[i]) / 100));
-			sumk += count[i] * korzina[i];
-			sumsk += (count[i] * korzina[i]) * (skidki[i]) / 100;
-		}
+}
+
+void menu(char* qr[], char* name[], int count[]) {
+	int i = 0;
+	printf("В наличии: \n");
+	for (i = 0; i < 10; i++)
+	{
+		printf("Qr: %s \n", qr[i]);
+		printf("Наименование: %s \n", name[i]);
+		printf("Цена: %d руб \n \n", count[i]);
 	}
-	printf("общая стоимость: %d \n", sumk);
-	printf("суммарная скидка: %d \n", sumsk);
-	printf("итоговая сумма к оплате: %d", sumk - sumsk);
+}
+
+
+int main()
+{
+	char* name[10];
+	char* qr[10];
+	int count[10];
+	int skidki[10];
+	setlocale(LC_ALL, "Rus");
+	srand((unsigned int)time(0));
+	wehave(name, qr, count, skidki);
+	menu(qr, name, count);
+	scan(qr);
+	check(korzina, name, skidki, count);
 	return 0;
 }
