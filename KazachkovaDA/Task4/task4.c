21b0ee2a781ebc9d28e52e94fcab9cfee7411c2c
KazachkovaDaria <kazackovadara5@gmail.com>
2025-11-26 15:48:46 +0300

    final

--- KazachkovaDA/Task4/task4.c
+++ KazachkovaDA/Task4/task4.c
@@ -6,10 +6,16 @@
 #define N 1000
 #define sizedb 15
 
-char* given_codes[sizedb] = { "0001", "0002", "0003", "0004", "0005", "0006", "0007", "0008", "0009", "0010", "0011", "0012", "0013", "0014", "0015"};
-char* name[sizedb] = { "apples", "milk", "bananas", "bread", "juice", "ketchup", "mayonaise", "eggs", "cheese", "tomatoes", "sausages", "salad", "meat", "butter", "fish"};
-int price[sizedb] = {100, 90, 75, 30, 300, 80, 75, 60, 140, 200, 300, 250, 400, 170, 270};
-float discount[sizedb] = {0.15, 0.10, 0.25, 0.35, 0.5, 0.20, 0.40, 0.35, 0.10, 0.35, 0.45, 0.10, 0.10, 0.20, 0.40};
+char* given_codes[sizedb] = { "0001", "0002", "0003", "0004", "0005", "0006",
+							  "0007", "0008", "0009", "0010", "0011", "0012",
+							  "0013", "0014", "0015"};
+char* name[sizedb] = { "apples", "milk", "bananas", "bread", "juice",
+					   "ketchup", "mayonaise", "eggs", "cheese", "tomatoes",
+	                   "sausages", "salad", "meat", "butter", "fish"};
+int price[sizedb] = {100, 90, 75, 30, 300, 80, 75, 60, 140, 200, 300, 250, 400,
+					 170, 270};
+float discount[sizedb] = {0.15, 0.10, 0.25, 0.35, 0.5, 0.20, 0.40, 0.35, 0.10,
+						  0.35, 0.45, 0.10, 0.10, 0.20, 0.40};
 int cnt[sizedb] = { 0 };//массив для подсчета количества товаров
 
 int scan(char code[]);
@@ -84,7 +90,10 @@ void print_receipt()
 	for (i = 0; i < sizedb; i++) {
 		if (cnt[i] > 0)
 		{
-			printf_s("Code: %s, name: %s, price: %d, count: %d, discount: %.2f = %.2f\n", given_codes[i], name[i], price[i], cnt[i], cnt[i] * price[i] * discount[i], cnt[i]*(price[i] - price[i] * discount[i]));
+			printf_s("Code: %s, name: %s, price: %d, count: %d, discount: %.2f = %.2f\n",
+				given_codes[i], name[i], price[i], cnt[i],
+				cnt[i] * price[i] * discount[i],
+				cnt[i]*(price[i] - price[i] * discount[i]));
 			total += cnt[i]*(price[i] - price[i] * discount[i]);
 		}
 	}
