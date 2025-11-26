1cb554cf261e49624506f14faa6ab12eef733c17
Sergey-Eroshkin <seregaeroschkin@mail.ru>
2025-11-26 15:57:02 +0300

    Added dynamic memory allocation(doesn't work)

--- EroshkinSA/Task5.1/Source.c
+++ EroshkinSA/Task5.1/Source.c
@@ -4,7 +4,9 @@
 #include <time.h>
 #include <windows.h>
 
-char name[1000][256] = { "" };
+int* sz;
+int* num;
+char** name;
 
 void swap(int* a, int* ai, int* b, int* bi) {
 	int tmp = *b, tmpi = *bi;
@@ -14,67 +16,67 @@ void swap(int* a, int* ai, int* b, int* bi) {
 	*ai = tmpi;
 }
 
-void BubbleSort(int num[], int v[], int n) {
+void BubbleSort(int n) {
 	int i, j;
 	for (i = 0; i < n - 1; i++) {
 		for (j = 0; j < n - i - 1; j++) {
-			if (v[j] > v[j + 1]) swap(&v[j], &num[j], &v[j + 1], &num[j + 1]);
+			if (sz[j] > sz[j + 1]) swap(&sz[j], &num[j], &sz[j + 1], &num[j + 1]);
 		}
 	}
 }
 
-void ClassicSort(int num[], int v[], int n) {
+void ClassicSort(int n) {
 	int i, j;
 	for (i = 0; i < n; i++) {
 		for (j = i + 1; j < n; j++) {
-			if (v[j] < v[i]) swap(&v[i], &num[i], &v[j], &num[j]);
+			if (sz[j] < sz[i]) swap(&sz[i], &num[i], &sz[j], &num[j]);
 		}
 	}
 }
 
-void ChoiceSort(int num[], int v[], int n) {
+void ChoiceSort(int n) {
 	int i, j;
 	for (i = 0; i < n; i++) {
-		int min = v[i], idx = i;
+		int min = sz[i], idx = i;
 		for (j = i + 1; j < n; j++) {
-			if (v[j] < min) {
-				min = v[j];
+			if (sz[j] < min) {
+				min = sz[j];
 				idx = j;
 			}
 		}
-		swap(&v[i], &num[i], &v[idx], &num[idx]);
+		swap(&sz[i], &num[i], &sz[idx], &num[idx]);
 	}
 }
 
-void InsertSort(int num[], int v[], int n) {
+void InsertSort(int n) {
 	int i;
 	for (i = 0; i < n; i++) {
-		int j = i - 1, tmp = v[i];
-		while (j >= 0 && v[j] > tmp) {
-			v[j + 1] = v[j];
+		int j = i - 1, tmp = sz[i];
+		while (j >= 0 && sz[j] > tmp) {
+			sz[j + 1] = sz[j];
 			num[j + 1] = num[j];
 			j--;
 		}
-		v[j + 1] = tmp;
+		sz[j + 1] = tmp;
 		num[j + 1] = i;
 	}
 }
 
-void RadixSort(int num[], int v[], int n) {
+void RadixSort(int n) {
 	int digitn[10][1000] = { {-1} }, digitv[10][1000] = { {-1} }, div = 1, cnt, d, i, p[10] = { 0 }, k, j;
 	do {
 		cnt = 0;
 		for (i = 0; i < n; i++) {
-			d = (v[i] / div) % 10;
+			d = (sz[i] / div) % 10;
 			if (d) cnt++;
 			digitn[d][p[d]] = num[i];
-			digitv[d][p[d]++] = v[i];
+			digitv[d][p[d]++] = sz[i];
 		}
 		k = 0;
 		for (d = 0; d < 10; d++) {
 			for (j = 0; j < p[d]; j++) {
 				num[k] = digitn[d][j];
-				v[k++] = digitv[d][j];
+				sz[k++] = digitv[d][j];
 			}
 			p[d] = 0;
 		}
@@ -84,31 +86,51 @@ void RadixSort(int num[], int v[], int n) {
 
 void path(char dir[]);
 
-int read(char dir[], int num[], int sz[], int pos) {
-	int k = pos;
+int read(char dir[]) {
+	int k = 0;
 	WIN32_FIND_DATA data;
 	HANDLE hFind = FindFirstFileA(dir, &data);
 	if (hFind != INVALID_HANDLE_VALUE) {
 		do {
-			if (data.dwFileAttributes == 16) {
+			/*if (data.dwFileAttributes == 16) {
 				char newdir[1024] = "";
 				strncpy(newdir, dir, strlen(dir) - 1);
 				strcat(newdir, data.cFileName);
 				path(newdir);
 				if (strcmp(data.cFileName, ".") != 0 && strcmp(data.cFileName, "..") != 0) {
 					printf("Open directory: %s\n", newdir);
-					k = read(newdir, num, sz, k);
+					//k = read(newdir, num, sz, k);
 					printf("Close directory: %s\n", newdir);
 				}
 				continue;
-			}
+			}*/
+			if (data.dwFileAttributes != 32) continue;
+			printf("Read: %s of %d bytes\n", data.cFileName, data.nFileSizeLow);
+			//strcpy(name[k], data.cFileName);
+			//sz[k] = data.nFileSizeLow;
+			//num[k] = k;
+			k++;
+		} while (FindNextFileA(hFind, &data));
+		FindClose(hFind);
+	}
+	sz = (int*)malloc(k * sizeof(int));
+	num = (int*)malloc(k * sizeof(int));
+	name = (char**)malloc(k * sizeof(char*));
+	for (int i = 0; i < k; i++) {
+		name[i] = (char*)malloc(256 * sizeof(char));
+	}
+	k = 0;
+	WIN32_FIND_DATA data;
+	HANDLE hFind = FindFirstFileA(dir, &data);
+	if (hFind != INVALID_HANDLE_VALUE) {
+		do {
 			if (data.dwFileAttributes != 32) continue;
 			printf("Read: %s of %d bytes\n", data.cFileName, data.nFileSizeLow);
 			strcpy(name[k], data.cFileName);
 			sz[k] = data.nFileSizeLow;
 			num[k] = k;
 			k++;
-		} while (FindNextFileA(hFind, &data) && k < 1000);
+		} while (FindNextFileA(hFind, &data));
 		FindClose(hFind);
 	}
 	return k;
@@ -118,7 +140,7 @@ void path(char dir[]) {
 	strcat(dir, add);
 }
 
-char fsort(char method[], int num[], int sz[], int k) {
+char fsort(char method[], int k) {
 	if (strcmp(method, "classic") == 0) {
 		ClassicSort(num, sz, k); return 1;
 	}
@@ -142,7 +164,6 @@ int main() {
 	char method[50], dir[1000] = "";
 	int i, k = 0, type = -1, f;
 	char ways[5][50] = { "classic", "insert", "choice", "bubble", "radix" };
-	int num[1000], sz[1000];
 	printf("Instruction:\n1 <path_to_directory> - Enter of usage directory\n2 - Read of files from entered directory\n3 <sort_type> - Sort and print a list of files and time of sort\n0 - Exit\n");
 	while (type != 0) {
 		scanf("%d%*c", &type);
@@ -155,7 +176,7 @@ int main() {
 			if (strcmp(dir, "") == 0) {
 				printf("Nothing to read!\n");
 			}
-			else k = read(dir, num, sz, 0);
+			else k = read(dir);
 			break;
 		case 3:
 			if (k == 0) {
@@ -164,7 +185,7 @@ int main() {
 			}
 			scanf("%s", method);
 			clock_t a = clock();
-			f = fsort(method, num, sz, k);
+			f = fsort(method, k);
 			clock_t b = clock();
 			if (f) {
 				for (i = 0; i < k; i++) printf("%d) %s -> %d\n", i + 1, name[num[i]], sz[i]);
@@ -177,5 +198,11 @@ int main() {
 			printf("Incorrect input!\n");
 		}
 	}
+	free(sz);
+	free(num);
+	for (int i = 0; i < k; i++) {
+		free(name[i]);
+	}
+	free(name);
 	return 0;
 }
