411f58b95183c9fb7378096f84077c86500590a2
Yaroslav-Vasiliev <yarvasiliev@gmail.com>
2025-11-26 16:16:08 +0300

    dd

--- VasilievYS/kassa/kassa/Source.c
+++ VasilievYS/kassa/kassa/Source.c
@@ -15,24 +15,25 @@ struct tovar bd[N] = { {"1000", "Вода 0,5л", 35.f, 10},
 		{ "1111", "Хлеб", 50.f, 5 } };
 int cnt_t[N] = { 0 };
 struct tovar scanned;
-void scan()
+int scan()
 {
-	int i;
+	int i, idx = -1;
 	char vvod[5];
 	printf("Введите код: ");
 	scanf_s("%s", vvod, (unsigned)sizeof(vvod));
-	scanned.price = 0;
 	for (i = 0; i < N; i++)
 	{
 		if (strcmp(vvod, bd[i].kod) == 0)
 		{
 			scanned = bd[i];
+			idx = i;
 		}
 	}
-	if (scanned.price == 0)
+	if (idx == -1)
 	{
 		printf("\nТакого товара нет.");
 	}
+	return idx;
 }
 void print(struct tovar n_t)
 {
@@ -47,80 +48,61 @@ void add(struct tovar n_t)
 		if (strcmp(n_t.kod, bd[i].kod) == 0)
 		{
 			cnt_t[i]++;
+			break;
 		}
 	}
 }
 void make_bill()
 {
-	int i = 0, cnt = 0;
+	int i = 0;
 	float price = 0.f;
 	for (i = 0; i < N; i++)
 	{
 		if (cnt_t[i] != 0)
 		{
-			cnt += cnt_t[i];
 			price += bd[i].price * (1 - 0.01 * bd[i].sale);
 			printf("\nНазвание: %s  Цена: %.2f  Скидка: %d%%  Цена со скидкой: %.2f  Количество: %d\n",
 				(bd[i].name), (bd[i].price), (bd[i].sale), bd[i].price * (1 - 0.01 * bd[i].sale), cnt_t[i]);
 		}
 
 	}
-	printf("\nВсего товаров: %d  Цена: %.2f\n", cnt, price);
+	printf("\nЦена: %.2f\n", price);
 }
 int main()
 {
-
-	int ans, i, res = 0;
+	int ans, ans2, i, res = 0, idx=0;
 	setlocale(LC_ALL, "Russian");
-	do
+  do////
 	{
-		printf("Сканировать товар - 1, завершить - 0: ");
+		if (res == 0){ printf("\nСканировать товар - 1, Завершить - 0: "); }
+		else { printf("\nСканировать товар - 1, Сформировать чек - 2, Завершить - 0: "); }
 		scanf_s("%d", &ans);
-	} while (ans != 1 && ans != 0);
-	while (ans != 0)
-	{
-		if (ans == 2)
+		switch (ans)
 		{
-			make_bill();
-		}
-		if (ans == 1)
-		{
+		case 1:
 			do
 			{
-				scan();
-			} while (scanned.price == 0);
+				idx = scan();
+			} while ( idx == -1);
 			do
 			{
 				printf("\nВывести информацию о товаре - 1, добавить товар в чек - 2, Назад - 0: ");
-				scanf_s("%d", &ans);
-				if (ans == 1)
+				scanf_s("%d", &ans2);
+
+				switch (ans2)
 				{
+				case 1:
 					print(scanned);
+
+				case 2:
+					add(scanned);
+					res++;
 				}
-			} while (ans != 2 && ans != 0);
-			if (ans == 2)
-			{
-				add(scanned);
-				res++;
-			}
-
+			} while (ans2 != 0 && ans2 != 2);
+		case 2:
+			make_bill();
 		}
-
-		do
-		{
-			if (res > 0)
-			{
-				printf("\nСканировать товар - 1, Сформировать чек - 2, Завершить - 0: ");
-				scanf_s("%d", &ans);
-			}
-			else
-			{
-				printf("\nСканировать товар - 1, завершить - 0: ");
-				scanf_s("%d", &ans);
-			}
-
-		} while (ans != 1 && ans != 0 && ans != 2);
-	}
+	} while (ans != 0);
 	if (res > 0)
 	{
 		printf("\nИтоговый чек: ");
