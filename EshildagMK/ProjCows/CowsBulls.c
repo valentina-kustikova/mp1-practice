712a7b664476a7ada47950d0af1f3c9632e05fc0
Maxesh <maximus6110@gmail.com>
2025-12-02 20:35:08 +0300

    EshildagMK lab3-fixed

--- EshildagMK/ProjCows/CowsBulls.c
+++ EshildagMK/ProjCows/CowsBulls.c
@@ -2,26 +2,24 @@
 #include <time.h>
 #include <stdlib.h>
 
-int formNum(int n);
+int formNum(int n, int* gm_digit);
 
 int main() {
-	int gnum = 0, plnum, i = 0, j = 0, ncount, numi, numj, ic = 0, ib = 0, temp, kansw = 0;
-	int cows[5] = { 11,11,11,11,11 };
-	int bulls[5] = { 11,11,11,11,11 };
-	int pl_digit[5] = { 11,11,11,11,11 };
-	int gm_digit[5] = { 11,11,11,11,11 };
+	int gnum = 0, plnum, i = 0, j = 0, ncount, ic = 0, ib = 0, temp;
+	int pl_digit[5] = { 0, 0, 0, 0, 0 };
+	int gm_digit[5] = { 0, 0, 0, 0, 0 };
+
+	srand(time(NULL));
 
 	printf("Enter ammount of numbers:\n");
 	scanf_s("%d", &ncount);
-	gnum = formNum(ncount);
-	printf("%d \n", gnum);
-	while (kansw != ncount) {
-		kansw = 0; ic = 0; ib = 0; i = 0;
+	gnum = formNum(ncount, gm_digit);
+	i = 0;
+
+	while (ib != ncount) {
+		ic = 0; ib = 0; i = 0;
 		for (i = 0; i < 5; i++) {
-			cows[i] = 11;
-			bulls[i] = 11;
-			pl_digit[i] = 11;
-			gm_digit[i] = 11;
+			pl_digit[i] = 0;
 		}
 		i = 0;
 		printf("Enter your number:\n");
@@ -33,52 +31,30 @@ int main() {
 			temp /= 10;
 			i++;
 		}
-		temp = gnum;
-		i = 0;
-
-		while (temp > 0) {
-			gm_digit[i] = temp % 10;
-			temp /= 10;
-			i++;
-		}
 		i = 0;
 
 		for (i = 0; i < ncount; i++) {
 			if (pl_digit[i] == gm_digit[i]) {
-				bulls[ib++] = pl_digit[i];
-				kansw++;
+				ib++;
 			}
 			else {
 				for (j = 0; j < ncount; j++) {
 					if (pl_digit[i] == gm_digit[j]) {
-						cows[ic++] = pl_digit[i];
+						ic++;
 					}
 				}
 			}
 		}
 
-		printf("Cows: ");
-		for (i = 0; i < ncount; i++) {
-			if (cows[i] < 10) {
-				printf("%d ", cows[i]);
-			}
-			else continue;
-		}
-		printf("\nBulls: ");
-		for (i = 0; i < ncount; i++) {
-			if (bulls[i] < 10) {
-				printf("%d ", bulls[i]);
-			}
-			else continue;
-		}
+		printf("Cows: %d", ic);
+		printf("\nBulls: %d", ib);
 		printf("\n");
 	}
-	printf("YOU WON!");
+	printf("YOU WON!  %d", gnum);
 }
 
-int formNum(int n) {
+int formNum(int n, int* gm_digit) {
 	int i = 0, rnum, s = 0;
-	srand(time(NULL));
 	int UsedNums[10] = { 0 };
 	while (i < n) {
 		rnum = rand() % 10;
@@ -88,6 +64,7 @@ int formNum(int n) {
 		UsedNums[rnum] = 1;
 		s *= 10;
 		s += rnum;
+		gm_digit[n - i - 1] = rnum;
 		i++;
 	}
 	return s;
