78db2ae69592b10d1372633d9b24d2c71380db06
AntonKirin11 <antonkirin11@gmail.com>
2025-12-07 21:46:53 +0300

    pravki

--- KartashovAP/lab4/lab4/main.c
+++ KartashovAP/lab4/lab4/main.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 #include <locale.h>
 #define K 5
 
 int main() {
     setlocale(LC_ALL, "Russian");
 
-    int basket, copybasket;
-    int i, j, flag;
+    char basket[5];
+    char copybasket[5];
+    int i, flag;
     float summskidka = 0.0;
     int summ = 0;
-    int barcode[4];
+    int pr;
 
-    int shtrixtov[K][4] = { {1,1,1,1}, {2,2,2,2}, {3,3,3,3}, {4,4,4,4}, {5,5,5,5} };
+
+    char shtrixtov[K][5] = { "1111", "2222", "3333", "4444", "5555" };
     char nazvanie[K][7] = { "Cheese", "Cake", "Curd", "Meat", "Tea" };
     int kol[K] = { 0 };
     int tsena[K] = { 1000, 500, 2000, 700, 250 };
@@ -19,60 +22,60 @@ int main() {
 
     do {
         printf("Введите штрихкод товара (1111-5555) или 0 для завершения: ");
-        scanf_s("%d", &basket);
-		int pr;
+        scanf_s("%4s", basket, (unsigned)sizeof(basket));
 
-		if (basket == 0) {
-			break;
-		}
+        while (getchar() != '\n');
 
-        copybasket = basket;
-        for (i = 3; i >= 0; i--) {
-            barcode[i] = copybasket % 10;
-            copybasket = copybasket / 10;
+        if (strcmp(basket, "0") == 0) {
+            break;
         }
+
+        strcpy_s(copybasket, sizeof(copybasket), basket);
 
         flag = 0;
         for (i = 0; i < K; i++) {
-            flag = 1;
-            for (j = 0; j < 4; j++) {
-                if (barcode[j] != shtrixtov[i][j]) {
-                    flag = 0;
-                    break;
+            if (strcmp(copybasket, shtrixtov[i]) == 0) {
+                flag = 1;
+                printf("Отсканированный товар: %s (Цена: %d, Скидка: %d%%)\n",
+                    nazvanie[i], tsena[i], skidka[i]);
+                printf("Добавим товар? Если да введите 11, если нет то введите 00: ");
+                scanf_s("%d", &pr);
+
+
+                while (getchar() != '\n');
+
+                if (pr == 11) {
+                    kol[i] += 1;
+                    printf("Товар добавлен: %s (Цена: %d, Скидка: %d%%)\n",
+                        nazvanie[i], tsena[i], skidka[i]);
                 }
-            }
-            if (flag == 1) {
-				printf("Отсканированный товар: %s (Цена: %d, Скидка: %d%%)\n", nazvanie[i], tsena[i], skidka[i]);
-				printf("Добавим товар? Если да введите 11, если нет то введите 00: ");
-				scanf_s("%d", &pr);
-				if (pr == 11) {
-					kol[i] += 1;
-					printf("Товар добавлен: %s (Цена: %d, Скидка: %d%%)\n", nazvanie[i], tsena[i], skidka[i]);
-				}
-				else {
-					printf("Отсканируйте другой товар\n");
-				}
+                else {
+                    printf("Отсканируйте другой товар\n");
+                }
                 break;
             }
         }
 
         if (flag == 0) {
-            printf("Товар с штрих-кодом %d не найден!\n", basket);
+            printf("Товар с штрих-кодом %s не найден!\n", basket);
         }
 
-    } while (basket !=0);
+    } while (1);
 
-    printf("ВАШ ЧЕК\n");
+    printf("\nВАШ ЧЕК\n");
+    printf("========================\n");
     for (i = 0; i < K; i++) {
         if (kol[i] != 0) {
             int total = kol[i] * tsena[i];
-            printf("%s - %d руб. x %d шт. = %d руб.\n", nazvanie[i], tsena[i], kol[i], total);
+            printf("%s - %d руб. x %d шт. = %d руб.\n",
+                nazvanie[i], tsena[i], kol[i], total);
             summ += total;
             summskidka = summskidka + (total * (skidka[i] / 100.0));
         }
     }
 
     printf("\nИтог:\n");
+    printf("========================\n");
     printf("Общая сумма: %d руб.\n", summ);
     printf("Скидка: %.2f руб.\n", summskidka);
     printf("К оплате: %.2f руб.\n", summ - summskidka);
