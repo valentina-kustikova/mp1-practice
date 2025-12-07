ae94a12f9505548ab6c4d4f32974a7d7f01fb405
zarudnijevgenij <zarudnij07@mail.ru>
2025-12-07 23:07:27 +0300

    Homework_3

--- homework_3/homework_3/main.c
+++ homework_3/homework_3/main.c
@@ -3,73 +3,83 @@
 #include <time.h>
 
 int main() {
-    int n, a, temp, dig1, dig2, k, len, coe1 = 10, coe2 = 10, i, copy_a, bull, cow, pos_n, pos_a;
-    printf("choose the length of the number\n");
+    int length;
     do {
-        scanf_s("%d", &len);
-    } while ((len < 2) || (len > 5));
-    for (i = 1; i < len - 1; i++) {
-        coe1 *= 10;
-        coe2 *= 10;
-    }
-    coe1 *= 10;
-    //generation number
-    srand((unsigned int)time(0));
-    a = rand() % (coe1 - coe2) + coe2;
-    copy_a = a;
-    //printf("%d\n", a);
-    while (copy_a > 0) {
-        k = 1;
-        dig1 = copy_a % 10;
-        temp = copy_a / 10;
-        while (temp > 0) {
-            dig2 = temp % 10;
+        printf("Enter number's lenght: ");
+        scanf_s("%d", &length);
+    } while (length < 2 || length > 5);
 
-            if (dig1 == dig2) {
-                k = 0;
-            }
+
+    int number[5];
+    int used_digits[10] = { 0 }; 
+    srand((unsigned int)time(0));
+
+    for (int i = 0; i < length; i++) {
+        int digit;
+        do {
+            digit = rand() % 9 + 1; 
+        } while (used_digits[digit]);
+        number[i] = digit;
+        used_digits[digit] = 1;
+    }
+
+ 
+    printf("Number: ");
+    for (int i = 0; i < length; i++) {
+        printf("%d", number[i]);
+    }
+    printf("\n");
+
+    int guess, attempts = 0;
+    int bulls, cows;
+
+    do {
+        printf("Try to guess the number ", length);
+        scanf_s("%d", &guess);
+
+        int temp = guess;
+        int digit_count = 0;
+        while (temp != 0) {
+            digit_count++;
             temp /= 10;
         }
-        copy_a /= 10;
-        if (k == 0) {
-            a = rand() % (coe1 - coe2) + coe2;
-            copy_a = a;
-            //printf("%d\n", a);
+
+        if (digit_count != length) {
+            printf("Incorrect lenght\n", length);
+            continue;
         }
-    }
 
-    //guessing
-    k = 0;
-    bull = 0;
-    while (bull < len) {
-        printf("try to guess the number\n");
-        scanf_s("%d", &n);
-        k++;
-        pos_n = len;
-        cow = 0;
-        bull = 0;
-        while (n > 0) {
-            copy_a = a;
-            pos_a = len;
-            while (copy_a > 0) {
-                dig1 = n % 10;
-                dig2 = copy_a % 10;
-                if ((dig1 == dig2) && (pos_a == pos_n)) {
-                    bull++;
-                }
-                if ((dig1 == dig2) && (pos_a != pos_n)) {
-                    cow++;
-                }
-                copy_a /= 10;
-                pos_a--;
+      
+        int attempt_digits[5];
+        temp = guess;
+        for (int i = length - 1; i >= 0; i--) {
+            attempt_digits[i] = temp % 10;
+            temp /= 10;
+        }
 
+
+        bulls = 0;
+        cows = 0;
+
+        for (int i = 0; i < length; i++) {
+            if (attempt_digits[i] == number[i]) {
+                bulls++;
             }
-            n /= 10;
-            pos_n--;
+            else {
+                for (int j = 0; j < length; j++) {
+                    if (i != j && attempt_digits[i] == number[j]) {
+                        cows++;
+                        break;
+                    }
+                }
+            }
         }
-        printf("cow = %d, bull = %d\n", cow, bull);
-    }
-    printf("right, k = %d\n", k);
-    return 0;
 
+        attempts++;
+        printf("Bulls: %d, Cows: %d\n", bulls, cows);
+
+    } while (bulls != length);
+
+    printf("U guessed, attemps: %d\n", attempts);
+    return 0;
 }
