7b23f730ef2aafd1a4caddebbbb2b0ade4653931
matveysavin17 <matvey.s.v.@mail.ru>
2025-12-15 00:18:26 +0300

    fix1

--- SavinMV/lab5/lab5/main.c
+++ SavinMV/lab5/lab5/main.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <windows.h>
-#include <time.h>
 #include <locale.h>
 
 typedef struct {
@@ -14,8 +13,11 @@ typedef struct {
 void swap(FileInfo* a, FileInfo* b);
 void bubbleSort(FileInfo arr[], int n);
 void insertionSort(FileInfo arr[], int n);
+void selectionSort(FileInfo arr[], int n);
 void quickSort(FileInfo arr[], int low, int high);
 int partition(FileInfo arr[], int low, int high);
+void mergeSort(FileInfo arr[], int left, int right);
+void merge(FileInfo arr[], int left, int mid, int right);
 FileInfo* copyFiles(const FileInfo* src, int n);
 void freeFiles(FileInfo* files, int n);
 char* appendWildcard(const char* input);
@@ -24,7 +26,7 @@ int main(void)
 {
     char inputPath[1024];
     setlocale(LC_ALL, "RUS");
-    printf("Введите путь к папке (например, C:\\test или .): ");
+    printf("Введите путь к папке: ");
     if (!fgets(inputPath, sizeof(inputPath), stdin)) {
         printf("Ошибка ввода пути.\n");
         return 1;
@@ -43,21 +45,16 @@ int main(void)
 
     if (hFind == INVALID_HANDLE_VALUE) {
         printf("Не удалось открыть директорию: %s\n", inputPath);
-        DWORD err = GetLastError();
-        if (err == ERROR_FILE_NOT_FOUND || err == ERROR_PATH_NOT_FOUND) {
-            printf("Папка не найдена или не содержит файлов.\n");
-        }
-        else {
-            printf("Код ошибки Windows: %lu\n", err);
-        }
-        free(directory);
-        return 1;
     }
 
     int capacity = 10;
     int count = 0;
     FileInfo* originalFiles = (FileInfo*)malloc(capacity * sizeof(FileInfo));
-
+    if (!originalFiles) {
+        printf("Ошибка\n");
+        free(directory);
+        return 1;
+    }
 
     do {
         if (findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
@@ -66,12 +63,26 @@ int main(void)
         if (count >= capacity) {
             capacity *= 2;
             FileInfo* temp = (FileInfo*)realloc(originalFiles, capacity * sizeof(FileInfo));
+            if (!temp) {
+                printf("Ошибка\n");
+                freeFiles(originalFiles, count);
+                free(directory);
+                FindClose(hFind);
+                return 1;
+            }
             originalFiles = temp;
         }
 
         size_t nameLen = strlen(findData.cFileName);
         originalFiles[count].name = (char*)malloc(nameLen + 1);
-        strcpy_s(originalFiles[count].name, nameLen + 1, findData.cFileName);
+        if (!originalFiles[count].name) {
+            printf("Ошибка\n");
+            freeFiles(originalFiles, count);
+            free(directory);
+            FindClose(hFind);
+            return 1;
+        }
+        strcpy(originalFiles[count].name, findData.cFileName);
 
         originalFiles[count].size = ((unsigned long long)findData.nFileSizeHigh << 32) | findData.nFileSizeLow;
         count++;
@@ -88,13 +99,15 @@ int main(void)
 
     int choice;
     while (1) {
-        printf("\n=== Меню сортировки ===\n");
+        printf("\nМеню сортировки\n");
         printf("Найдено файлов: %d\n", count);
         printf("1. Пузырьковая сортировка (Bubble Sort)\n");
         printf("2. Сортировка вставками (Insertion Sort)\n");
-        printf("3. Быстрая сортировка (Quick Sort)\n");
+        printf("3. Сортировка выбором (Selection Sort)\n");
+        printf("4. Быстрая сортировка (Quick Sort)\n");
+        printf("5. Сортировка слиянием (Merge Sort)\n");
         printf("0. Выйти из программы\n");
-        printf("Ваш выбор (0-3): ");
+        printf("Ваш выбор (0-5): ");
 
         if (scanf_s("%d", &choice) != 1) {
             while (getchar() != '\n');
@@ -108,40 +121,51 @@ int main(void)
         }
 
         FileInfo* workingCopy = copyFiles(originalFiles, count);
+        if (!workingCopy) {
+            printf("Ошибка копирования данных.\n");
+            continue;
+        }
+        LARGE_INTEGER start, end, freq;
+        double t_ms;
 
-        clock_t start, end;
-        double cpu_time_used;
+        QueryPerformanceFrequency(&freq);
+        QueryPerformanceCounter(&start);
 
-        start = clock();
 
         switch (choice) {
         case 1:
             bubbleSort(workingCopy, count);
-            printf("\n--- Пузырьковая сортировка ---\n");
+            printf("\nПузырьковая сортировка\n");
             break;
         case 2:
             insertionSort(workingCopy, count);
-            printf("\n--- Сортировка вставками ---\n");
+            printf("\nСортировка вставками\n");
             break;
         case 3:
+            selectionSort(workingCopy, count);
+            printf("\nСортировка выбором\n");
+            break;
+        case 4:
             quickSort(workingCopy, 0, count - 1);
-            printf("\n--- Быстрая сортировка ---\n");
+            printf("\nБыстрая сортировка\n");
+            break;
+        case 5:
+            mergeSort(workingCopy,0, count-1);
+            printf("\nСортировка слиянием\n");
             break;
         default:
             printf("Неверный выбор.\n");
             freeFiles(workingCopy, count);
             continue;
         }
-
-        end = clock();
-
-        cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC * 1000.0;
+        QueryPerformanceCounter(&end);
+        t_ms = (double)(end.QuadPart - start.QuadPart) / freq.QuadPart * 1000.0;
 
         for (int i = 0; i < count; i++) {
             printf("Файл: %s, Размер: %llu байт\n", workingCopy[i].name, workingCopy[i].size);
         }
 
-        printf("\n Время сортировки: %.3f мс\n", cpu_time_used);
+        printf("\n Время сортировки: %.3f мс\n", t_ms);
 
         freeFiles(workingCopy, count);
     }
@@ -163,6 +187,7 @@ void bubbleSort(FileInfo arr[], int n) {
         }
     }
 }
+
 void insertionSort(FileInfo arr[], int n) {
     for (int i = 1; i < n; i++) {
         FileInfo key = arr[i];
@@ -174,25 +199,100 @@ void insertionSort(FileInfo arr[], int n) {
         arr[j + 1] = key;
     }
 }
+
+void selectionSort(FileInfo arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIdx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j].size < arr[minIdx].size) {
+                minIdx = j;
+            }
+        }
+        if (minIdx != i) {
+            swap(&arr[i], &arr[minIdx]);
+        }
+    }
+}
+
+int partition(FileInfo arr[], int low, int high) {
+    int mid = low + (high - low) / 2;
+    unsigned long long pivot = arr[mid].size;
+    int i = low - 1;
+    int j = high + 1;
+
+    while (1) {
+        do {
+            i++;
+        } while (arr[i].size < pivot);
+        do {
+            j--;
+        } while (arr[j].size > pivot);
+        if (i >= j)
+            return j;
+        swap(&arr[i], &arr[j]);
+    }
+}
 void quickSort(FileInfo arr[], int low, int high) {
     if (low < high) {
         int pi = partition(arr, low, high);
-        quickSort(arr, low, pi - 1);
+        quickSort(arr, low, pi);
         quickSort(arr, pi + 1, high);
     }
 }
-int partition(FileInfo arr[], int low, int high) {
-    unsigned long long pivot = arr[high].size;
-    int i = low - 1;
-    for (int j = low; j < high; j++) {
-        if (arr[j].size <= pivot) {
-            i++;
-            swap(&arr[i], &arr[j]);
-        }
+
+void merge(FileInfo arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    FileInfo* leftArr = (FileInfo*)malloc(n1 * sizeof(FileInfo));
+    FileInfo* rightArr = (FileInfo*)malloc(n2 * sizeof(FileInfo));
+    if (!leftArr || !rightArr) {
+        fprintf(stderr, "Ошибка\n");
+        free(leftArr);
+        free(rightArr);
+        return;
     }
-    swap(&arr[i + 1], &arr[high]);
-    return i + 1;
+
+    for (int i = 0; i < n1; i++)
+        leftArr[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        rightArr[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (leftArr[i].size <= rightArr[j].size) {
+            arr[k] = leftArr[i];
+            i++;
+        }
+        else {
+            arr[k] = rightArr[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1) {
+        arr[k] = leftArr[i];
+        i++; k++;
+    }
+    while (j < n2) {
+        arr[k] = rightArr[j];
+        j++; k++;
+    }
+    free(leftArr);
+    free(rightArr);
 }
+
+void mergeSort(FileInfo arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
+
 FileInfo* copyFiles(const FileInfo* src, int n) {
     FileInfo* copy = (FileInfo*)malloc(n * sizeof(FileInfo));
     if (!copy) return NULL;
@@ -214,9 +314,6 @@ FileInfo* copyFiles(const FileInfo* src, int n) {
 }
 void freeFiles(FileInfo* files, int n) {
     if (!files) return;
-    for (int i = 0; i < n; i++) {
-        free(files[i].name);
-    }
     free(files);
 }
 char* appendWildcard(const char* input) {
