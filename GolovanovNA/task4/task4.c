7d48920ef9c952a65ea18ea01a072563cbdad938
Nikita-Glvnv <a43264635@gmail.com>
2025-12-17 21:24:56 +0300

    lol

--- GolovanovNA/task4/task4.c
+++ GolovanovNA/task4/task4.c
@@ -1,46 +1,73 @@
 #include <stdio.h>
 #include <string.h>
-#define _CRT_SECURE_NO_WARNINGS
 #define brange 4
-#define crange 100
 
-int bd(char code[5], char tp[][30]);
+int bd(char code[5], char tp[][30], int chk[]);
 
 void printer(char tp[3][30]) {
-	printf("code %s name: %s cost: %s", tp[0], tp[1], tp[2]);
+    printf("name: %s cost: %s\n", tp[1], tp[2]);
 }
-
 
 int main() {
-	char tp[3][30];
-	char cod = "9999";
-	int sum = 0;
-	while (1 == 1) {
-		char inp[5];
-		fgets(inp, 4, stdin);
-		if (strcmp(inp, "end") ==0) {
-			break;
-		}
-		else {
-			sum += bd(inp, tp);
-			printer(tp);
-		}
-	}
-	printf("total: %d", sum);
+    char tp[4][30];
+    int sum = 0, i = 0, u = 0;
+    int chek[brange] = { 0, 0, 0, 0 };
+    char codes[brange][5] = { "0001", "0002", "0003", "0004" };
+
+    while (1) {
+        char inp[10];
+        fgets(inp, sizeof(inp), stdin);
+        inp[strcspn(inp, "\n")] = 0;
+
+        if (strcmp(inp, "end") == 0) {
+            break;
+        }
+        else {
+            sum += bd(inp, tp, chek);
+            printer(tp);
+            
+        }
+    }
+    printf("calculating your personal discounts...\n");
+    for (i = 0; i < brange; i++) {
+        if (chek[i] > 0) {
+            int c = chek[i];
+            int g = c * bd(codes[i], tp, chek);
+            printf("name: %s cost: %s X %d = %d with a %s %% discount\n", tp[1], tp[2], c, g, tp[3]);
+        }
+
+    }
+    printf("total: %d\n", sum);
+    return 0;
 }
 
-int bd(char code[5], char tp[][30]) {
-	char codes[brange][4] = { "0001", "0002", "0003", "0004" };
-	char names[brange][29] = { "milk", "bread", "slave", "salt" };
-	char costs[brange][29] = { "100", "30", "10", "20" };
-	int icosts[brange] = { 100, 30, 10, 20 };
-	int i, k;
-	for (i = 0; i < brange; i++) {
-		if (strcmp(code, codes[i]) == 0);
-		k = i;
-	}
-	strcpy(tp[0], codes[k]);
-	strcpy(tp[1], names[k]);
-	strcpy(tp[2], costs[k]);
-	return icosts[k];
+int bd(char code[5], char tp[][30], int chk[]) {
+    char codes[brange][5] = { "0001", "0002", "0003", "0004" };
+    char names[brange][30] = { "milk", "bread", "bun", "salt" };
+    char costs[brange][30] = { "100", "30", "10", "20" };
+    char sales[brange][30] = { "10", "0", "15", "50" };
+    int icosts[brange] = { 90, 30, 10, 10 };
+
+    int k = -1;
+    for (int i = 0; i < brange; i++) {
+        if (strcmp(code, codes[i]) == 0) {
+            k = i;
+            break;
+        }
+    }
+
+    if (k == -1) {
+        strcpy(tp[0], "not found");
+        strcpy(tp[1], "not found");
+        strcpy(tp[2], "not found");
+        strcpy(tp[3], "not found");
+        return 0;
+    }
+
+    strcpy(tp[0], codes[k]);
+    strcpy(tp[1], names[k]);
+    strcpy(tp[2], costs[k]);
+    strcpy(tp[3], sales[k]);
+    chk[k]++;
+    return icosts[k];
 }
---
+++ GolovanovNA/task5/task5.c
@@ -0,0 +1,319 @@
+﻿#include <stdio.h>
+#include <windows.h>
+#include <string.h>
+#include <locale.h>
+#include <time.h>
+#define T 256
+//C:\Users\1\Desktop\Maybe
+//C:\Users\Student2023\source\repos\Project7\Project7
+void StartProv(int* cn, char ad[], unsigned int* ma) {
+	int i;
+	char plus[3] = "\\*\0";
+	printf("Enter the address of the directory:\n");
+	scanf("%s", ad);
+	strcat(ad, plus);
+	WIN32_FIND_DATAA data;
+	HANDLE hFind = FindFirstFileA(ad, &data);
+	if (hFind != INVALID_HANDLE_VALUE) {
+		do {
+			if (data.dwFileAttributes == 32) {
+				if ((*ma) < strlen(data.cFileName)) {
+					(*ma) = strlen(data.cFileName);
+				}
+				(*cn)++;
+			}
+		} while (FindNextFileA(hFind, &data));
+		FindClose(hFind);
+	}
+	else {
+		DWORD error = GetLastError();
+		printf("Fatal -- Invalid directory address\n");
+		printf("Error �ode -- %d\n", error);
+		exit(0);
+	}
+	if ((*cn) == 0) {
+		printf("There are no files in the directory!\n");
+		exit(0);
+	}
+}
+void StartReady(int cnt, int* size, int* ind, char** name, char ad[], unsigned int max) {
+	int q = 0;
+	WIN32_FIND_DATAA data;
+	HANDLE hFind = FindFirstFileA(ad, &data);
+	if (hFind != INVALID_HANDLE_VALUE) {
+		do {
+			if (data.dwFileAttributes == 32) {
+				strcpy(name[q], data.cFileName);
+				size[q++] = data.nFileSizeLow;
+				printf("FILE --> %s --> %d\n", data.cFileName, data.nFileSizeLow);
+			}
+		} while (FindNextFileA(hFind, &data));
+		FindClose(hFind);
+	}
+}
+void Clean(int cnt, int* cl, int* size, int* ind) {
+	int i;
+	for (i = 0; i < cnt; i++) {
+		size[i] = cl[i];
+		ind[i] = i;
+	}
+}
+void BubbleSort(int cnt, int* size, int* ind, double* tt) {
+	int i, j, p, e;
+	LARGE_INTEGER st, end, freq;
+	QueryPerformanceFrequency(&freq);
+	QueryPerformanceCounter(&st);
+	for (i = 0; i < cnt - 1; i++) {
+		for (j = 0; j < cnt - i - 1; j++) {
+			if (size[j] > size[j + 1]) {
+				e = size[j];
+				size[j] = size[j + 1];
+				size[j + 1] = e;
+				p = ind[j];
+				ind[j] = ind[j + 1];
+				ind[j + 1] = p;
+			}
+		}
+	}
+	QueryPerformanceCounter(&end);
+	(*tt) = (double)(end.QuadPart - st.QuadPart) * 1000.0 / freq.QuadPart;
+}
+void InsertSort(int cnt, int* size, int* ind, double* tt) {
+	int i, j, temp, z;
+	LARGE_INTEGER st, end, freq;
+	QueryPerformanceFrequency(&freq);
+	QueryPerformanceCounter(&st);
+	for (i = 0; i < cnt; i++) {
+		z = size[i];
+		temp = ind[i];
+		j = i - 1;
+		while (j >= 0 && (size[j] > z)) {
+			size[j + 1] = size[j];
+			ind[j + 1] = ind[j];
+			j--;
+		}
+		size[j + 1] = z;
+		ind[j + 1] = temp;
+	}
+	QueryPerformanceCounter(&end);
+	(*tt) = (double)(end.QuadPart - st.QuadPart) * 1000.0 / freq.QuadPart;
+}
+void Merge(int l, int m, int r, int cnt, int* size, int* ind, int* arr, int* iarr) {
+	int i = l, j = m + 1, k = 0;
+	while (i <= m && j <= r) {
+		if (size[i] <= size[j]) {
+			arr[k] = size[i];
+			iarr[k++] = ind[i++];
+		}
+		else {
+			arr[k] = size[j];
+			iarr[k++] = ind[j++];
+		}
+	}
+	while (i <= m) {
+		arr[k] = size[i];
+		iarr[k] = ind[i];
+		k++; i++;
+	}
+	while (j <= r) {
+		arr[k] = size[j];
+		iarr[k] = ind[j];
+		k++; j++;
+	}
+	for (i = 0; i < r - l + 1; i++) {
+		size[l + i] = arr[i];
+		ind[l + i] = iarr[i];
+	}
+}
+void MergeSort(int l, int r, int cnt, int* size, int* ind, int* arr, int* iarr) {
+	int m;
+	if (l + 1 <= r) {
+		m = l + (r - l) / 2;
+		MergeSort(l, m, cnt, size, ind, arr, iarr);
+		MergeSort(m + 1, r, cnt, size, ind, arr, iarr);
+		Merge(l, m, r, cnt, size, ind, arr, iarr);
+	}
+}
+void Msort(int cnt, int* size, int* ind, int* arr, int* iarr, double* tt) {
+	int l = 0, r = cnt - 1, i;
+	LARGE_INTEGER st, end, freq;
+	QueryPerformanceFrequency(&freq);
+	QueryPerformanceCounter(&st);
+	MergeSort(l, r, cnt, size, ind, arr, iarr);
+	QueryPerformanceCounter(&end);
+	(*tt) = (double)(end.QuadPart - st.QuadPart) * 1000.0 / freq.QuadPart;
+}
+void SimpleSort(int cnt, int* size, int* ind, double* tt) {
+	LARGE_INTEGER st, end, freq;
+	int i, j, p, t;
+	QueryPerformanceFrequency(&freq);
+	QueryPerformanceCounter(&st);
+	for (i = 0; i < cnt; i++) {
+		for (j = i + 1; j < cnt; j++) {
+			if (size[i] > size[j]) {
+				t = size[i];
+				size[i] = size[j];
+				size[j] = t;
+				p = ind[i];
+				ind[i] = ind[j];
+				ind[j] = p;
+			}
+		}
+	}
+	QueryPerformanceCounter(&end);
+	(*tt) = (double)(end.QuadPart - st.QuadPart) * 1000.0 / freq.QuadPart;
+}
+void ChoiceSort(int cnt, int* size, int* ind, double* tt) {
+	int i, j, min, in, p, t;
+	LARGE_INTEGER st, end, freq;
+	QueryPerformanceFrequency(&freq);
+	QueryPerformanceCounter(&st);
+	for (i = 0; i < cnt; i++) {
+		min = size[i];
+		in = i;
+		for (j = i + 1; j < cnt; j++) {
+			if (size[j] < min) {
+				min = size[j];
+				in = j;
+			}
+		}
+		t = size[i];
+		size[i] = size[in];
+		size[in] = t;
+		p = ind[i];
+		ind[i] = ind[in];
+		ind[in] = p;
+	}
+	QueryPerformanceCounter(&end);
+	(*tt) = (double)(end.QuadPart - st.QuadPart) * 1000.0 / freq.QuadPart;
+}
+void HoarSort(int cnt, int n1, int n2, int* size, int* ind) {
+	int m = n1 + (n2 - n1) / 2;
+	int i = n1, j = n2, t, f;
+	int p = size[m];
+	do {
+		while (size[i] < p) {
+			i++;
+		}
+		while (size[j] > p) {
+			j--;
+		}
+		if (i <= j) {
+			t = ind[i];
+			ind[i] = ind[j];
+			ind[j] = t;
+			f = size[i];
+			size[i] = size[j];
+			size[j] = f;
+			i++;
+			j--;
+		}
+	} while (i <= j);
+	if (n1 < j) {
+		HoarSort(cnt, n1, j, size, ind);
+	}
+	if (i < n2) {
+		HoarSort(cnt, i, n2, size, ind);
+	}
+}
+void Hsort(int cnt, int* size, int* ind, double* tt) {
+	int n1 = 0, n2 = cnt - 1, i;
+	LARGE_INTEGER st, end, freq;
+	QueryPerformanceFrequency(&freq);
+	QueryPerformanceCounter(&st);
+	HoarSort(cnt, n1, n2, size, ind);
+	QueryPerformanceCounter(&end);
+	(*tt) = (double)(end.QuadPart - st.QuadPart) * 1000.0 / freq.QuadPart;
+}
+void Print_and_Protect(int cnt, int* cl, int* size) {
+	int i;
+	for (i = 0; i < cnt; i++) {
+		cl[i] = size[i];
+	}
+	printf("\n");
+	printf("Select sorting or end the program:\n");
+	printf("1 - Bubble sorting\n");
+	printf("2 - Insertion sorting\n");
+	printf("3 - Merge sorting\n");
+	printf("4 - Simplest sorting\n");
+	printf("5 - Choice sorting\n");
+	printf("6 - Quick sorting\n");
+	printf("7 - End the program\n\n");
+}
+void Ans(int cnt, int* size, int* ind, char** name, double t) {
+	int i;
+	for (i = 0; i < cnt; i++) {
+		printf("%s -- %d\n", name[ind[i]], size[i]);
+	}
+	printf("Execution time - %.6f nanoseconds\n", t);
+}
+int main() {
+	unsigned int max = 0;
+	unsigned int* ma = &max;
+	double t;
+	double* tt = &t;
+	int* arr;
+	int* iarr;
+	int* size;
+	int* ind;
+	char** name;
+	char* locale = setlocale(LC_ALL, "");
+	int cnt = 0, fl, b = 1, i;
+	int* cn = &cnt;
+	int* cl;
+	char ad[T];
+	StartProv(cn, ad, ma);
+	size = (int*)malloc(cnt * sizeof(int));
+	name = (char**)malloc(cnt * sizeof(char*));
+	ind = (int*)malloc(cnt * sizeof(int));
+	arr = (int*)malloc(cnt * sizeof(int));
+	iarr = (int*)malloc(cnt * sizeof(int));
+	for (i = 0; i < cnt; i++) {
+		ind[i] = i;
+		name[i] = (char*)malloc((max + 1) * sizeof(char));
+	}
+	StartReady(cnt, size, ind, name, ad, max);
+	cl = (int*)malloc(cnt * sizeof(int));
+	Print_and_Protect(cnt, cl, size);
+	while (b != 0) {
+		scanf("%d", &fl);
+		switch (fl) {
+		case 1:
+			BubbleSort(cnt, size, ind, name);
+			break;
+		case 2:
+			InsertSort(cnt, size, ind, name);
+			break;
+		case 3:
+			Msort(cnt, size, ind, name, arr, iarr);
+			break;
+		case 4:
+			SimpleSort(cnt, size, ind, name);
+			break;
+		case 5:
+			ChoiceSort(cnt, size, ind, name);
+			break;
+		case 6:
+			Hsort(cnt, size, ind, name);
+			break;
+		case 7:
+			b = 0;
+			printf("The end!");
+			break;
+		default:
+			printf("Fatal -- There is no sorting with a given number\n");
+		}
+		Ans(cnt, size, ind, name, t);
+		Clean(cnt, cl, size, ind);
+	}
+	free(arr);
+	free(iarr);
+	free(cl);
+	free(size);
+	free(ind);
+	for (i = 0; i < cnt; i++) {
+		free(name[i]);
+	}
+	free(name);
+	return 0;
+}
