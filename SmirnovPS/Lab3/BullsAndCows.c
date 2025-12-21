44265c195c847453081fe302c1ec4de806a958bb
Tensety <smpaveel@gmail.com>
2025-12-21 21:33:00 +0300

    Fixed with arrays

--- SmirnovPS/Lab3/BullsAndCows.c
+++ SmirnovPS/Lab3/BullsAndCows.c
@@ -4,64 +4,66 @@
 #define M 10
 int main() {
 	srand((unsigned int)time(0));
-	int i, j, number = 0, n, flag = 0, check, bulls = 0, cows = 0, guess, numcheck, guesscheck;
-	int a[M];
-	printf("Enter length of the number:\n");
+	int i, j, n, flag, check, bulls, cows, numguess;
+	int guess[M], number[M];
+
 	do {
+		printf("Enter length (2 - 5) of the number:\n");
 		scanf_s("%d", &n);
 	} while (n < 2 || n > 5);
 
+
 	do {
-		number = 0;
-		for (i = 0; i < n; i++) {
-			number = number * 10 + rand() % 10;
-		}
-
-
-		for (i = 0; i < M; i++) {
-			a[i] = 0;
-		}
-
 		flag = 1;
-		check = number;
 
-
-		while (check > 0) {
-			a[check % 10]++;
-			check /= 10;
+		for (i = 0; i < n; i++) {
+			number[i] = rand() % 10;
 		}
 
-		for (i = 0; i < M; i++) {
-			if (a[i] > 1) {
-				flag = 0;
-			}
+
+		for (i = 0; i < n; i++) {
+			for (j = i + 1; j < n; j++)
+				if (number[i] == number[j])
+					flag = 0;
 		}
+
+		if (number[0] == 0) {
+			flag = 0;
+		}
+
 
 	} while (flag == 0);
 
 
-	while (bulls != 5) {
+	bulls = 0;
+
+
+	while (bulls != n) {
 		bulls = 0;
 		cows = 0;
+
 		printf("Enter your guess:\n");
-		scanf_s("%d", &guess);
-		guesscheck = guess;
-		for (i = 0; i < n; i++) {
-			numcheck = number;
-			for (j = 0; j < n; j++) {
-				if (numcheck % 10 == guesscheck % 10) {
-					if (i == j)
-						bulls += 1;
-					else
-						cows += 1;
-				}
-				numcheck /= 10;
-			}
-			guesscheck /= 10;
+		scanf_s("%d", &numguess);
+
+		for (i = n - 1; i >= 0; i--) {
+			guess[i] = numguess % 10;
+			numguess /= 10;
 		}
+
+		for (i = 0; i < n; i++) {
+			for (j = 0; j < n; j++) {
+				if (number[i] == guess[j])
+					if (i == j)
+						bulls++;
+					else
+						cows++;
+			}
+		}
+
 		printf("Amount of bulls: %d\n", bulls);
 		printf("Amount of cows: %d\n", cows);
 	}
+
 	printf("You've guessed the number!");
 	return 0;
 }
