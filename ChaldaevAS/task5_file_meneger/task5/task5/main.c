a4f6942d7154ddf91246bf14d3ed798eecc7063f
“ArseniyChaldaev” <arsenii.chaldaev@gmail.com>
2025-12-22 03:36:02 +0300

    Added 3 type of sort, fixed display

--- ChaldaevAS/task5_file_meneger/task5/task5/main.c
+++ ChaldaevAS/task5_file_meneger/task5/task5/main.c
@@ -3,7 +3,8 @@
 #include <string.h>
 #include <time.h>
 
-#define MAX_FILES 1000
+#define MAX_FILES 5000
+#define MAX_DISPLAYED 5000
 
 char file_names[MAX_FILES][256];
 long file_sizes[MAX_FILES];
@@ -15,11 +16,11 @@ void show_files() {
         return;
     }
 
-    printf("\n%-30s %s\n", "File Name", "Size");
+    printf("\n%-10s %-70s %s\n", "Number", "File Name", "Size");
     printf("----------------------------------------\n");
 
-    for (int i = 0; i < file_count; i++) {
-        printf("%-30s %ld\n", file_names[i], file_sizes[i]);
+    for (int i = 0; i < min(file_count, MAX_DISPLAYED); i++) {
+        printf("%-10d %-70s %ld\n", i + 1, file_names[i], file_sizes[i]);
     }
 }
 
@@ -61,7 +62,6 @@ void get_files(char* path) {
     WIN32_FIND_DATAA file_data;
     char search_path[300];
 
-    // Используем sprintf_s вместо sprintf
     sprintf_s(search_path, sizeof(search_path), "%s\\*", path);
 
     HANDLE handle = FindFirstFileA(search_path, &file_data);
@@ -77,7 +77,6 @@ void get_files(char* path) {
         if (strcmp(file_data.cFileName, "..") == 0) continue;
 
         if (!(file_data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
-            // Используем strcpy_s вместо strcpy
             strcpy_s(file_names[file_count], 256, file_data.cFileName);
             file_sizes[file_count] = file_data.nFileSizeLow;
             file_count++;
@@ -90,70 +89,128 @@ void get_files(char* path) {
     printf("Found %d files\n", file_count);
 }
 
-void bubble_sort(int ascending) {
-    for (int i = 0; i < file_count - 1; i++) {
-        for (int j = 0; j < file_count - i - 1; j++) {
+void selection_sort(char names[][256], long sizes[], int count, int ascending) {
+    for (int i = 0; i < count - 1; i++) {
+        int extreme_index = i;
+
+        for (int j = i + 1; j < count; j++) {
+            if (ascending == 1) {
+                if (sizes[j] < sizes[extreme_index]) {
+                    extreme_index = j;
+                }
+            }
+            else {
+                if (sizes[j] > sizes[extreme_index]) {
+                    extreme_index = j;
+                }
+            }
+        }
+
+        if (extreme_index != i) {
+            long temp_size = sizes[i];
+            sizes[i] = sizes[extreme_index];
+            sizes[extreme_index] = temp_size;
+
+            char temp_name[256];
+            strcpy_s(temp_name, 256, names[i]);
+            strcpy_s(names[i], 256, names[extreme_index]);
+            strcpy_s(names[extreme_index], 256, temp_name);
+        }
+    }
+}
+
+void insertion_sort(char names[][256], long sizes[], int count, int ascending) {
+    for (int i = 1; i < count; i++) {
+        long key_size = sizes[i];
+        char key_name[256];
+        strcpy_s(key_name, 256, names[i]);
+
+        int j = i - 1;
+
+        if (ascending == 1) {
+            while (j >= 0 && sizes[j] > key_size) {
+                sizes[j + 1] = sizes[j];
+                strcpy_s(names[j + 1], 256, names[j]);
+                j--;
+            }
+        }
+        else {
+            while (j >= 0 && sizes[j] < key_size) {
+                sizes[j + 1] = sizes[j];
+                strcpy_s(names[j + 1], 256, names[j]);
+                j--;
+            }
+        }
+
+        sizes[j + 1] = key_size;
+        strcpy_s(names[j + 1], 256, key_name);
+    }
+}
+
+void bubble_sort(char names[][256], long sizes[], int count, int ascending) {
+    for (int i = 0; i < count - 1; i++) {
+        for (int j = 0; j < count - i - 1; j++) {
             int need_swap = 0;
 
             if (ascending == 1) {
-                if (file_sizes[j] > file_sizes[j + 1]) {
+                if (sizes[j] > sizes[j + 1]) {
                     need_swap = 1;
                 }
             }
             else {
-                if (file_sizes[j] < file_sizes[j + 1]) {
+                if (sizes[j] < sizes[j + 1]) {
                     need_swap = 1;
                 }
             }
 
             if (need_swap) {
-                long temp_size = file_sizes[j];
-                file_sizes[j] = file_sizes[j + 1];
-                file_sizes[j + 1] = temp_size;
+                long temp_size = sizes[j];
+                sizes[j] = sizes[j + 1];
+                sizes[j + 1] = temp_size;
 
                 char temp_name[256];
-                strcpy_s(temp_name, 256, file_names[j]);
-                strcpy_s(file_names[j], 256, file_names[j + 1]);
-                strcpy_s(file_names[j + 1], 256, temp_name);
+                strcpy_s(temp_name, 256, names[j]);
+                strcpy_s(names[j], 256, names[j + 1]);
+                strcpy_s(names[j + 1], 256, temp_name);
             }
         }
     }
 }
 
-void quick_sort(int left, int right, int ascending) {
+void quick_sort(char names[][256], long sizes[], int left, int right, int ascending) {
     if (left >= right) return;
 
-    long pivot = file_sizes[(left + right) / 2];
+    long pivot = sizes[(left + right) / 2];
     int i = left;
     int j = right;
 
     while (i <= j) {
         if (ascending == 1) {
-            while (file_sizes[i] < pivot) i++;
-            while (file_sizes[j] > pivot) j--;
+            while (sizes[i] < pivot) i++;
+            while (sizes[j] > pivot) j--;
         }
         else {
-            while (file_sizes[i] > pivot) i++;
-            while (file_sizes[j] < pivot) j--;
+            while (sizes[i] > pivot) i++;
+            while (sizes[j] < pivot) j--;
         }
 
         if (i <= j) {
-            long temp_size = file_sizes[i];
-            file_sizes[i] = file_sizes[j];
-            file_sizes[j] = temp_size;
+            long temp_size = sizes[i];
+            sizes[i] = sizes[j];
+            sizes[j] = temp_size;
 
             char temp_name[256];
-            strcpy_s(temp_name, 256, file_names[i]);
-            strcpy_s(file_names[i], 256, file_names[j]);
-            strcpy_s(file_names[j], 256, temp_name);
+            strcpy_s(temp_name, 256, names[i]);
+            strcpy_s(names[i], 256, names[j]);
+            strcpy_s(names[j], 256, temp_name);
 
             i++;
             j--;
         }
     }
 
-    if (left < j) quick_sort(left, j, ascending);
-    if (i < right) quick_sort(i, right, ascending);
+    if (left < j) quick_sort(names, sizes, left, j, ascending);
+    if (i < right) quick_sort(names, sizes, i, right, ascending);
 }
 
 int get_sort_choice() {
@@ -161,7 +218,9 @@ int get_sort_choice() {
 
     printf("\nChoose sorting method:\n");
     printf("1. Bubble Sort\n");
-    printf("2. Quick Sort\n");
+    printf("2. Selection Sort\n");
+    printf("3. Insertion Sort\n");
+    printf("4. Quick Sort\n");
     printf("Enter choice: ");
 
     if (scanf_s("%d", &choice) != 1) {
@@ -231,18 +290,42 @@ int main() {
             int sort_type = get_sort_choice();
             int order = get_order_choice();
 
-            start = get_time();
 
-            if (sort_type == 1) {
-                bubble_sort(order == 1);
+            switch (sort_type) {
+            case 1:
+                start = get_time();
+                bubble_sort(file_names, file_sizes, file_count, order == 1);
+                end = get_time();
                 printf("Sorted with Bubble Sort\n");
-            }
-            else {
-                quick_sort(0, file_count - 1, order == 1);
+                break;
+            case 2:
+                start = get_time();
+                selection_sort(file_names, file_sizes, file_count, order == 1);
+                end = get_time();
+                printf("Sorted with Selection Sort\n");
+                break;
+            case 3:
+                start = get_time();
+                insertion_sort(file_names, file_sizes, file_count, order == 1);
+                end = get_time();
+                printf("Sorted with Insertion Sort\n");
+                break;
+            case 4:
+                start = get_time();
+                quick_sort(file_names, file_sizes, 0, file_count - 1, order == 1);
+                end = get_time();
                 printf("Sorted with Quick Sort\n");
+                break;
+            default:
+                start = get_time();
+                bubble_sort(file_names, file_sizes, file_count, order == 1);
+                end = get_time();
+                printf("Sorted with Bubble Sort (default)\n");
+                break;
             }
 
-            end = get_time();
+
+
             double time_taken = end - start;
             if (time_taken < 0.001) {
                 printf("\nSorting time: %.6f milliseconds\n", time_taken * 1000);
