03f4f60b932e57575d97657a1890d05ebd7733f3
Tensety <smpaveel@gmail.com>
2025-12-28 23:38:54 +0300

    Fixed with char

--- SmirnovPS/Lab4/barcodes.c
+++ SmirnovPS/Lab4/barcodes.c
@@ -4,37 +4,45 @@
 #define N 10
 #define NAME_LEN 20
 
-void listProducts(int barcodes[][4], char names[][NAME_LEN], int prices[], int discounts[]);
-int findProduct(int barcode[4], int barcodes[][4], int count);
-int equalBarcodes(int barcode1[4], int barcode2[4]);
-void copyBarcode(int barcode1[4], int barcode2[4]);
-void inputBarcode(int barcode[4]);
-int scanProduct(int productBarcodes[][4], int productsCount, int barcode[4], char productNames[][NAME_LEN]);
+int findProduct(char barcode[], char barcodes[][5], int count);
+int equalBarcodes(char barcode1[], char barcode2[]);
+void copyBarcode(char dest[], char src[]);
+void inputBarcode(char barcode[]);
+int scanProduct(char productBarcodes[][5], int productsCount, char barcode[], char productNames[][NAME_LEN]);
 void showProductInfo(int index, char names[][NAME_LEN], int prices[], int discounts[]);
-void addToCheck(int index, int barcode[4], int checkBarcodes[][4], int checkAmount[], int* checkCount, char names[][NAME_LEN]); 
-void printCheck(int checkBarcodes[][4], int checkAmount[], int checkCount, int productBarcodes[][4], char names[][NAME_LEN], 
-    int prices[], int discounts[], int* totalCost, int* totalDiscount, int* finalCost); 
+void addToCheck(int index, char barcode[], char checkBarcodes[][5], int checkAmount[], int* checkCount, char names[][NAME_LEN]);
+void printCheck(char checkBarcodes[][5], int checkAmount[], int checkCount,
+    char productBarcodes[][5], char names[][NAME_LEN], int prices[], int discounts[],
+    int* totalCost, int* totalDiscount, int* finalCost);
 void calculateFinalCost(int totalCost, int totalDiscount, int finalCost);
 
 int main() {
     // База товаров
-    int productBarcodes[N][4];
-    char productNames[N][NAME_LEN];
-    int productPrices[N];
-    int productDiscounts[N];
+    char productBarcodes[N][5] = {
+        "1222",
+        "2333",
+        "3444",
+        "4555"
+    };
+    char productNames[N][NAME_LEN] = {
+        "Milk",
+        "Banana",
+        "Juice",
+        "Chicken"
+    };
+    int productPrices[N] = { 50, 25, 90, 140 };
+    int productDiscounts[N] = { 10, 20, 15, 30 };
     int productsCount = 4;
 
     // Чек
-    int checkBarcodes[N][4];
+    char checkBarcodes[N][5];
     int checkAmount[N] = { 0 };
     int checkCount = 0;
 
     int totalCost = 0, totalDiscount = 0, finalCost = 0;
 
     int i = -1;
-    int barcode[4];
-
-    listProducts(productBarcodes, productNames, productPrices, productDiscounts);
+    char barcode[5];
 
     printf("<CASH REGISTER>");
 
@@ -80,71 +88,33 @@ int main() {
     } while (1);
 }
 
-// База данных
-void listProducts(int barcodes[][4], char names[][NAME_LEN], int prices[], int discounts[]) {
-    barcodes[0][0] = 1; barcodes[0][1] = 2; barcodes[0][2] = 2; barcodes[0][3] = 2;
-    strcpy_s(names[0], NAME_LEN, "Milk");
-    prices[0] = 50;
-    discounts[0] = 10;
 
-
-    barcodes[1][0] = 2; barcodes[1][1] = 3; barcodes[1][2] = 3; barcodes[1][3] = 3;
-    strcpy_s(names[1], NAME_LEN, "Banana");
-    prices[1] = 25;
-    discounts[1] = 20;
-
-
-    barcodes[2][0] = 3; barcodes[2][1] = 4; barcodes[2][2] = 4; barcodes[2][3] = 4;
-    strcpy_s(names[2], NAME_LEN, "Juice");
-    prices[2] = 90;
-    discounts[2] = 15;
-
-
-    barcodes[3][0] = 4; barcodes[3][1] = 5; barcodes[3][2] = 5; barcodes[3][3] = 5;
-    strcpy_s(names[3], NAME_LEN, "Chicken");
-    prices[3] = 140;
-    discounts[3] = 30;
-
-}
-
-
-int findProduct(int barcode[4], int barcodes[][4], int count) {
-    int i;
-    for (i = 0; i < count; i++) {
-        if (equalBarcodes(barcodes[i], barcode) == 1) {
+int findProduct(char barcode[], char barcodes[][5], int count) {
+    for (int i = 0; i < count; i++) {
+        if (strcmp(barcodes[i], barcode) == 0)
             return i;
-        }
     }
     return -1;
 }
 
 
-int equalBarcodes(int barcode1[4], int barcode2[4]) {
-    int i;
-    for (i = 0; i < 4; i++) {
-        if (barcode1[i] != barcode2[i]) {
-            return 0;
-        }
-    }
-    return 1;
-}
-
-void inputBarcode(int barcode[4]) {
-    printf("Enter barcode: 4 digits\n");
-    int i;
-    for (i = 0; i < 4; i++) 
-        scanf_s("%1d", &barcode[i]);
+int equalBarcodes(char barcode1[], char barcode2[]) {
+    return strcmp(barcode1, barcode2) == 0;
 }
 
 
-void copyBarcode(int barcode1[4], int barcode2[4]) {
-    int i;
-    for (i = 0; i < 4; i++) 
-        barcode1[i] = barcode2[i];
+void inputBarcode(char barcode[]) {
+    printf("Enter barcode (4 digits): ");
+    scanf_s("%4s", barcode, 5);
 }
 
 
-int scanProduct(int productBarcodes[][4], int productsCount, int barcode[4], char productNames[][NAME_LEN]) {
+void copyBarcode(char dest[], char src[]) {
+    strcpy_s(dest, 5, src);
+}
+
+
+int scanProduct(char productBarcodes[][5], int productsCount, char barcode[], char productNames[][NAME_LEN]) {
     inputBarcode(barcode);
     int index = findProduct(barcode, productBarcodes, productsCount);
 
@@ -155,6 +125,7 @@ int scanProduct(int productBarcodes[][4], int productsCount, int barcode[4], cha
 
     return index;
 }
+
 
 
 void showProductInfo(int index, char names[][NAME_LEN], int prices[], int discounts[]) {
@@ -169,38 +140,31 @@ void showProductInfo(int index, char names[][NAME_LEN], int prices[], int discou
 }
 
 
-void addToCheck(int index, int barcode[4], int checkBarcodes[][4], int checkAmount[], int* checkCount, char names[][NAME_LEN]) {
-    int j;
+void addToCheck(int index, char barcode[], char checkBarcodes[][5], int checkAmount[], int* checkCount, char names[][NAME_LEN]) {
     if (index == -1) {
         printf("Scan product first\n");
         return;
     }
 
-    for (j = 0; j < *checkCount; j++) {
-        if (equalBarcodes(checkBarcodes[j], barcode)) {
+    for (int j = 0; j < *checkCount; j++) {
+        if (strcmp(checkBarcodes[j], barcode) == 0) {
             checkAmount[j]++;
             printf("Added to check: %s\n", names[index]);
             return;
         }
     }
 
-    if (*checkCount < N) {
-        for (j = 0; j < 4; j++)
-            checkBarcodes[*checkCount][j] = barcode[j];
+    strcpy_s(checkBarcodes[*checkCount], 5, barcode);
+    checkAmount[*checkCount] = 1;
+    (*checkCount)++;
 
-        checkAmount[*checkCount] = 1;
-        (*checkCount)++;
-
-        printf("Added to check: %s\n", names[index]);
-    }
-    else {
-        printf("Check is full\n");
-    }
+    printf("Added to check: %s\n", names[index]);
 }
 
 
-void printCheck(int checkBarcodes[][4], int checkAmount[], int checkCount,
-    int productBarcodes[][4], char names[][NAME_LEN], int prices[], int discounts[],
+
+void printCheck(char checkBarcodes[][5], int checkAmount[], int checkCount,
+    char productBarcodes[][5], char names[][NAME_LEN], int prices[], int discounts[],
     int* totalCost, int* totalDiscount, int* finalCost) {
 
     if (checkCount == 0) {
