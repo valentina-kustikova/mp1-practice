ef0e3e6d0f7ef4be648da94608198c215e94db45
MichaelIvanckov <m.i.ivanckov@gmail.com>
2026-01-12 03:33:14 +0300

    Иванков М. И. Lab4 - Касса - доделал что-то

--- IvankovMI/Task5(Lab4)/Task5(Lab4)/Main.c
+++ IvankovMI/Task5(Lab4)/Task5(Lab4)/Main.c
@@ -9,12 +9,17 @@
 //НАСТРОЙКИ:                          --------------------------------------------------------------------------------------|
 #define N   1000     //макс длина ввода                                                                                     |
 #define NPR 9999     //макс длина таблицы продуктов (не больше 9999 при длине штрихкода = 4)  (при отрицательном значении   |
-#define BCL 4        //длина штрихкода                                                         память выделится динамически)|
-#define PNL 30       //макс длина имени продукта                                                                            |
+#define MIDC 1       //минимальный размер скидки (в %)                                         память выделится динамически)|
+#define MADC 50      //максимальный размер скидки (в %)                                                                     |
+#define BCL 4        //длина штрихкода                                                                                      |
+#define PNL 70       //макс длина имени продукта                                                                            |
 #define ERL 100      //макс длина сообщения об ошибке                                                                       |
 #define SOURCE_FILE  list_of_items_ANSI.txt          //имя файла со списком продуктов (только ANSI)                         |
 //--------------------------------------------------------------------------------------------------------------------------|
 
+
+#define DEBUG_RAW 39
+#define NOP int __a__ = 1;
 
 #define CONCAT(a, b) a##b
 #define TO_STR_(a) #a
@@ -35,8 +40,8 @@
 #define ISLW is_last_word()
 #define scan {printf("--------\b\b\b\b\b\b\b\b"); scanf_s(format, inp, sizeof(char) * N);} //УРААААА!!! РАБОТАЕТ!!!! ВВОД ПОчИНИЛИ!!!
 #define scan_t scanf_s(format, inp, sizeof(char) * N)
-#define old_scan_with_error {printf("-----\b\b\b\b\b"); scanf_s(format, inp, sizeof(inp));}  //вот она, где собака была зарыта: "sizeof(inp)" возвращает размер УКАЗАТЕЛЯ, как раз, видимо, равный 8 байтам, а вот "sizeof(char) * N" возвращает размер байтового массива длиной N
 //деклассированные элементы:    (возможно, никогда не используются)
+#define old_scan_with_error {printf("-----\b\b\b\b\b"); scanf_s(format, inp, sizeof(inp));}  //вот она, где собака была зарыта: "sizeof(inp)" возвращает размер УКАЗАТЕЛЯ, как раз, видимо, равный 8 байтам, а вот "sizeof(char) * N" возвращает размер байтового массива длиной N
 #define scan_old {printf("----\b\b\b\b"); scanf_s(format, inp, sizeof(inp), inp2, sizeof(inp2));}
 #define FGscan {char buff[N + 1]; printf("----\b\b\b\b"); fgets(buff, N, stdin); sscanf_s(buff, format, inp, sizeof(inp), inp2, sizeof(inp2));}
 #define scan_(fl) input(format, b, inp, inp2, fl)
@@ -53,23 +58,31 @@
 // финиш (завершение покупки)
 // выход.
 
-void choose(char* inp, char* inp2, int* coup, int ic);   //+
-void coupon(char* inp, int* coup);   //+
-void info() { ; }
-//void help();                        //+
-//void callthecashier();              //+
+//void choose(char* inp, char* inp2, int* coup, receipt_t receipt_, int n, int ic);
+void choose(char* inp, char* inp2, int* coup, struct item_in_receipt receipt[], int* l, int n, int ic);
+void coupon(char* inp, int* coup);                        //+
+void info(char* inp, struct product products[], int n);   //+
+//void help();                                            //+
+//void callthecashier();                                  //+
 void Galya() { ; }   // отмена поз.
 void Galina() { ; }  // отмена покупки
 void final() { ; }
-void barcode(char* inp);
-void test(char* inp);
+void barcode(char* inp, struct product products[], struct item_in_receipt receipt[], int* l, int n);
+void test(char* inp);                                     //+
+void show_all_products(struct product products[], int n); //+
+void show_basket(struct item_in_receipt basket[], int n);
 
 void file_to_sortedmatrix(FILE* crt, char* dst[][4], int n);                //старый вариант для старого массива
 errno_t fill_the_table(FILE* src, struct product dst[], int n);
+void set_discounts(struct product dst[], int n);
 int input(char format[], char buff[], char inp[], char inp2[], int flag);   //стереть ?????
 int is_last_word();
+char* point_to_the_first_nonspace(char* str, int n);
+struct product* fast_search(struct product products[], int n, char key[]);
+struct item_in_receipt* fast_search_in_receipt(struct item_in_receipt receipt[], int n, char key[]);
 
 struct { char* msg; int data; int data2;  } error_msg;   //указатель на сообщение об ошибке
+int error_flag = 0;  //флаг, поднимаемый ситуативно вызываемыми функциями в случае ошибки
 char helpi[] = "Вводите ниже цифры \"отсканированных штрихкодов\" и специальные команды, а программа \nсформирует чек и расчитает итоговую стоимость и размер скидки в рублях (без копеек). \nЦифры \"штрихкода\" вводите слитно (без пробелов), в десятичной системе счисления, \nкоманды и \"штрихкоды\" разделяйте пробелами и/или переносами строк. \nСписок команд: \n.coupon           — предъявить скидочный купон (затем попросят ввести номер купона) \n.info <штрихкод>  — получить информацию о товаре, не добавляя его в корзину \n(обратите внимание, команду и \"штрихкод\" надо писать раздельно, пример: .info 0123) \n.. / .fin         — завершить \"сканирование товаров\" и перейти к оплате \n.. / .fin (после оплаты) — закончить просмотр чека и завершить покупку \n.callthecashier   — позвать сотрудника \n.Galya            — отменить уже добавленный к покупке товар \n.Galina           — отменить весь процесс покупки \n.quit             — выйти из программы и завершить процесс \n.help             — вывести эту инструкцию ещё раз \n*просто введённый штрихкод добавляет товар в корзину и выводит базовую информацию о нём \n*\"касса\" обслуживает покупателей непрерывно: после завершения одной покупки начнется следующая\n\n";
 char format[14];                         //строка формата для scanf_s вида "%Ns", где N - максимально разрешенная длина ввода
 
@@ -80,6 +93,7 @@ struct product {
 	int price;
 	int discount;
 } products[NPR];
+struct item_in_receipt { struct product* item; int count; int position; };
 #else
 struct product {
 	char number[BCL + 1];
@@ -88,8 +102,9 @@ struct product {
 	int discount;
 } * products; 
 struct product * make_list_of_products(int n);
+struct item_in_receipt { struct product* item; int count; int position; } * make_receipt(int n);
 #endif
-
+typedef struct item_in_receipt* receipt_t;
 
 
 
@@ -115,8 +130,13 @@ int main() {
 	
 	FILE* list;
 	char inp[N + 1], error_output[ERL + 1], inp2[N + 1]; //buff[N + 1];    //format[9];
-	int n, coup = 0, ic = 0;
+	int n, coup = 0, l = 0, ic = 0;
 	errno_t  error;
+#if (NPR > 0)
+	struct item_in_receipt receipt[NPR];
+#else
+	struct item_in_receipt* receipt;
+#endif
 	
 	//b = buff;
 	
@@ -139,11 +159,16 @@ int main() {
 		printf("Не удалось прочитать колличество продуктов в файле.\n");
 		return 0;
 	}
-	printf("%d\n", n);
+	printf("Всего товаров в магазине: %d\n", n);
 #if ! (NPR > 0)
 	products = make_list_of_products(n);
 	if (products == NULL) {
 		printf("Ошибка выделения памяти при динамическом выделении памяти для таблицы продуктов.\n");
+		return 0;
+	}
+	reciept = make_receipt(n);
+	if (reciept == NULL) {
+		printf("Ошибка выделения памяти при динамическом выделении памяти для товарного чека.\n");
 		return 0;
 	}
 #endif
@@ -154,6 +179,7 @@ int main() {
 		printf(error_output, error_msg.data);
 		return error;
 	}
+	set_discounts(products, n);
 	printf("\n------------------ Добро пожаловать в магазин \"Магазин\"! Вводите свои товары ------------------\n\n");
 
 	//printf("%d\n", n);
@@ -161,7 +187,13 @@ int main() {
 	scan;
 	while (strcmp(inp, ".quit") != 0) {
 		
-		choose(inp, inp2, &coup, ic);
+		choose(inp, inp2, &coup, receipt, &l, n, ic);
+		if (error_flag) {
+			printf("Ошибка обработки команды пользователя.\n");
+			sprintf_s(error_output, sizeof(char) * ERL, "Сообщение об ошибке: %s\n", error_msg.msg);
+			printf(error_output, error_msg.data);
+			return 0;
+		}
 		scan;
 
 	}
@@ -184,10 +216,73 @@ int is_last_word() {            //странная, конечно, реализация. но зато простая
 	}
 }
 
+char* point_to_the_first_nonspace(char* str, int n){
+	int i;
+	for (i = 0; i < n; i++) {
+		if (str[i] == '\n')
+			return NULL;
+		if (str[i] != ' ')
+			return str + i;
+	}
+	return NULL;
+}
+
+struct product* fast_search(struct product products[], int n, char key[]){
+	int i;
+	for (i = 0; i < n; i++) {
+		int ok = 1, k = 0;
+		char* search_ptr = products[i].number,* key_ptr = key;
+		for (; (*search_ptr != '\0') && (*key_ptr != '\0'); search_ptr++, key_ptr++, k++) {
+			//if ((search_ptr > BCL + 1) || (key_ptr > BCL + 1)) {
+			//	error_msg.msg = "";
+			//}
+			if (k > BCL + 1) {       //для безопасности
+				error_msg.msg = "Один из штрихкодов не заканчивается нулем (%d по счету при поиске).";
+				error_msg.data = i;
+				error_flag = 1;
+				return NULL;
+			}
+			if (*search_ptr != *key_ptr) {
+				ok = 0;
+				break;
+			}
+		}
+		if (ok)
+			return products + i;
+	}
+	return NULL;
+}
+
+struct item_in_receipt* fast_search_in_receipt(struct item_in_receipt receipt[], int n, char key[]){
+	int i;
+	for (i = 0; i < n; i++) {
+		int ok = 1, k = 0;
+		char* search_ptr = receipt[i].item->number, * key_ptr = key;
+		for (; (*search_ptr != '\0') && (*key_ptr != '\0'); search_ptr++, key_ptr++, k++) {
+			if (k > BCL + 1) {       //для безопасности
+				error_msg.msg = "Один из штрихкодов не заканчивается нулем (%d по счету при поиске).";
+				error_msg.data = i;
+				error_flag = 1;
+				return NULL;
+			}
+			if (*search_ptr != *key_ptr) {
+				ok = 0;
+				break;
+			}
+		}
+		if (ok)
+			return receipt + i;
+	}
+	return NULL;
+}
+
 
 #if ! (NPR > 0)
 struct product* make_list_of_products(int n){
 	return (struct product*) malloc(sizeof(struct product) * n);
+}
+struct item_in_receipt* make_receipt(int n){
+	return (struct item_in_receipt*)malloc(sizeof(struct item_in_receipt) * n);
 }
 #endif
 
@@ -205,14 +300,18 @@ errno_t fill_the_table(FILE* src, struct product dst[], int n){
 	//5 - У продукта отсутствует цена
 	//6 - Что-то пошло не так с заполнением имени продукта
 	//7 - Что-то пошло не так с заполнением имени продукта (2)
-	int i, error, l, k = 0;
+	//8 - Имя продукта не удалось записать
+	int i, error, l;
 	//float price_buff;
 	char buffer[PNL + 1 + 10], buffer_copy[PNL + 1 + 10];   //"+10" - на пробелы вначале строки
 	char* start, * end, * price;
 	size_t size;
 	for (i = 0; i < n; i++) {
 		char* t = (char*)(1),* context = NULL;
-		error = fscanf_s(src, BCformat, dst[i].number, sizeof(char) * (BCL + 1));
+		int k = 0;
+		//if (i == DEBUG_RAW)
+		//	NOP;
+		error = fscanf_s(src, BCformat, dst[i].number, (size_t)(sizeof(char) * (BCL + 1)));
 		if (error == 0 || error == EOF) {
 			error_msg.msg = (error == 0) ? "Не удалось прочитать штрихкод в %d строке." : "Достигнут конец файла в %d строке.";
 			error_msg.data = i;
@@ -257,17 +356,26 @@ errno_t fill_the_table(FILE* src, struct product dst[], int n){
 		size = sizeof(char) * l;
 		dst[i].name = (char*)malloc(size);
 		if (k == 1) {
-			error = (int)strncpy_s(dst[i].name, sizeof(char) * ((size_t)(l)), start, (size_t)(l - 1));
+			error = (int)strncpy_s(dst[i].name, size, start, (size_t)(l - 1));
+			dst[i].name[l - 1] = '\0';
 		}
 		else {
-			char* second_word,* context_ = NULL;
-			second_word = (strtok_s(buffer_copy, " \t\n", &context_), strtok_s(NULL, " \t\n", &context_));
-			if (second_word == NULL) {
-				error_msg.msg = "Что-то пошло не так с заполнением имени продукта в %d строке.";
+			//char* second_word,* context_ = NULL;
+			//second_word = (strtok_s(buffer_copy, " \t\n", &context_), strtok_s(NULL, " \t\n", &context_));
+			//if (second_word == NULL) {
+			//	error_msg.msg = "Что-то пошло не так с заполнением имени продукта в %d строке.";
+			//	error_msg.data = i;
+			//	return (errno_t)(6);
+			//}
+			//error = (int)strncat_s(start, (size_t)(PNL + 1) - strlen(start), second_word, (size_t)(PNL)-strlen(start));
+			start = point_to_the_first_nonspace(buffer_copy, PNL);
+			if (start == NULL) {
+				error_msg.msg = "Имя продукта в %d строке не удалось записать.";
 				error_msg.data = i;
-				return (errno_t)(6);
+				return (errno_t)(8);
 			}
-			error = (int)strncat_s(start, (size_t)(PNL + 1) - strlen(start), second_word, (size_t)(PNL)-strlen(start));
+			error = (int)strncpy_s(dst[i].name, size, start, (size_t)(l - 1));
+			dst[i].name[l - 1] = '\0';
 		}
 		//strncpy_s(dst[i].name, sizeof(char) * ((size_t)(l)), buffer_copy, (size_t)(l - 1));
 		if (error != 0) {
@@ -278,6 +386,14 @@ errno_t fill_the_table(FILE* src, struct product dst[], int n){
 		}
 	}
 	return (errno_t)(0);
+}
+
+void set_discounts(struct product dst[], int n){
+	int i;
+	srand((unsigned int)time(NULL));
+	for (i = 0; i < n; i++) {
+		dst[i].discount = MIDC + (rand() % (MADC + 1));
+	}
 }
 
 
@@ -294,7 +410,7 @@ int input(char format[], char buff[], char inp[], char inp2[], int flag) {
 }
 
 
-void choose(char* inp, char* inp2, int* coup, int ic) {
+void choose(char* inp, char* inp2, int* coup, struct item_in_receipt receipt[], int* l, int n, int ic) {
 	if (R(".help"))
 		printf(helpi);
 	else if (R(".callthecashier"))
@@ -302,17 +418,21 @@ void choose(char* inp, char* inp2, int* coup, int ic) {
 	else if (R(".coupon"))
 		coupon(inp, coup);
 	else if (R(".info"))
-		info();
-	else if (R(".Galya"))
+		info(inp, products, n);
+	else if (R(".GalyaOtmena"))
 		Galya();
-	else if (R(".Galina"))
+	else if (R(".GalinaOtmena"))
 		Galina();
 	else if (R("..") && R(".fin"))
 		final();
 	else if (R(".test"))
 		test(inp);
+	else if (R(".showall"))
+		show_all_products(products, n);
+	else if (R(".showmy"))
+		show_basket(receipt, *l);
 	else
-		barcode(inp);
+		barcode(inp, products, receipt, l, n);
 }
 
 
@@ -328,13 +448,64 @@ void coupon(char* inp, int* coup) {
 	*coup = 1;
 }
 
+void info(char* inp, struct product products[], int n){
+	//int numb;
+	struct product* product_ptr;
+	if (is_last_word()) {
+		printf("Введите штрихкод продукта: ");
+	}
+	scan;
+	product_ptr = fast_search(products, n, inp);
+	if (product_ptr != NULL)
+		printf("ШТРИХКОД: %s  НАЗВАНИЕ: %s \nЦЕНА: %d руб.  СКИДКА: %d%%\n", product_ptr->number, product_ptr->name, product_ptr->price, product_ptr->discount);
+	else
+		printf("Товар с таким штрихкодом не найден.\n");
+}
 
-void barcode(char* inp) {
-	printf("ТЕСТ: Штрихкод %s принят\n", inp);
+
+void barcode(char* inp, struct product products[], struct item_in_receipt receipt[], int* l, int n) {
+	//printf("ТЕСТ: Штрихкод %s принят\n", inp);
+	struct product* product_ptr;
+	struct item_in_receipt* pos_in_rec;
+	product_ptr = fast_search(products, n, inp);
+	if (product_ptr != NULL) {
+		pos_in_rec = fast_search_in_receipt(receipt, *l, inp);
+		if (pos_in_rec == NULL) {
+			//receipt[*l] = struct item_in_receipt { product_ptr, 1, *l};   //поч не работает?
+			receipt[*l].item = product_ptr;
+			receipt[*l].count = 1;
+			receipt[*l].position = *l;
+			(*l)++;
+		}
+		else
+			pos_in_rec->count++;
+		printf("Товар добавлен в корзину.\nШТРИХКОД: %s  НАЗВАНИЕ: %s \nЦЕНА: %d руб.  СКИДКА: %d%%\n", product_ptr->number, product_ptr->name, product_ptr->price, product_ptr->discount);
+	}	
+	else
+		printf("Товар с таким штрихкодом не найден.\n");
 }
 
 
 void test(char* inp){
 	scan;
 	printf("ТЕСТ: %s\n", inp);
+}
+
+void show_all_products(struct product products[], int n){
+	int i;
+	printf("Всего товаров в магазине: %d\n", n);
+	for (i = 0; i < n; i++) {
+		printf("№п/п: %d  ШТРИХКОД: %s  НАЗВАНИЕ: %s \nЦЕНА: %d руб.  СКИДКА: %d%%\n", i, products[i].number, products[i].name, products[i].price, products[i].discount);
+	}
+}
+
+void show_basket(struct item_in_receipt basket[], int n){
+	int i, c = 0;
+	//for (struct item_in_receipt x: basket)          //эх..
+	for (i = 0; i < n; i++)
+		c += basket[i].count;
+	printf("Всего товаров в корзине: %d (%d видов)\n", c, n);
+	for (i = 0; i < n; i++) {
+		printf("№п/п: %d  ШТРИХКОД: %s  НАЗВАНИЕ: %s \nЦЕНА: %d руб.  СКИДКА: %d%%  КОЛЛИЧЕСТВО: %d\n", i, basket[i].item->number, basket[i].item->name, basket[i].item->price, basket[i].item->discount, basket[i].count);
+	}
 }
