c34bad3b384f8fc3b6bd09b8cb72388c77ab8e48
Заварзина Анна Викторовна <s25380324@unn.ru>
2026-03-11 16:10:47 +0300

    task6

--- task6/task6/main.c
+++ task6/task6/main.c
@@ -1,34 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h> //для памяти malloc()
 #include <locale.h>
+#include <string.h>//для strtok
 
-#include "struct.h"
-#include "funcs.h"
+
+#define MAX_LINE_LEN 512
+
+typedef struct {
+	char* authors;
+	char* title;
+	char* publisher;
+	int year;
+}Book;
+
+
+//выделение памяти 
+void* safe_malloc(size_t size) {
+	void* memory_book = malloc(size);
+	return memory_book;
+}
+
+//деление строки
+Book divide_line(char* line) {
+	Book book;
+	char* token;
+
+	book.authors = safe_malloc(MAX_LINE_LEN);
+	book.title = safe_malloc(MAX_LINE_LEN);
+	book.publisher= safe_malloc(MAX_LINE_LEN);
+	//год
+
+
+	token = strtok(line, ";");
+	if (token != NULL) {
+		strcpy(book.authors, token);
+	}
+	else {
+		strcpy(book.authors, "");
+	}
+
+
+	token = strtok(line, ";");
+	if (token != NULL) {
+		strcpy(book.title, token);
+	}
+	else {
+		strcpy(book.title, "");
+	}
+
+	token = strtok(line, ";");
+	if (token != NULL) {
+		strcpy(book.publisher, token);
+	}
+	else {
+		strcpy(book.publisher, "");
+	}
+	//год
+
+	return book;
+}
+
+//освобождение памяти
+
+
 
 int main() {
-    char file_name[256];
-    char author[256];
-    int count = 0;
+	char* filename = "gggg.txt";
+	setlocale(LC_ALL, "Russian");
 
-    setlocale(LC_ALL, "Russian")
+	FILE* file = fopen(filename, "r");
+	if (file == NULL) {
+		return NULL;
+	}
 
-    printf("введите файл: ");
-    scanf_s("%255s", file_name);
 
-    Book* books = read_books(file_name, &count);
-    if (books==0) {
-        return 1;
-    }
-
-    printf("введите автора: ");
-    getchar();
-    fgets(author, sizeof(author), stdin);
-
-    author[strcspn(author, "\n")] = 0;
-
-    search_author(books, count, author);
-
-    free_books(books, count);
-
-    return 0;
 }
