e294dbd80340def9dcce681258c92d220f1cdbe1
Alexander-Ivashchenko <sasaivasenko154@gmail.com>
2026-03-17 21:38:54 +0300

    Add Task6

---
+++ Task6/library.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include "library.h"
+
+
+void searchByAuthor(Book* books, int count, char* author) {
+    for (int i = 0; i < count; i++) {
+        
+        if (strstr(books[i].authors, author) != NULL) {
+            printf("Авторы: %s\n", books[i].authors);
+            printf("Название: %s\n", books[i].title);
+            printf("Издательство: %s\n", books[i].publisher);
+            printf("Год: %d\n", books[i].year);
+            printf("-------------------\n");
+        }
+    }
+}
