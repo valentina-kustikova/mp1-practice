197bd9e2f39ba9c131ef3775f72b9cad0e86407c
VladislavAvdeev07 <Vlad.Avdeev07@mail.ru>
2026-03-18 15:42:57 +0300

    tk1 lab7

--- AvdeevVS/Task7/Task7/find.h
+++ AvdeevVS/Task7/Task7/find.h
@@ -1,28 +1,34 @@
 #ifndef _FIND_H_
 #define _FIND_H_
+
 #define MAX_LEN 1000
+
 typedef struct {
   int day;
   int month;
   int year;
 } date_of_birth;
+
 typedef struct {
   char* position;
   char* place;
   char* reason_for_termination;
 } last_job;
+
 typedef struct {
   char* town;
   char* street;
   int home;
   int flat;
 } str_address;
+
 typedef struct {
   char* phone_number;
   str_address address;
 } str_contact;
+
 typedef struct {
-  char* full_name;
+  char* full_name; // fio
   date_of_birth birth_date;
   char* profession;
   char* education;
@@ -30,6 +36,13 @@ typedef struct {
   char* marital_status;
   str_contact contact_information;
 } jobless_people;
+
+typedef struct {
+	jobless_people* persons;
+	int count;
+} person_library;
+
 void Finding_right_options(jobless_people* jobless, int njobless, int* nrequired_indexes, int** required_indexes);
+void Finding_right_options(person_library *library, int* nrequired_indexes, int** required_indexes);
 //float percentage_of_higher_education(int nrequired_indexes, int njobless);
 #endif
