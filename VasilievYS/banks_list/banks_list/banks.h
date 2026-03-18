b3a6c862e7c514e11f8011bb2c3f50c93a5c3617
Yaroslav-Vasiliev <yarvasiliev@gmail.com>
2026-03-18 16:06:40 +0300

    notcmpl

--- VasilievYS/banks_list/banks_list/banks.h
+++ VasilievYS/banks_list/banks_list/banks.h
@@ -1,13 +1,25 @@
 #ifndef __f
 #define __f
+//константы как макс размер сьроки
+typedef struct
+{
+	char name[100];
+	float percentage;
+} deposit;
 
 typedef struct
 {
 	char name[99];
 	char owner[99];
-	float demand;
-	float term;
-	float save;
+	deposit* deposites;
+	int deposits_count;
 }bank;
-int find(bank* list, int cnt_s, char* udep);
+
+typedef struct
+{
+	bank* banks;
+	int cnt;
+} banks_library;
+
+int find(banks_library* library, char* udep);
 #endif 
