1c291319b22424e7c55e8b7e8411daa562df1aec
Sergey-Eroshkin <seregaeroschkin@mail.ru>
2026-03-23 22:20:46 +0300

    Some changes

--- EroshkinSA/Task8.1/school.cpp
+++ EroshkinSA/Task8.1/school.cpp
@@ -1,16 +1,44 @@
 #include <string>
+#include <iostream>
 #include "school.h"
 using namespace std;
 
-birth_date::birth_date(string s) {
-	if (s.size() < 10) throw "Too short date!";
-	this->day = stoi("" + s[0] + s[1]);
-	this->month = stoi("" + s[3] + s[4]);
-	string b = "";
-	for (int i = 6; i < s.size(); i++) b += s[i];
-	this->year = stoi(b);
+birth_date::birth_date() {
+	this->day = stoi("20");
+	this->month = stoi("09");
+	this->year = stoi("2007");
 }
 
-school::school(string s) {
+home_address::home_address() {
+	this->city = "NN";
+	this->country = "Russia";
+	this->district = "Avtaz";
+	this->flat = 24;
+	this->house = "14";
+	for (int i = 0; i < 6; i++) this->index[i] = '6';
+	this->region = "NO";
+	this->street = "Krasnyh Partizan";
+}
 
+pupil::pupil() {
+	this->name = "Sergey";
+	this->date = birth_date();
+	this->home = home_address();
+	this->fathername = "Aleksandrovich";
+	this->gen = gender::MAN;
+	this->klass = "11A";
+	this->surname = "Eroshkin";
+}
+
+ostream& pupil::operator<<(ostream& cout) {
+	cout << "#___#" << endl;
+	cout << "NAME: " << this->surname << " " << this->name << " " << this->fathername << endl;
+	cout << "CLASS: " << this->klass << "   GENDER: " << endl;
+	if (this->gen == gender::MAN) cout << "Male";
+	else cout << "Female";
+	cout << endl << "HOME ADDRESS: " << this->home.index << " - " << this->home.country << " "
+		<< this->home.region << " " << this->home.district << " " << this->home.city << " "
+		<< this->home.street << " " << this->home.house << " " << this->home.flat << endl;
+	cout << "BIRTH DATE: " << this->date.day << "." << this->date.month << "." << this->date.year << endl << "#___#" << endl;
+	return cout;
 }
--- EroshkinSA/Task8.1/school.h
+++ EroshkinSA/Task8.1/school.h
@@ -1,26 +1,58 @@
 #ifndef __SCH_H_
 #define __SCH_H_
+using namespace std;
+
+struct home_address {
+	char index[6];
+	int flat;
+	string country = "", region, district, city, street, house;
+
+	home_address();
+	~home_address();
+};
 
 struct birth_date {
 	int day, month, year;
-	
-	birth_date(string);
+
+	birth_date();
+	~birth_date();
 };
 
-struct home_address {
-	int index, home, flat;
-	string country, region, district, city, street;
-
-	home_address(string);
+enum class gender{
+	MAN = 1, WOMAN
 };
 
-struct school {
-	string name, klass, gen;
+struct pupil {
+	string surname, name, fathername, klass;
+	gender gen;
 	birth_date date;
 	home_address home;
 
-	school(string);
-	school operator < (const school&);
+	pupil();
+	~pupil();
+
+	void swap(pupil& a, pupil& b);
+
+	ostream& operator<<(ostream&);
 };
+
+struct school {
+	int count;
+	pupil* list;
+
+	school(string);
+	~school();
+
+	void sort();
+};
+
+void swap(pupil* a, pupil* b); // +
+void read(const char* source, school* sch); // constructor
+void school_sort(school* sch); // +
+void class_sort(school* sch); // inside of school_sort
+int compare(pupil* a, pupil* b); 
+void sorted_list(school* sch); // -
+void print_pupil(pupil s); // ?
+void free_all(school* sch); // ?
 
 #endif
