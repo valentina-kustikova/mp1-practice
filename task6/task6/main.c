702bd17de69b934c344e9d530663dc21346079e5
annazavarzina <zavarzinaana218@gmail.com>
2026-03-24 21:39:23 +0300

    task6

--- task6/task6/main.c
+++ task6/task6/main.c
@@ -8,68 +8,6 @@
 #include "funcs.h"
 #define MAX_LINE_LEN 512 
 
-//typedef struct {
-//    char* authors;
-//    char* title;
-//    char* publisher;
-//    int year;
-//}Book;
-//
-//
-//Book divide_line(char* line) {
-//    Book book;
-//    char* token = strtok(line, ";");
-//    book.authors = (char*)malloc(sizeof(char) * (strlen(token) + 1));
-//    strcpy(book.authors, token);
-//
-//    token = strtok(NULL, ";");
-//    book.title = (char*)malloc(sizeof(char) * (strlen(token) + 1));
-//    strcpy(book.title, token);
-//
-//    token = strtok(NULL, ";");
-//    book.publisher = (char*)malloc(sizeof(char) * (strlen(token) + 1));
-//    strcpy(book.publisher, token);
-//
-//    token = strtok(NULL, ";");
-//    book.year = atoi(token);
-//
-//    return book;
-//}
-//
-//void show_books(Book* books, int n) {
-//    for (int i = 0; i < n; i++)
-//    {
-//        printf("%s;%s;%s;%d\n", books[i].authors, books[i].title, books[i].publisher, books[i].year);
-//    }
-//    printf("\n");
-//}
-//
-//char* to_lower(char* arr) {
-//    for (int i = 0; arr[i] != '\0'; i++) {
-//        arr[i] = tolower(arr[i]);
-//    }
-//    return arr;
-//}
-//
-//
-//
-//void find_by_author(Book* books, int n, char* author) {
-//    for (int i = 0; i < n; i++) {
-//        if (strstr(books[i].authors, author)) {
-//            printf("%s;%s;%s;%d\n", books[i].authors, books[i].title, books[i].publisher, books[i].year);
-//        }
-//    }
-//}
-//
-//void free_books(Book* books, int n) {
-//    for (int i = 0; i < n; i++) {
-//        free(books[i].authors);
-//        free(books[i].title);
-//        free(books[i].publisher);
-//    }
-//    free(books);
-//}
-
 
 int main() {
     const char* filename = "kartoteka.txt";
