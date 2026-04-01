ccf18e42381d71963a09cbb2242501bb58f7a33d
elenaa-gh <hhobana@yandex.ru>
2026-04-01 10:19:41 +0300

    fixxx

--- IvanovaES/LAB2.2/LAB2.2/artist.h
+++ IvanovaES/LAB2.2/LAB2.2/artist.h
@@ -3,7 +3,18 @@
 
 #define MAX_LEN 2048
 
+typedef enum {
+	RENAISSANCE = 0,
+	BAROQUE = 1,
+	IMPRESSIONISM = 2,
+	POST_IMPRESSIONISM = 3,
+	CUBISM = 4,
+	SURREALISM = 5,
+	POP_ART = 6,
+} Style;
+
 typedef struct Artist {
+	// int painter_id;
 	char name[MAX_LEN];
 	int birthYear; // Date
 	char style[MAX_LEN];
--- IvanovaES/LAB2.2/LAB2.2/paintings.h
+++ IvanovaES/LAB2.2/LAB2.2/paintings.h
@@ -3,12 +3,35 @@
 
 #define MAX_LEN 2048
 
+typedef enum {
+	PORTRAIT = 0,
+	RELIGIOUS = 1,
+	DRAWING = 2,
+	SCULPTURE = 3,
+	HISTORY = 4,
+	GROUP_PORTRAIT = 5,
+	SELF_PORTRAIT = 6,
+	MYTHOLOGICAL = 7,
+	SEASCAPE = 8,
+	LANDSCAPE = 9,
+	STILL_LIFE = 10,
+	INTERIOR = 11,
+	SURREAL = 12,
+	POP_ART = 13,
+} Genre;
+
+typedef enum {
+	MUSEUM = 0,
+	PRIVATE = 1,
+} Location;
+
 typedef struct Painting {
-	char artistName[MAX_LEN];
+	// int painting_id;
+	char artistName[MAX_LEN]; // int artist_id;
 	char title[MAX_LEN];
 	int year;
-	char genre[MAX_LEN];
-	char location[MAX_LEN];
+	char genre[MAX_LEN]; // Genre genre;
+	char location[MAX_LEN]; // Location location
 } Painting;
 
 typedef struct PaintingsLibrary {
