75b4e951203dc210ee9af79d7c1b20f5f91ac812
Gerasimova Evgeniya <youwerescammed526@gmail.com>
2026-04-14 21:09:03 +0300

    final changes

--- GerasimovaED/task_6_library_finale/library.c
+++ GerasimovaED/task_6_library_finale/library.c
@@ -1,1 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include "library.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void author_finder(char* author_name, int* au_cnt, BOOK** au_lib, int cnt, BOOK* _lib) {
+	int i, j = 0;
+	for (i = 0; i < cnt; i++) if (strstr(_lib[i].author, author_name) != NULL) (*au_cnt)++;
+	*au_lib = (BOOK*)malloc(*au_cnt * sizeof(BOOK));
+	for (i = 0; i < cnt; i++) {
+		if ((strstr(_lib[i].author, author_name) != NULL)) {
+			(*au_lib)[j].author = (char*)malloc((strlen(_lib[i].author) + 1) * sizeof(char));
+			(*au_lib)[j].name = (char*)malloc((strlen(_lib[i].name) + 1) * sizeof(char));
+			(*au_lib)[j].publishing = (char*)malloc((strlen(_lib[i].publishing) + 1) * sizeof(char));
+			strcpy((*au_lib)[j].author, _lib[i].author);
+			strcpy((*au_lib)[j].name, _lib[i].name);
+			strcpy((*au_lib)[j].publishing, _lib[i].publishing);
+			(*au_lib)[j].year = _lib[i].year;
+			j++;
+		}
+	}
+
+	return;
+}
--- GerasimovaED/task_6_library_finale/main.c
+++ GerasimovaED/task_6_library_finale/main.c
@@ -1,9 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "library.h"
+#include "auxiliary.h"
+
 #include <stdio.h>
 #include <stdlib.h>
-
-#include "auxiliary.h"
-#include "library.h"
+#include <string.h>
+#include <windows.h>
 
 int main() {
+	BOOK* _lib = NULL;
+	BOOK* au_lib = NULL;
+	int cnt = 0, is_exist = 0, au_cnt = 0, i;
+	char author_name[MAX_SIZE+1];
+	char pathway[MAX_SIZE+1];
+
+	while (cnt == 0) {
+		printf("INPUT PATHWAY: ");
+		scanf_s("%256s", pathway, sizeof(pathway));
+		printf("\n");
+		is_exist = cab_counter(&cnt, pathway);
+		if (is_exist == 404) {
+			system("pause");
+			system("cls");
+		}
+	}
+	cab_reader(&cnt, pathway, &_lib);
+
+	printf("INPUT AUTHOR NAME: ");
+	scanf_s("%256s", author_name, sizeof(author_name));
+	printf("\n");
+	author_finder(author_name, &au_cnt, &au_lib, cnt, _lib);
+	cab_printer(au_cnt, au_lib);
+
+	for (i = 0; i < cnt; i++) {
+		free(_lib[i].author);
+		free(_lib[i].name);
+		free(_lib[i].publishing);
+	}
+	for (i = 0; i < au_cnt; i++) {
+		free(au_lib[i].author);
+		free(au_lib[i].name);
+		free(au_lib[i].publishing);
+	}
+	free(_lib);
+	free(au_lib);
+
 	return 0;
 }
