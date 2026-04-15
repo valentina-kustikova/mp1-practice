1877979249004437db966aa4b03d9fd0d0adf5ad
Mikhail-Drumov <drumov2007misha@mail.ru>
2026-04-15 15:03:59 +0300

    INIT

---
+++ DrumovMI/Lab6HR_cpp_class/hr_db.hpp
@@ -0,0 +1,46 @@
+#ifndef __HR_DB_HPP
+#define __HR_DB_HPP
+
+#include <vector>
+#include <string>
+#include <chrono>
+
+class Employee {
+	struct Passport {
+		int series, number;
+		std::string issued_by;
+		std::chrono::year_month_day issue_date;
+		std::chrono::year_month_day birthday;
+		std::string place_of_residence;
+	} passport;
+	std::string education;
+	std::string specialty;
+	std::string department;
+	std::string position;
+	int salary;
+	std::chrono::year_month_day entry_date;
+	std::chrono::year_month_day last_designation_date;
+public:
+	Employee() {};
+	Employee(const std::string& s);
+
+	int age_by_date(std::chrono::year_month_day date) const;
+
+	friend std::ostream& operator<<(std::ostream& out, Employee employee);
+};
+
+class Employees {
+	Employee* employees;
+	size_t n;
+public:
+	Employees(size_t _n) : n(_n), employees(new Employee[_n]) {}
+	Employees(const Employees& empl);
+	Employees(Employees&& empl);
+	~Employees() { delete[] employees; }
+
+	Employee& operator[](size_t ind);
+	Employees pension_employees(std::chrono::year_month_day date) const;
+	friend std::ostream& operator<<(std::ostream& out, Employees& empl);
+};
+
+#endif
