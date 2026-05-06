7f8e4b95b8251e31e4d428ab552b4b12e8bd1d16
MichaelIvanckov <m.i.ivanckov@gmail.com>
2026-05-06 15:17:10 +0300

    Иванков М. И. - Lab7 - библиотека - Удалили и закомментировали лишнее.

--- IvankovMI/Lab7/Lab7/auxiliary.c
+++ IvankovMI/Lab7/Lab7/auxiliary.c
@@ -14,11 +14,11 @@
 
 
 // ‘ункции, опредленные здесь (в т. ч. статические, дл€ устраени€ ошибок компил€ци):
-
-void create_library(book** lib, size_t* lib_s, size_t start_size);
-int fill_library(FILE* src_file, book** lib, size_t* lib_s);
-book fill_book(char* src);
-char* read_line(FILE* f, size_t start_size, bool* valid);
+//
+//void create_library(book** lib, size_t* lib_s, size_t start_size);
+//int fill_library(FILE* src_file, book** lib, size_t* lib_s);
+//book fill_book(char* src);
+//char* read_line(FILE* f, size_t start_size, bool* valid);
 
 static char* pretty_format(char* str);
 static bool check_valid_symb(char symb, const char* forbidden, size_t n);
