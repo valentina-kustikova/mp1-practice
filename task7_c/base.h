e2026feb82af433287dcb3a47dbde57e23da2636
Голованов Никита Алексеевич <s25380321@unn.ru>
2026-05-06 16:08:38 +0300

    lol

--- task7_c/base.h
+++ task7_c/base.h
@@ -1,6 +1,39 @@
 #pragma once
 
 typedef struct {
+	char* street;
+	char* hnum;
+} Address;
+
+typedef struct {
+	int hours;
+	int minutes;
+} Time;
+
+typedef enum {
+	MONDAY = 0,
+	//...
+} Day;
+
+typedef struct {
+	Time start;
+	Time finish;
+	Day day;
+	// DayStatus ?
+} OpenCloseDay; // rename
+
+#define DAYS_NUMBER 7
+
+typedef struct {
+	char* name;
+	Address address;
+	OpenCloseDay modes[DAYS_NUMBER];
+	char* phones;
+	char* special;
+	char* form;
+} Shop;
+
+typedef struct {
 	char name[30];
 	char adres[70];
 	char phones[50];
