c41a7da426fe3f93c6064d371d73c0f4cd7f116e
mashamalkova <masha.malkova.2007@yandex.ru>
2026-05-13 14:53:38 +0300

    task9

--- MalkovaMM/lab9/lab9/pol.h
+++ MalkovaMM/lab9/lab9/pol.h
@@ -22,26 +22,29 @@ public:
 	const polinom& operator =(const polinom&);
 	friend std::ostream& operator << (std::ostream& out, const polinom& p)
 	{
-		out << p.coef[p.deg] << "x^" << p.deg;
+		out << p.coef[p.deg] << "x^" << p.deg << " ";
 		for (int i = p.deg - 1; i >= 2; i--)
 		{
-			if (p.coef[i + 1] < 0)
-				out << " - ";
-			else
-				out << " + ";
-			if (abs(p.coef[i]) == 1)
-				out << "x^" << i;
-			else
-				out << abs(p.coef[i]) << "x^" << i;
+			if (p.coef[i] != 0)
+			{
+				if (p.coef[i + 1] < 0)
+					out << "- ";
+				else
+					out << "+ ";
+				if (abs(p.coef[i]) == 1)
+					out << "x^" << i << " ";
+				else
+					out << abs(p.coef[i]) << "x^" << i << " ";
+			}
 		}
 		if (p.coef[1] < 0)
-			out << " - " << -p.coef[1] << "x ";
-		else
-			out << " + " << p.coef[1] << "x ";
+			out << "- " << -p.coef[1] << "x ";
+		else if (p.coef[1] > 0)
+			out << "+ " << p.coef[1] << "x ";
 		if (p.coef[0] < 0)
 			out << "- " << -p.coef[0];
-		else
-			out << "+ " << p.coef[0];;
+		else if (p.coef[0] > 0)
+			out << "+ " << p.coef[0];
 		return out;
 	};
 };
