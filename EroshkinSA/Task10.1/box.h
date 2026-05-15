f1994cee0ab34b03c88d7b9bd6ecada881b937f0
Sergey-Eroshkin <seregaeroschkin@mail.ru>
2026-05-15 22:46:52 +0300

    Added specialization

--- EroshkinSA/Task10.1/box.h
+++ EroshkinSA/Task10.1/box.h
@@ -3,7 +3,7 @@
 
 template<typename T>
 class Box {
-	size_t size, capacity, step = 1;
+	size_t size, capacity, step;
 	T* elem;
 public:
 	Box(size_t, size_t);
@@ -19,14 +19,21 @@ public:
 	void remove(T&);
 	void remove(T&&);
 	friend std::ostream& operator<< <T>(std::ostream&, const Box<T>&);
+	~Box();
 };
+
+template<typename T>
+Box<T>::~Box() {
+	delete[]elem;
+}
 
 template<typename T>
 std::ostream& operator<<(std::ostream& out, const Box<T>& b) {
 	out << "size = " << b.size << " | capacity = " << b.capacity << " | step = " << b.step << "\n(";
 	for (int i = 0; i < b.size; i++) {
 		out << b.elem[i];
-		if (i + 1 != b.size) out << " ";
+		if (i + 1 == b.size) break;
+		out << " ";
 	}
 	out << ")";
 	return out;
@@ -53,17 +60,25 @@ template<typename T>
 Box<T>::Box(Box<T>&& b) : size(b.size), capacity(b.capacity) {
 	this->elem = b.elem;
 	b.elem = nullptr;
+	b.size = 0;
+	b.capacity = 0;
 }
 
 template<typename T>
 T& Box<T>::operator[](size_t ind) {
-	if (ind > this->size) throw std::exception("Out of range");
+	if (ind > this->size) {
+		std::cerr << "Out of range";
+		throw std::exception("Out of range");
+	}
 	return this->elem[ind];
 }
 
 template<typename T>
 const T& Box<T>::operator[](size_t ind) const {
-	if (ind > this->size) throw std::exception("Out of range");
+	if (ind > this->size) {
+		std::cerr << "Out of range";
+		throw std::exception("Out of range");
+	}
 	return this->elem[ind];
 }
 
@@ -90,7 +105,10 @@ int Box<T>::find(T& el) {
 template<typename T>
 void Box<T>::remove(T& el) {
 	int pos = this->find(el);
-	if (pos == -1) throw std::exception("Not found");
+	if (pos == -1) {
+		std::cerr << "Not found";
+		throw std::exception("Not found");
+	}
 	this->elem[pos] = this->elem[--size];
 }
 
@@ -98,7 +116,10 @@ void Box<T>::remove(T& el) {
 template<typename T>
 void Box<T>::remove(T&& el) {
 	int pos = this->find(el);
-	if (pos == -1) throw std::exception("Not found");
+	if (pos == -1) {
+		std::cerr << "Not found";
+		throw std::exception("Not found");
+	}
 	this->elem[pos] = this->elem[--size];
 }
 
@@ -124,6 +145,149 @@ void Box<T>::push(T&& el) {
 		elem = buff;
 	}
 	elem[size++] = el;
+}
+
+//__________
+//__________
+
+template<typename T>
+class Box<T*> {
+	size_t size, capacity, step;
+	T** elem;
+	size_t* sizes;
+public:
+	Box(size_t, size_t);
+	Box(size_t, size_t, T*&, size_t);
+	Box(const Box<T*>&);
+	Box(Box<T*>&&);
+	T*& operator[](size_t);
+	const T*& operator[](size_t) const;
+	const Box& operator=(const Box&);
+	int find(T*&, size_t);
+	void push(T*&, size_t);
+	void push(T*&&, size_t);
+	void remove(T*&, size_t);
+	void remove(T*&&, size_t);
+	friend std::ostream& operator<< <T*>(std::ostream&, const Box<T*>&);
+	~Box();
+};
+
+template<typename T>
+Box<T*>::~Box() {
+	for (int i = 0; i < size; i++) delete[]elem[i];
+	delete[]sizes;
+	delete[]elem;
+}
+template<typename T>
+Box<T*>::Box(size_t cp, size_t st) : size(0), capacity(cp), step(st) {
+	this->elem = new T * [capacity];
+	this->sizes = new size_t[capacity];
+}
+
+template<typename T>
+Box<T*>::Box(size_t cp, size_t st, T*& el, size_t len) : capacity(cp), step(st), size(cp) {
+	this->elem = new T*[capacity];
+	this->sizes = new size_t[capacity];
+	for (int i = 0; i < size; i++) {
+		elem[i] = el;
+		sizes[i] = len;
+	}
+}
+
+template<typename T>
+T*& Box<T*>::operator[](size_t ind) {
+	if (ind > this->size) {
+		std::cerr << "Out of range";
+		throw std::exception("Out of range");
+	}
+	return this->elem[ind];
+}
+
+template<typename T>
+const T*& Box<T*>::operator[](size_t ind) const {
+	if (ind > this->size) {
+		std::cerr << "Out of range";
+		throw std::exception("Out of range");
+	}
+	return this->elem[ind];
+}
+
+template<typename T>
+int Box<T*>::find(T*& el, size_t len) {
+	for (int i = 0; i < this->size; i++) {
+		if (sizes[i] == len) {
+			bool f = 1;
+			for (int j = 0; j < sizes[i]; j++) {
+				if (elem[i][j] != el[j]) {
+					f = 0;
+					break;
+				}
+			}
+			if (f) return i;
+		}
+	}
+	return -1;
+}
+
+template<typename T>
+void Box<T*>::remove(T*& el, size_t len) {
+	int pos = this->find(el, len);
+	if (pos == -1) {
+		std::cerr << "Not found";
+		throw std::exception("Not found");
+	}
+	this->elem[pos] = this->elem[size];
+	sizes[pos] = elem[--size];
+}
+
+
+template<typename T>
+void Box<T*>::remove(T*&& el, size_t len) {
+	int pos = this->find(el, len);
+	if (pos == -1) {
+		std::cerr << "Not found";
+		throw std::exception("Not found");
+	}
+	this->elem[pos] = this->elem[size];
+	sizes[pos] = elem[--size];
+}
+
+template<typename T>
+void Box<T*>::push(T*& el, size_t len) {
+	if (this->size == this->capacity) {
+		this->capacity += this->step;
+		T** buff = new T*[this->capacity];
+		size_t* szbuff = new size_t[capacity];
+		for (int i = 0; i < size; i++) {
+			buff[i] = elem[i];
+			szbuff[i] = sizes[i];
+		}
+		delete[]elem;
+		delete[]sizes;
+		elem = buff;
+		sizes = szbuff;
+	}
+	elem[size] = el;
+	sizes[size++] = len;
+}
+
+template<typename T>
+void Box<T*>::push(T*&& el, size_t len) {
+	if (this->size == this->capacity) {
+		this->capacity += this->step;
+		T** buff = new T * [this->capacity];
+		size_t szbuff = new size_t[capacity];
+		for (int i = 0; i < size; i++) {
+			buff[i] = elem[i];
+			szbuff[i] = sizes[i];
+		}
+		delete[]elem;
+		delete[]sizes;
+		elem = buff;
+		sizes = szbuff;
+	}
+	elem[size] = el;
+	sizes[size++] = len;
 }
 
 #endif
--- EroshkinSA/Task10.1/main.cpp
+++ EroshkinSA/Task10.1/main.cpp
@@ -4,13 +4,18 @@
 using namespace std;
 
 int main() {
-	int step;
-	cin >> step;
-	Box<int> b(5, step);
-	cout << b << endl;
-	b.push(2);
-	cout << b << endl;
-	b.remove(b[0]);
-	cout << b << endl;
+	try {
+		int step; cin >> step;
+
+		int* a1 = new int[2];
+		a1[0] = 1; a1[1] = 2;
+		int* a2 = new int[3];
+		a2[0] = 2; a2[1] = 3; a2[2] = 4;
+		Box<int*> b1(1, step);
+		b1.push(a2, 3);
+		b1.push(a1, 2);
+		cout << b1.find(a1, 2);
+	}
+	catch (...) {}
 	return 0;
 }
